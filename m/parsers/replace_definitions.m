ef0cd5fffc0cc205bfa3a93e004ccf1fdf22d0ad
Junior Maih <junior.maih@gmail.com>
2012-12-05 15:23:52 -0500

    special function for insertion of definitions

---
+++ m/parsers/replace_definitions.m
@@ -0,0 +1,9 @@
+function batch=replace_definitions(batch,Definitions)
+
+Definitions=regexp(Definitions,'=','split');
+Definitions=vertcat(Definitions{:});
+Definitions=strrep(Definitions,';','');
+for id=size(Definitions,1):-1:1
+    % the parentheses here are not very efficient
+    batch=strrep(batch,Definitions{id,1},['(',Definitions{id,2},')']);
+end
