c8d9f19936fd8f1b71c5c26941840bb8136c8bc4
Junior Maih <junior.maih@gmail.com>
2012-12-05 15:23:16 -0500

    update of symbolic collector

--- m/parsers/collect_symbolic_list.m
+++ m/parsers/collect_symbolic_list.m
@@ -1,5 +1,11 @@
-function list_ss=collect_symbolic_list(batch,item)
-list_ss=regexp(batch,['(?<![\w])',item,'[\d]+(?![\w])'],'match');
+function list_ss=collect_symbolic_list(batch,items)
+if ischar(items)
+    items={items};
+end
+items=strcat(items,'|');
+items=cell2mat(items);
+items=items(1:end-1);
+list_ss=regexp(batch,['(?<![\w])(',items,')[\d]+(?![\w])'],'match');
 if ~isempty(list_ss) && iscell(list_ss{1})
     list_ss_={};
     for ilist=1:numel(list_ss)
@@ -8,3 +14,6 @@ if ~isempty(list_ss) && iscell(list_ss{1})
     list_ss=list_ss_;
 end
 list_ss=unique(list_ss);
+
+
+% (?<!\w)(x|b|c)(\()(\d+)(\))
