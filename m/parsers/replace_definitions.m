3e77f6a6cd67c462f74bab3f9a42b6933b602479
Junior Maih <junior.maih@gmail.com>
2012-12-08 14:23:41 -0500

    minor changes in replace_definitions

--- m/parsers/replace_definitions.m
+++ m/parsers/replace_definitions.m
@@ -1,9 +1,11 @@
 function batch=replace_definitions(batch,Definitions)
 
-Definitions=regexp(Definitions,'=','split');
-Definitions=vertcat(Definitions{:});
-Definitions=strrep(Definitions,';','');
-for id=size(Definitions,1):-1:1
-    % the parentheses here are not very efficient
-    batch=strrep(batch,Definitions{id,1},['(',Definitions{id,2},')']);
+if ~isempty(Definitions)
+    Definitions=regexp(Definitions,'=','split');
+    Definitions=vertcat(Definitions{:});
+    Definitions=strrep(Definitions,';','');
+    for id=size(Definitions,1):-1:1
+        % the parentheses here are not very efficient
+        batch=strrep(batch,Definitions{id,1},['(',Definitions{id,2},')']);
+    end
 end
