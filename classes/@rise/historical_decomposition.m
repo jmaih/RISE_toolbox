60e0ebccb007b44af6c6eb440c56c02b6337d44f
Junior Maih <junior.maih@gmail.com>
2012-12-09 20:39:55 -0500

    historical decomposition cleaned up
    Following a request by Kaiji Chen

--- classes/@rise/historical_decomposition.m
+++ classes/@rise/historical_decomposition.m
@@ -1,4 +1,4 @@
-function obj=historical_decomposition(obj,param_type,dec_start_date,method)
+function Histdec=historical_decomposition(obj,varargin)
 
 % PURPOSE: Computes historical decompositions of a DSGE model
 %-------------------------------------------------------------
@@ -23,159 +23,174 @@ function obj=historical_decomposition(obj,param_type,dec_start_date,method)
 % Junior Maih, Dept of Economics
 % The Central Bank of Norway
 % junior.maih@norges-bank.no
-% this update [Mai 03, 2011]
+% this update [December 09, 2012]
+
+Defaults=struct('histdec_start_date','',...
+    'histdec_method',0);
+
 if isempty(obj)
-    obj=struct();
+    Histdec=Defaults;
     return
 end
 
-if nargin<4
-    method=0;
-    if nargin<3
-        dec_start_date=[];
-        if nargin<2
-            param_type=[];
-            if nargin <1
-                error([mfilename,':: At least a model object should be provided'])
-            end
-        end
-    end
+if numel(obj)>1
+    error([mfilename,':: Historical decomposition for multiple models not permitted'])
 end
 
-mode_=vertcat(obj.estimated_parameters.mode);
-mean_=vertcat(obj.estimated_parameters.mean);
-startval=vertcat(obj.estimated_parameters.startval);
-params=[];
-if ~isempty(param_type)
-    if strcmp(param_type,'mode')
-        if isempty(mode_)
-            warning([mfilename,':: mode has not been estimated']) %#ok<WNTAG>
-        end
-        params=mode_;
-    elseif strcmp(param_type,'mean')
-        if isempty(mean_)
-            if isempty(mode_)
-                warning([mfilename,':: model has not been estimated, using calibration']) %#ok<WNTAG>
-            else
-                warning([mfilename,':: mean has not been estimated, using the mode']) %#ok<WNTAG>
-            end
-            params=mode_;
-        else
-            params=mean_;
-        end
-    elseif strcmp(param_type,'calibration')
-        params=startval;
-    else
-        error([mfilename,':: unrecognized parameter type ',param_type])
-    end
-end
+obj=set_options(obj,varargin{:});
 
-if obj.NumberOfRegimes>1
-    error([mfilename,':: Historical decomposition for multiple regime models not yet implemented'])
-end
-[obj,junk,junk,retcode]=obj.filter('evaluate_params',params);	% by default, this will also smooth....
+[obj,~,~,retcode]=obj.filter();	% by default, this will also smooth....
 
 % extract the state and transition matrices
 if retcode
     error([mfilename,':: model could not be solved'])
 end
+
+oldoptions=obj.options;
+thisDefault=Defaults;
+Fields=fieldnames(thisDefault);
+for ifield=1:numel(Fields)
+    vi=Fields{ifield};
+    if isfield(oldoptions,vi)
+        thisDefault.(vi)=oldoptions.(vi);
+    end
+end
+histdec_start_date=thisDefault.histdec_start_date;
+histdec_method  =thisDefault.histdec_method  ;
+
 % collect the state matrices
 T=obj.T;
 R=obj.R;
 SS=obj.steady_state_and_balanced_growth_path(1:obj.NumberOfEndogenous(2),:);
 
-hist_start_date=obj.Filters.smoothed_variables.regime_1.TimeInfo(1);
-hist_end_date=obj.Filters.smoothed_variables.regime_1.TimeInfo(end);
-if isempty(dec_start_date)
-    dec_start_date=hist_start_date;
-elseif ischar(dec_start_date)
-    dec_start_date=rise_date(dec_start_date);
+smoothed_variables=rise_time_series.collect(obj.Filters.Expected_smoothed_variables);
+smoothed_shocks=rise_time_series.collect(obj.Filters.Expected_smoothed_shocks);
+smoothed_probabilities=rise_time_series.collect(obj.Filters.smoothed_probabilities);
+varList=smoothed_variables.varnames;
+shockList=smoothed_shocks.varnames;
+probList=smoothed_probabilities.varnames;
+hist_start_date=smoothed_variables.TimeInfo(1);
+hist_end_date=smoothed_variables.TimeInfo(end);
+if isempty(histdec_start_date)
+    histdec_start_date=hist_start_date;
+else
+    histdec_start_date=rise_date(histdec_start_date);
 end
-if dec_start_date<hist_start_date || dec_start_date>hist_end_date
+if histdec_start_date<hist_start_date || histdec_start_date>hist_end_date
     error([mfilename,':: the decomposition start date must lie between ',hist_start_date.date,' and ',hist_end_date.date])
 end
-% hist_start=1;
-% hist_finish=obj.options.data.NumberOfObservations;
-% dec_start=Date2Observation(hist_start_date,dec_start_date);
+smoothed_variables=window(smoothed_variables,hist_start_date);
+smoothed_variables=double(smoothed_variables);
+NumberOfObservations=size(smoothed_variables,1);
+smoothed_probabilities=window(smoothed_probabilities,hist_start_date);
+smoothed_probabilities=permute(double(smoothed_probabilities),[2,1]);
+% apply the smoothed probabilities to SS
+Regimes=obj.Regimes;
+[nreg,nchains]=size(Regimes);
+Probs=ones(nreg,NumberOfObservations);
+chainNames={obj.markov_chains.name};
+for ireg=1:nreg
+    reg_i=Regimes(ireg,:);
+    for ichain=1:nchains
+        ch_reg=[chainNames{ichain},'_',int2str(reg_i(ichain))];
+        loc=strcmp(ch_reg,probList);
+        Probs(ireg,:)=Probs(ireg,:).*smoothed_probabilities(loc,:);
+    end
+end
+
+SS=SS(:,1); % to be adjusted for markov chain later
+smoothed_variables=bsxfun(@minus,smoothed_variables,transpose(SS));
+smoothed_variables=permute(smoothed_variables,[2,1]);
+smoothed_shocks=window(smoothed_shocks,hist_start_date);
+smoothed_shocks=permute(double(smoothed_shocks),[2,1,3]);
+
+endo_names={obj.varendo.name};
+exo_names={obj.varexo.name};
+
+% re-order the smoothed variables
+varlocs=locate_variables(varList,endo_names);
+smoothed_variables=smoothed_variables(varlocs,:);
+varlocs=locate_variables(shockList,exo_names);
+smoothed_shocks=smoothed_shocks(varlocs,:);
 
 % number of variables
-endo_nbr = obj.NumberOfEndogenous(2);
+endo_nbr = numel(endo_names);
 
-% number of shocks
-exo_nbr = obj.NumberOfExogenous;
-
-smoothed_vars=bsxfun(@minus,...
-    cell2mat(obj.Filters.smoothed_variables.regime_1.data(2:end,2:end)),...
-    transpose(SS(:,1)));
-smoothed_shocks=cell2mat(obj.Filters.smoothed_shocks.regime_1.data(2:end,2:end,:));
-
-% keep only the relevant part of the sample
-dec_start=hist_start_date.date_2_observation(dec_start_date);
-smoothed_vars=smoothed_vars(:,dec_start:end);
-smoothed_shocks=smoothed_shocks(:,dec_start:end,:);
-% reshape both into var x time x ...
-smoothed_vars=transpose(smoothed_vars);
-smoothed_shocks=permute(smoothed_shocks,[2,1,3]);
-NumberOfObservations=size(smoothed_shocks,2);
+% number of shocks: this is potentially a problem if there are exogenous deterministic variables
+exo_nbr = numel(exo_names);
 
 z = zeros(endo_nbr,exo_nbr+2,NumberOfObservations);
-z(:,end,:)=smoothed_vars;
+z(:,end,:)=smoothed_variables;
 
-z=HistoricalDecompositionEngine(T,R,z,smoothed_shocks,method);
+z=HistoricalDecompositionEngine(z,smoothed_shocks);
 
 if max(max(abs(squeeze(sum(z(:,1:end-1,:),2))-squeeze(z(:,end,:)))))>1e-9
     error([mfilename,':: Decomposition failed'])
 end
 
-ContributingNames=char(char(obj.varexo.name),'InitialConditions');
-Histdec=rise_time_series(dec_start_date,permute(z(:,1:end-1,:),[3,1,2]),...
-    char(obj.varendo.name),ContributingNames);
-obj=obj.set_properties('hist_dec',Histdec);
+ContributingNames=[exo_names,'InitialConditions'];
 
-% for j=1:endo_nbr
-%     vj=obj.varendo(j).name;
-%     Histdec.(vj)=rise_time_series(dec_start_date,squeeze(zz(:,1:end-1,j)),ContributingNames);
-% end
-
-function z=HistoricalDecompositionEngine(A,B,z,epsilon,method)
-
-[endo_nbr,exo_nbrPlus2,NumberOfObservations]=size(z);
-exo_nbr=exo_nbrPlus2-2;
-
-NumberOfAnticipatedSteps=size(B,3);
-
-for t=1:NumberOfObservations
-    if method
-        if t==1
-            % Collect the shocks
-            for j=1:NumberOfAnticipatedSteps
-                z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
-            end
-            % Find initial condition contribution
-            z(:,exo_nbr+1,t) = z(:,exo_nbr+2,t) - sum(z(:,1:exo_nbr,t),2);
-        else
-            % evolve initial condition
-            z(:,exo_nbr+1,t)=A*z(:,exo_nbr+1,t-1);
-            % collect the shocks
-            z(:,1:exo_nbr,t)=A*z(:,1:exo_nbr,t-1);
-            for j=1:NumberOfAnticipatedSteps
-                z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
-            end
-        end
+Histdec=struct();
+for ii=1:endo_nbr
+    theData=transpose(squeeze(z(ii,1:end-1,:)));
+    if ii==1
+        Histdec.(endo_names{ii})=rise_time_series(histdec_start_date,theData,ContributingNames);
+        TimeInfo=Histdec.(endo_names{ii}).TimeInfo;
     else
-        if t > 1
-            z(:,1:exo_nbr,t) = A*z(:,1:exo_nbr,t-1);
-        end
-        
-        % collect the shocks
-        for j=1:NumberOfAnticipatedSteps
-            z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
-        end
-        % initial conditions
-        z(:,exo_nbr+1,t) = z(:,exo_nbr+2,t) - sum(z(:,1:exo_nbr,t),2);
+        Histdec.(endo_names{ii})=rise_time_series(TimeInfo,theData,ContributingNames);
     end
 end
 
+
+    function z=HistoricalDecompositionEngine(z,epsilon)
+        
+        NumberOfAnticipatedSteps=size(R,3);
+        
+        for t=1:NumberOfObservations
+            [A,B]=expected_state_matrices(t);
+            if histdec_method
+                if t==1
+                    % Collect the shocks
+                    for j=1:NumberOfAnticipatedSteps
+                        z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
+                    end
+                    % Find initial condition contribution
+                    z(:,exo_nbr+1,t) = z(:,exo_nbr+2,t) - sum(z(:,1:exo_nbr,t),2);
+                else
+                    % evolve initial condition
+                    z(:,exo_nbr+1,t)=A*z(:,exo_nbr+1,t-1);
+                    % collect the shocks
+                    z(:,1:exo_nbr,t)=A*z(:,1:exo_nbr,t-1);
+                    for j=1:NumberOfAnticipatedSteps
+                        z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
+                    end
+                end
+            else
+                if t > 1
+                    z(:,1:exo_nbr,t) = A*z(:,1:exo_nbr,t-1);
+                end
+                
+                % collect the shocks
+                for j=1:NumberOfAnticipatedSteps
+                    z(:,1:exo_nbr,t) = z(:,1:exo_nbr,t) + B(:,:,j).*repmat(epsilon(:,t,j)',endo_nbr,1);
+                end
+                % initial conditions
+                z(:,exo_nbr+1,t) = z(:,exo_nbr+2,t) - sum(z(:,1:exo_nbr,t),2);
+            end
+        end
+        
+        function [A,B]=expected_state_matrices(t)
+            probs_t=Probs(:,t);
+            A=probs_t(1)*T(:,:,1);
+            B=probs_t(1)*R(:,:,:,1);
+            for st=2:nreg
+                A=A+probs_t(st)*T(:,:,st);
+                B=B+probs_t(st)*R(:,:,:,st);
+            end
+        end
+    end
+
+end
 %%%%function Packeddec_db=SelectionAndRepackagingEngine(Histdec,Packages,var_list)
 %%%%
 %%%%endo_names=fieldnames(Histdec);
