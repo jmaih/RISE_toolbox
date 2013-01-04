bd096362ab96b1577140046aee40b738ae9e385d
Junior Maih <junior.maih@gmail.com>
2013-01-04 17:55:19 -0500

    deleted old files

--- classes/@rise_anonymous/Contents.m
+++
@@ -1,3 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%   path: classes\@rise_anonymous   %%%%%%%%%%%%%%%%%%%%
-%
-%   <a href="matlab:help classes\@rise_anonymous\rise_anonymous">classes\@rise_anonymous\rise_anonymous</a> - val_i=eval(obj(ii),varargin{:}); %#ok<*EVLC>
