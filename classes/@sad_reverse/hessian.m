af964da31f9159e337f7d45dc0467aad168dfb90
Junior Maih <junior.maih@gmail.com>
2013-01-04 18:02:11 -0500

    update of reverse-mode symbolic differentiation

--- classes/@sad_reverse/hessian.m
+++ classes/@sad_reverse/hessian.m
@@ -1,21 +1,51 @@
-function [H,Jac_char,Hrs,Jac_rs]=hessian(objectives,varnames,wrt)
-if nargin<2
-    error([mfilename,':: at least 2 input arguments should be provided'])
+function [H,this,hjmap,jac_hess_mat]=hessian(funcs,varList,wrt)
+
+if ischar(funcs)
+    funcs=cellstr(funcs);
+elseif iscell(funcs)
+    if isa(funcs{1},'sad_reverse')
+        funcs=[funcs{:}];
+    end
 end
-if nargin<3
-    wrt=varnames;
+if ischar(wrt)
+    wrt=cellstr(wrt);
+end
+eqtn_nbr=numel(funcs);
+wrt_nbr=numel(wrt);
+
+if eqtn_nbr>1
+    error('number of equations must be 1')
 end
 
-[Jac_char,Jac_rs]=sad_reverse.jacobian(objectives,varnames,wrt);
-Jac_char=Jac_char(:);
-Jac_rs=Jac_rs(:);
+% call the jacobian in non-vectorized form
+[this,jmap,jac_restrict_refs]=sad_reverse.jacobian(funcs,varList,wrt,false);
 
-n=numel(wrt);
-H=cell(sum(1:n),1);
-Hrs=sad_reverse.empty(0,1);
-iter=0;
-for irow=1:n
-    [Hi,Hrs(iter+(1:n-irow+1),1)]=sad_reverse.jacobian(Jac_rs(irow),varnames,wrt(irow:end));
-    H(iter+(1:n-irow+1))=Hi;
-    iter=iter+(n-irow+1);
+% initialize the Hessian matrix
+H=sad_reverse(0);
+H=H(ones(wrt_nbr));
+
+for irow=1:wrt_nbr
+    % call the rest in non-vectorized form as well
+    H(irow:end,irow)=sad_reverse.jacobian(this(irow),varList,wrt(irow:end),false);
 end
+
+jmap.fixed_references=union(jmap.fixed_references,...
+    sad_reverse.extract_list(jac_restrict_refs,jmap.prefix_list(1)));
+% build on the existing map to create something for the hessian
+[hess_restrict_refs,hmap]=char(H,jmap);
+hjmap=hmap;
+% cut the head as it belongs to jmap
+hmap.fid=hmap.fid(jmap.line_count+1:end,:);
+hmap.line_count=size(hmap.fid,1);
+hjmap.fixed_references=union(jmap.fixed_references,...
+    sad_reverse.extract_list(hess_restrict_refs,hmap.prefix_list(1)));
+hmat=sad_reverse.create_matrix(hmap,hess_restrict_refs,wrt_nbr,'Hess_');
+% closing statement to include the elements above the diagonal
+hmat=[hmat
+    'Hess_=tril(Hess_)+tril(Hess_,-1)'';'];
+% create a matrix for the jacobian as well
+jmat=sad_reverse.create_matrix(jmap,jac_restrict_refs,wrt_nbr,'Jac_');
+
+% put jacobian and hessian matrices together
+jac_hess_mat=[jmat;hmat];
+
