0da1bb3c3060bf3761ad06c1ed758910e7c965d3
Junior Maih <junior.maih@gmail.com>
2013-01-10 18:59:25 -0500

    temporary modification for speed testing

--- m/filtering/CheckPositiveDefiniteness.m
+++ m/filtering/CheckPositiveDefiniteness.m
@@ -3,16 +3,29 @@ function [ispd,dF,iF]=CheckPositiveDefiniteness(F)
 ispd=false;
 iF=[];
 dF=[];
-if ~(any(any(isnan(F)))||any(any(isinf(F))))
-    [V,D]=eig(F);
-    eigvals=diag(D);
-    ispd=min(eigvals)>0;
-    if ispd
-        dF=prod(eigvals);
-        iF=V*diag(1./eigvals)*V';
+
+test=true;
+if test
+    dF = det(F);
+    if dF<=0
+        return
+    else
+        iF=F\eye(size(F,1));%inv(F);
+    end
+    if ~any(any(isnan(iF))) &&  ~any(any(isinf(iF)))
+        ispd=true;
+    end
+else
+    if ~(any(any(isnan(F)))||any(any(isinf(F))))
+        [V,D]=eig(F);
+        eigvals=diag(D);
+        ispd=min(eigvals)>0;
+        if ispd
+            dF=prod(eigvals);
+            iF=V*diag(1./eigvals)*V';
+        end
     end
 end
-
 end
 
 % all( all( M == M' ) ) & min( eig( M ) ) > 0
