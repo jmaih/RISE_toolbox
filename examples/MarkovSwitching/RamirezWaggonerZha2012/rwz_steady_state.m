991240674e47f08ce10c39de0860c6dabbd719ff
Junior Maih <junior.maih@gmail.com>
2013-02-14 06:47:21 -0500

    steady state file for the frwz example updated

--- examples/MarkovSwitching/RamirezWaggonerZha2012/rwz_steady_state.m
+++ examples/MarkovSwitching/RamirezWaggonerZha2012/rwz_steady_state.m
@@ -1,4 +1,4 @@
-function [ss,retcode,imposed]=rwz_steady_state(param_obj,flag)%param_struct=struct(parlist,parvals)
+function [params,ss,retcode,imposed]=rwz_steady_state(params,flag)%param_struct=struct(parlist,parvals)
 % if flag==0, then return the list of the variables, else return the values
 % of their steady states
 
@@ -7,13 +7,15 @@ imposed=true;
 retcode=0;
 switch flag
     case 0
-        ss={'PAI','Y','R'};
+        params={'PAI','Y','R'};
     case 1
-        number_of_regimes=numel(param_obj(1).startval);
+        name_loc=strcmp('name',params(1,:));
+        param_names=params{2,name_loc};
+        val_loc=strcmp('startval',params(1,:));
+        par_vals=params{2,val_loc};
+        number_of_regimes=size(par_vals,2);
         number_of_variables=3;
-        param_names={param_obj.name};
         
-        par_vals=vertcat(param_obj.startval);
         ss=nan(number_of_variables,number_of_regimes);
         for ii=1:number_of_regimes
             [ss(:,ii)]=regime_specific_steady_state(param_names,par_vals(:,ii));
