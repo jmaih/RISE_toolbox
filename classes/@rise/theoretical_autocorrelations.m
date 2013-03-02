e2311f319645c1aa022c51c4e0fbb0edc1648b6c
Junior Maih <junior.maih@gmail.com>
2013-03-02 07:32:15 -0500

    change in behavior of autocorrelation and autocovariance

--- classes/@rise/theoretical_autocorrelations.m
+++ classes/@rise/theoretical_autocorrelations.m
@@ -1,4 +1,4 @@
-function [A,info]=theoretical_autocorrelations(obj,ar)%,resolve_flag
+function [A,retcode]=theoretical_autocorrelations(obj,varargin)%,resolve_flag
 if isempty(obj)
     if nargout>1
         error([mfilename,':: when the object is emtpy, nargout must be at most 1'])
@@ -6,13 +6,22 @@ if isempty(obj)
     A=theoretical_autocovariances(obj);
     return
 end
-if nargin<2
-    ar=[];
+
+nobj=numel(obj);
+if nobj>1
+    A=cell(1,nobj);
+    retcode=cell(1,nobj);
+    for iobj=1:nobj
+        [A{iobj},retcode{iobj}]=theoretical_autocorrelations(obj(iobj),varargin{:});
+    end
+    return
 end
 
-[A,info]=theoretical_autocovariances(obj,ar);%,resolve_flag
+[A,retcode]=theoretical_autocovariances(obj,varargin{:});%,resolve_flag
 
-if ~info
+if ~retcode
+    obj=set_options(obj,varargin{:});
+    ar=obj.options.ar;
     stdevmat=sqrt(diag(A(:,:,1)));
     bad=stdevmat<=0;
     if any(bad)
