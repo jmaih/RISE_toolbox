f4ddfd619573e9bbd52512704b2db104bec81d86
Junior Maih <junior.maih@gmail.com>
2013-04-16 17:30:59 -0400

    folder for kronecker operations

---
+++ m/utilities/kronecker_operations/A_times_kron_I_B.m
@@ -0,0 +1,21 @@
+function C=A_times_kron_I_B(A,B,q)
+% computes C=A*kron(Iq,B)
+
+% clc,rb=10;cb=7;q=9;ra=3;ca=rb*q;
+% B=rand(rb,cb);A=rand(ra,ca);
+% max(max(abs(A_times_kron_I_B(A,B,q)-A*kron(eye(q),B))))
+
+[rb,cb]=size(B);
+[ra,ca]=size(A);
+if ca~=rb*q
+    error('mbtrices sizes inconsistent')
+end
+
+C=zeros(ra,q*cb);
+for ii=1:q
+    iter_d=(ii-1)*rb+1:ii*rb;
+    iter_c=(ii-1)*cb+1:ii*cb;
+    C(:,iter_c)=A(:,iter_d)*B;
+end
+
+end
---
+++ m/utilities/kronecker_operations/A_times_kron_I_B_I.m
@@ -0,0 +1,14 @@
+function C=A_times_kron_I_B_I(A,B,q,r)
+% computes C=A*kron(kron(Iq,B),Ir)
+
+% clc,rb=10;cb=7;q=9;ra=3;r=4;ca=rb*q*r;
+% B=rand(rb,cb);A=rand(ra,ca);
+% max(max(abs(A_times_kron_I_B_I(A,B,q,r)-A*kron(kron(eye(q),B),eye(r)))))
+
+if size(A,2)~=q*r*size(B,1)
+    error('mbtrices sizes inconsistent')
+end
+
+C=A_times_kron_I_B(kron(B,eye(r)),A,q);
+
+end
