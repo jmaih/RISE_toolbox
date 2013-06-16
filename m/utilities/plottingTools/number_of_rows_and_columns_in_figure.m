080a20bd6a4f521ac278ec16dd537c559084893a
Junior Maih <junior.maih@gmail.com>
2013-06-15 21:52:14 -0400

    update of number of plots

--- m/utilities/plottingTools/number_of_rows_and_columns_in_figure.m
+++ m/utilities/plottingTools/number_of_rows_and_columns_in_figure.m
@@ -2,12 +2,17 @@ function [Remains,r,c]=number_of_rows_and_columns_in_figure(fig,nvar,r0,c0)
 Remains=nvar-(fig-1)*(r0*c0);
 
 if Remains<r0*c0
-    r=min(1,Remains);
-    c=min(1,Remains);
-    while r*c<Remains
-        r=min(r+1,r0);
-        if r*c<Remains
-            c=min(c+1,c0);
+    if Remains==3 && r0>=3
+        r=3;
+        c=1;
+    else
+        r=min(1,Remains);
+        c=min(1,Remains);
+        while r*c<Remains
+            r=min(r+1,r0);
+            if r*c<Remains
+                c=min(c+1,c0);
+            end
         end
     end
 else
