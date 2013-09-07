2f0091378c474e9e764b0a8b7336958f8911e9fd
Junior Maih <junior.maih@gmail.com>
2013-09-07 11:03:07 -0400

    uint64 issue

--- Alpha/classes/@rise_sym/commit.m
+++ Alpha/classes/@rise_sym/commit.m
@@ -6,7 +6,7 @@ end
 
 [eqtn,obj]=recompose(obj);
 if ~isKey(rise_sym_main_map.fid,eqtn);
-    Count=rise_sym_main_map.fid.Count+1;
+    Count=double(rise_sym_main_map.fid.Count)+1;
     ref=['ref_',rise_sym_main_map.tag,'_',sprintf('%.0f',Count),'_']; % adding an underscore latter so that we can use strrep, which should be faster, rather than regexp
     obj.ref=ref; % handle will push this information automatically.
     obj.key=eqtn; % handle will push this information automatically.
