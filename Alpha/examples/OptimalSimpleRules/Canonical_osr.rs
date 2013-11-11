a991ea9fe6e64f161c77785a626112b61bd01bb1
jmaih <junior.maih@gmail.com>
2013-11-11 18:49:39 +0100

    Update of the OSR example (for Paul McNelis)

--- Alpha/examples/OptimalSimpleRules/Canonical_osr.rs
+++ Alpha/examples/OptimalSimpleRules/Canonical_osr.rs
@@ -11,26 +11,26 @@ model(linear);
 
    PAI=lamb_lag*PAI(-1)+lamb_lead*PAI(+1)+lamb_y*Y(-1)+sigpai*EPAI;
 
-   // Taylor rule
+   % Taylor rule
    I=gam_lag*I(-1)+(1-gam_lag)*(PAI(+4)+gam_y*Y)+sigi*EI;
 				  
    R=I-PAI(+1);
-   // interest rate smoothing
+   % interest rate smoothing
    DI=I-I(-1);
 
-// no need to specify a list of observable variables
+% no need to specify a list of observable variables
 
-// specify the planner's objective. 
-planner_objective -.5*(1*PAI^2+.3*Y^2+0.9*DI^2);//{discount = 0.99}
-// The discount factor does not play a role. So I can... neglect it.
-//planner_objective{discount = 0.99} -.5*(1*PAI^2+.3*Y^2+0.9*DI^2);	 
+% specify the planner's objective. 
+planner_objective -.5*(1*PAI^2+.3*Y^2+0.9*DI^2);%{discount = 0.99}
+% The discount factor does not play a role. So I can... neglect it.
+%planner_objective{discount = 0.99} -.5*(1*PAI^2+.3*Y^2+0.9*DI^2);	 
 
 parameterization;
-// not estimated
+% not estimated
 	beta_lag 	 ,0.5000;     
 	beta_lead	 ,0.4000;	 
 	beta_r  	 ,0.9000;
-	// estimate the monetary policy parameter
+	% estimate the monetary policy parameter
 	gam_lag 	 ,0.6000, 0.0000, 1.0000;	 
 	gam_y   	 ,0.5000, 0.0000, 10.0000;	 
 	lamb_lag	 ,0.8000;	 
