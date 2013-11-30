cb59e9d0a5f41ea7fb8b068ad56feadc5181c311
jmaih <junior.maih@gmail.com>
2013-11-30 17:39:55 +0100

    corrected bug introduced earlier today
    Issue comes from substituting rise_sym to planar. rise_sym was still
    used by the parser.

--- Alpha/classes/+parser/parse.m
+++ Alpha/classes/+parser/parse.m
@@ -903,7 +903,7 @@ end
 
 function [derivs,numEqtns,numVars,jac_toc]=differentiate_system(myfunc,input_list,incidence,wrt,Partitions,order)
 
-with_respect_to=rise_sym.extend_differentiation_list(incidence,wrt);
+with_respect_to=extend_differentiation_list(incidence,wrt);
 
 myfunc=analytical_symbolic_form(myfunc,input_list,'symbolic');
 
@@ -967,4 +967,41 @@ for oo=1:order
     end
 end
 derivs=myderivs;
+
+    function wrt=extend_differentiation_list(incidence,wrt0)
+        
+        set_differentiation_list();
+        
+        incidence=incidence(:);
+        n=numel(incidence);
+        wrt=cell(1,n);
+        wrt(incidence)=wrt0;
+        iter=0;
+        for ii_=1:n
+            if isempty(wrt{ii_})
+                iter=iter+1;
+                wrt{ii_}=['zZzZzZz_',sprintf('%0.0f',iter)];
+            end
+        end
+        
+        function set_differentiation_list()
+            reprocess=size(wrt0,2)==2 && isnumeric(wrt0{1,2});
+            if reprocess
+                with_respect_to=cell(1,300);
+                jter=0;
+                for iii=1:size(wrt0,1)
+                    digits=wrt0{iii,2};
+                    xx=wrt0{iii,1};
+                    for id=1:numel(digits)
+                        jter=jter+1;
+                        if jter==size(with_respect_to,2)
+                            with_respect_to{end+300}={};
+                        end
+                        with_respect_to{jter}=[xx,'_',sprintf('%0.0f',digits(id))];
+                    end
+                end
+                wrt0=with_respect_to(1:jter);
+            end
+        end
+    end
 end
