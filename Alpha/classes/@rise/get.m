4996c30c5cc9b34e29b3867aab4e6c2945899fa6
jmaih <junior.maih@gmail.com>
2014-01-01 17:55:27 +0100

    Simplified code for listing

--- Alpha/classes/@rise/get.m
+++ Alpha/classes/@rise/get.m
@@ -19,16 +19,16 @@ elseif ismember(lower(PropertyName),{'sstate','steadystate','steady_state'})
     Reply=load_steady_state_or_balanced_growth(obj.solution.ss);
 elseif ismember(lower(PropertyName),{'par_vals','parameters'})% <--strcmpi(PropertyName,'par_vals')
     Reply=load_par_vals();
-elseif strncmpi(PropertyName,'par_list',8)
-    Reply=load_par_list();
 elseif strcmpi(PropertyName,'par_texnames')
     Reply=obj.parameters.tex_name;
+elseif strncmpi(PropertyName,'par_list',8)
+    Reply=load_list('parameters');
 elseif strncmpi(PropertyName,'endo_list',9)
-    Reply=load_endo_list();
+    Reply=load_list('endogenous');
 elseif strncmpi(PropertyName,'exo_list',8)
-    Reply=load_exo_list();
+    Reply=load_list('exogenous');
 elseif strncmpi(PropertyName,'obs_list',8)
-    Reply=load_obs_list();
+    Reply=load_list('observables');
 elseif strcmpi(PropertyName,'chain_list')
     Reply=obj.markov_chains.chain_names;
 elseif strcmpi(PropertyName,'regime_list')
@@ -105,88 +105,8 @@ end
         end
         
     end
-    function  Reply=load_par_list()
-        Reply=obj.parameters.name;
-        proplength=length('par_list');% =8
-        if length(PropertyName)>proplength
-            if ~strcmp(PropertyName(proplength+1),'(')
-                error(['left parenthesis expected after par_list, but found ',PropertyName(proplength+1)])
-            end
-            if ~strcmp(PropertyName(end),')')
-                error(['right parenthesis expected at the end of string, but found ',PropertyName(end)])
-            end
-            item=PropertyName(proplength+2:end-1);
-            chain_loc=find(strcmpi(item,obj.markov_chains.chain_names));
-            if ~isempty(chain_loc)
-                mygovern=obj.parameters.governing_chain==chain_loc;
-            else
-                switch lower(item)
-                    case 'switch'
-                        mygovern=obj.parameters.is_switching;
-                    case '~switch'
-                        mygovern=~obj.parameters.is_switching;
-                    case 'in_use'
-                        mygovern=obj.parameters.is_in_use;
-                    case '~in_use'
-                        mygovern=~obj.parameters.is_in_use;
-                    otherwise
-                        error(['',item,''' is not recognized as the name of a markov chain'])
-                end
-            end
-            Reply=Reply(mygovern);
-        end
-    end
-    function Reply=load_obs_list()
-        Reply=obj.observables.name;
-        proplength=length('obs_list');% =8
-        if length(PropertyName)>proplength
-            if ~strcmp(PropertyName(proplength+1),'(')
-                error(['left parenthesis expected after exo_list, but found ',PropertyName(proplength+1)])
-            end
-            if ~strcmp(PropertyName(end),')')
-                error(['right parenthesis expected at the end of string, but found ',PropertyName(end)])
-            end
-            item=PropertyName(proplength+2:end-1);
-            switch lower(item)
-                case 'endo'
-                    mygovern=obj.observables.is_endogenous;
-                case '~endo'
-                    mygovern=~obj.observables.is_endogenous;
-                otherwise
-                    error(['',item,''' is not recognized as a property of observable variables'])
-            end
-            Reply=Reply(mygovern);
-        end
-    end
-    function  Reply=load_exo_list()
-        Reply=obj.exogenous.name;
-        proplength=length('exo_list');% =8
-        if length(PropertyName)>proplength
-            if ~strcmp(PropertyName(proplength+1),'(')
-                error(['left parenthesis expected after exo_list, but found ',PropertyName(proplength+1)])
-            end
-            if ~strcmp(PropertyName(end),')')
-                error(['right parenthesis expected at the end of string, but found ',PropertyName(end)])
-            end
-            item=PropertyName(proplength+2:end-1);
-            switch lower(item)
-                case 'obs'
-                    mygovern=obj.exogenous.is_observed;
-                case '~obs'
-                    mygovern=~obj.exogenous.is_observed;
-                case 'in_use'
-                    mygovern=obj.exogenous.is_original;
-                case '~in_use'
-                    mygovern=~obj.exogenous.is_original;
-                otherwise
-                    error(['',item,''' is not recognized as a property of exogenous variables'])
-            end
-            Reply=Reply(mygovern);
-        end
-    end
-    function Reply=load_endo_list()
-        
-        Reply=obj.endogenous.name;
+    function Reply=load_list(type)
+        Reply=obj.(type).name;
         proplength=length('endo_list');% =9
         if length(PropertyName)>proplength
             if ~strcmp(PropertyName(proplength+1),'(')
@@ -195,42 +115,21 @@ end
             if ~strcmp(PropertyName(end),')')
                 error(['right parenthesis expected at the end of string, but found ',PropertyName(end)])
             end
-            item=PropertyName(proplength+2:end-1);
-            switch lower(item)
-                case 'lagrange_multiplier'
-                    mygovern=obj.endogenous.is_lagrange_multiplier;
-                case '~lagrange_multiplier'
-                    mygovern=~obj.endogenous.is_lagrange_multiplier;
-                case 'static'
-                    mygovern=obj.endogenous.is_static;
-                case '~static'
-                    mygovern=~obj.endogenous.is_static;
-                case 'predetermined'
-                    mygovern=obj.endogenous.is_predetermined;
-                case '~predetermined'
-                    mygovern=~obj.endogenous.is_predetermined;
-                case 'pred_frwrd_looking'
-                    mygovern=obj.endogenous.is_pred_frwrd_looking;
-                case '~pred_frwrd_looking'
-                    mygovern=~obj.endogenous.is_pred_frwrd_looking;
-                case 'frwrd_looking'
-                    mygovern=obj.endogenous.is_frwrd_looking;
-                case '~frwrd_looking'
-                    mygovern=~obj.endogenous.is_frwrd_looking;
-                case 'original'
-                    mygovern=obj.endogenous.is_original;
-                case '~original'
-                    mygovern=~obj.endogenous.is_original;
-                case 'log_var'
-                    mygovern=obj.endogenous.is_log_var;
-                case '~log_var'
-                    mygovern=~obj.endogenous.is_log_var; %
-                case {'auxiliary'}
-                    mygovern=obj.endogenous.is_auxiliary;
-                case {'~auxiliary'}
-                    mygovern=~obj.endogenous.is_auxiliary; %
-                otherwise
-                    error(['',item,''' is not recognized as a property of endogenous variables'])
+            item=lower(PropertyName(proplength+2:end-1));
+            ff=fieldnames(obj.(type));
+            ff=regexp(ff,'(?<!\w+)is_\w+','match');
+            ff=[ff{:}];
+            negative=strcmp(item(1),'~');
+            if negative
+                item=item(2:end);
+            end
+            if ~ismember(['is_',item],ff)
+                disp(strrep(ff,'is_',''))
+                error(['"',item,'" is not a valid property. The valid properties are listed above'])
+            end
+            mygovern=obj.(type).(['is_',item]);
+            if negative
+                mygovern=~mygovern;
             end
             Reply=Reply(mygovern);
         end
