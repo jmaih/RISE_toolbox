b53951883875d0780123980a8da052ff3f68dc30
jmaih <junior.maih@gmail.com>
2014-07-13 15:14:47 +0200

    New function for definition insertion (not in use)

---
+++ m/+parser/insert_definitions.m
@@ -0,0 +1,33 @@
+function [listing,nlist]=insert_definitions(listing,flag)
+nlist=size(listing,1);
+
+if flag
+    isdef=false(nlist,1);
+    def_unfinished=false;
+    for irow=1:nlist
+        thiseq=listing{irow,2};
+        thiseq(isspace(thiseq))=[];
+        %--------------
+        isdef(irow)=strcmp(thiseq(1),'#')||def_unfinished;
+        if isdef(irow)            
+            if def_unfinished
+                rhs=[rhs,thiseq]; %#ok<AGROW>
+            else
+                eq_sign=strfind(thiseq,'=');
+                lhs=thiseq(2:eq_sign-1);
+                rhs=thiseq(eq_sign+1:end);
+            end
+            if strcmp(rhs(end),';')
+                def_unfinished=false;
+                repstr=['(',rhs(1:end-1),')'];
+                expr=['(?<!\w+)',lhs,'(?!\w+)'];
+                listing(irow+1:end,2)=regexprep(listing(irow+1:end,2),expr,repstr);
+            else
+                def_unfinished=true;
+            end
+        end
+        %--------------
+    end
+    listing=listing(~isdef,:);
+    nlist=size(listing,1);
+end
