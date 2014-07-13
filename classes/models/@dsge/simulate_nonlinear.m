001556b75ea004923012ce6d1b48e29fd4bae582
jmaih <junior.maih@gmail.com>
2014-07-13 14:50:57 +0200

    More time series-like inputs for nonlinear simulation

--- classes/models/@dsge/simulate_nonlinear.m
+++ classes/models/@dsge/simulate_nonlinear.m
@@ -5,112 +5,156 @@ if isempty(obj)
         'simul_exogenous_func','');
     return
 end
+sims=[];
+
+obj.options.simul_burn=0;
+
 [obj,retcode]=solve(obj,varargin{:});
 
+if retcode
+    return
+    %     error('model cannot be solved')
+end
+
+% initial conditions
+%-------------------
+Initcond=generic_tools.set_simulation_initial_conditions(obj);
 bigt=obj.options.simul_periods;
 
 simul_stack_solve_algo=obj.options.simul_stack_solve_algo;
 
-simul_recursive=obj.options.simul_recursive;
+forecast_exogenous=upload_exogenous_forecast_func();
 
-forecast_exogenous=obj.options.simul_exogenous_func;
-if isempty(forecast_exogenous)
-    error('an simulation function for the exogenous should be provided in simul_exogenous_func')
-end
+% without any argument, the function should return the period of the last
+% non-zero shock. This is to prevents the algorithms from running
+% unnecessary optimizations when the process has already landed.
+t_last_shock=forecast_exogenous();
 
 endo_nbr=obj.endogenous.number(end);
-sims=[];
-if ~retcode
-    s0=1;
-    s1=1;
-    param=obj.parameter_values(:,s1);
-    sparam=param;
-    ss=obj.solution.ss{s1};
-    % initial and final conditions
-    %-----------------------------
-    sims=ss(:,ones(1,bigt+2)); % initial,simul,final
-    ystart=ss;
-    yfinal=ss;
-    sims(:,1)=ystart;
-    sims(:,end)=yfinal;
-    
-    def=obj.solution.definitions{s1};
-    % dynamic model
-    %--------------
-    dynamic_model=obj.routines.dynamic;
-    jacobian_dynamic_model=obj.routines.probs_times_dynamic_derivatives(1);
-    order_var=obj.order_var.after_solve;
-    % locations of various variables in the jacobian
-    %-----------------------------------------------
-    ns=numel(obj.locations.before_solve.v.s_0);
-    np=numel(obj.locations.before_solve.v.p_0);
-    nb=numel(obj.locations.before_solve.v.b_0);
-    nf=numel(obj.locations.before_solve.v.f_0);
-    aplus_cols=ns+np+(1:nb+nf);
-    aplus=obj.locations.before_solve.v.bf_plus;
-    aminus_cols=ns+(1:np+nb);
-    aminus=obj.locations.before_solve.v.pb_minus;
-    a0=obj.locations.before_solve.v.t_0;
-    jac_aminus_a0_aplus=[aminus,a0,aplus];
-    jac_aminus_a0=[aminus,a0];
-    jac_a0_aplus=[a0,aplus];
-    A_aminus_a0=[aminus_cols,endo_nbr+(1:endo_nbr)];
-    A_aminus_a0_aplus=[A_aminus_a0,2*endo_nbr+aplus_cols];
-    A_a0_aplus=[1:endo_nbr,endo_nbr+aplus_cols];
-    
-    lead_lag_incidence=obj.lead_lag_incidence.after_solve;
-    the_leads=find(lead_lag_incidence(:,1)>0);
-    the_current=(1:endo_nbr)';
-    the_lags=find(lead_lag_incidence(:,3)>0);
-    exo_nbr=sum(obj.exogenous.number);
-    x=[];
-    if simul_recursive
-        nsmpl=bigt;
-    else
-        nsmpl=1;
+
+s0=1;
+s1=1;
+param=obj.parameter_values(:,s1);
+sparam=param;
+ss=obj.solution.ss{s1};
+% initial and final conditions
+%-----------------------------
+sims=ss(:,ones(1,bigt+2)); % initial,simul,final
+ystart=Initcond.y.y;
+yfinal=ss;
+sims(:,1)=ystart;
+sims(:,end)=yfinal;
+
+def=obj.solution.definitions{s1};
+% dynamic model
+%--------------
+dynamic_model=obj.routines.dynamic;
+order__=1;
+if isa(obj.routines.probs_times_dynamic_derivatives,'function_handle')
+    jacobian_dynamic_model=@(varargin)obj.routines.probs_times_dynamic_derivatives(order__,varargin{:});
+else
+    jacobian_dynamic_model=obj.routines.probs_times_dynamic_derivatives(order__);
+end
+order_var=obj.order_var.after_solve;
+% locations of various variables in the jacobian
+%-----------------------------------------------
+ns=numel(obj.locations.before_solve.v.s_0);
+np=numel(obj.locations.before_solve.v.p_0);
+nb=numel(obj.locations.before_solve.v.b_0);
+nf=numel(obj.locations.before_solve.v.f_0);
+aplus_cols=ns+np+(1:nb+nf);
+aplus=obj.locations.before_solve.v.bf_plus;
+aminus_cols=ns+(1:np+nb);
+aminus=obj.locations.before_solve.v.pb_minus;
+a0=obj.locations.before_solve.v.t_0;
+jac_aminus_a0_aplus=[aminus,a0,aplus];
+jac_aminus_a0=[aminus,a0];
+jac_a0_aplus=[a0,aplus];
+A_aminus_a0=[aminus_cols,endo_nbr+(1:endo_nbr)];
+A_aminus_a0_aplus=[A_aminus_a0,2*endo_nbr+aplus_cols];
+A_a0_aplus=[1:endo_nbr,endo_nbr+aplus_cols];
+
+lead_lag_incidence=obj.lead_lag_incidence.after_solve;
+the_leads=find(lead_lag_incidence(:,1)>0);
+the_current=(1:endo_nbr)';
+the_lags=find(lead_lag_incidence(:,3)>0);
+exo_nbr=sum(obj.exogenous.number);
+x=[];
+if obj.options.simul_recursive
+    nsmpl=bigt;
+else
+    nsmpl=1;
+end
+for t=1:min(t_last_shock,nsmpl)
+    if ~retcode
+        % compute the forecast of the exogenous
+        %--------------------------------------
+        x=forecast_exogenous(obj,t);
+        
+        % initialize the solver at the steady state
+        %-------------------------------------------
+        fprintf(1,'\n pass # %0.0f of %0.0f\n',t,nsmpl);
+        switch simul_stack_solve_algo
+            case 'fsolve'
+                options=obj.options.optimset;
+                if obj.options.debug||obj.options.fix_point_verbose
+                    options.Display='iter';
+                end
+                Y0=sims(:,1+(t:bigt));
+                [sims(:,1+(t:bigt)),~,exitflag]=...
+                    fsolve(@objective,Y0,options,sims(:,t),sims(:,end));
+                %-------------------------------------------
+                if exitflag==0
+                    retcode=21;
+                elseif ~utils.error.valid(sims)
+                    retcode=22;
+                else
+                    retcode=(~ismember(exitflag,[1,2,3,4]))*23;
+                end
+                %-------------------------------------------
+            case 'sparse'
+                if t==100
+                    keyboard
+                end
+                [sims(:,t:end),retcode]=sparse_simulation_algorithm(sims(:,t:end));
+            case 'lbj'
+                error('LBJ''s relaxation algorithm not yet implemented')
+            otherwise
+                error(['unknown algorithm ',parser.any2str(simul_stack_solve_algo)])
+        end
     end
-    for t=1:nsmpl
-        if ~retcode
-            % compute the forecast of the exogenous
-            %--------------------------------------
-            x=forecast_exogenous(obj,t);
-            
-            % initialize the solver at the steady state
-            %-------------------------------------------
-            switch simul_stack_solve_algo
-                case 'fsolve'
-                    options=obj.options.optimset;
-                    if obj.options.debug||obj.options.fix_point_verbose
-                        options.Display='iter';
-                    end
-                    Y0=sims(:,1+(t:bigt));
-                    [sims(:,1+(t:bigt)),~,exitflag]=...
-                        fsolve(@objective,Y0,options,sims(:,t),sims(:,end));
-                    %-------------------------------------------
-                    if exitflag==0
-                        retcode=21;
-                    elseif ~utils.error.valid(sims)
-                        retcode=22;
-                    else
-                        retcode=(~ismember(exitflag,[1,2,3,4]))*23;
-                    end
-                    %-------------------------------------------
-                case 'sparse'
-                    [sims(:,t:end),retcode]=sparse_simulation_algorithm(sims(:,t:end));
-                case 'lbj'
-                    error('LBJ''s relaxation algorithm not yet implemented')
-                otherwise
-                    error(['unknown algorithm ',parser.any2str(simul_stack_solve_algo)])
+end
+% store the simulations in a database: use the date for last observation in
+% history and not first date of forecast
+%--------------------------------------------------------------------------
+if isempty(obj.options.simul_history_end_date)
+    obj.options.simul_history_end_date=0;
+end
+start_date= obj.options.simul_history_end_date;
+sims=ts(start_date,full(sims)',obj.endogenous.name);
+sims=pages2struct(sims);
+
+
+    function forecast_exogenous=upload_exogenous_forecast_func()
+        
+        forecast_exogenous=obj.options.simul_exogenous_func;
+        if isempty(forecast_exogenous)
+            forecast_exogenous=@x_forecast;
+        end
+        function x=x_forecast(~,t)
+            if nargin==0
+                last_shock_period=find(any(Initcond.shocks,1),1,'last');
+                x=last_shock_period;
+            else
+                if obj.options.simul_recursive
+                    x=zeros(size(Initcond.shocks));
+                    x(:,1)=Initcond.shocks(:,t);
+                else
+                    x=Initcond.shocks(:,1:end);
+                end
             end
         end
     end
-    % store the simulations in a database: use the date for last observation in
-    % history and not first date of forecast
-    %--------------------------------------------------------------------------
-    start_date=0;
-    sims=ts(start_date,full(sims)',obj.endogenous.name);
-    sims=pages2struct(sims);
-end
 
     function resid=objective(X,X0,Xlast)
         X=reshape(X,endo_nbr,[]);
@@ -184,8 +228,12 @@ end
             end
             dx=-A\resid(:);
             
-            crit=max(abs(dx)); % <---crit=max(abs(resid(:)));
-                Y(order_var,2:end-1)=Y(order_var,2:end-1)+reshape(dx,endo_nbr,[]);
+            crit=max(abs(dx));
+            dx=reshape(dx,endo_nbr,[]);
+            if nsyst==1
+                dx=dx(:,1);
+            end
+            Y(order_var,2:end-1)=Y(order_var,2:end-1)+dx;
         end
     end
 
