1594ff4231a8829823f0e4a12eb90b32deb17630
jmaih <junior.maih@gmail.com>
2014-07-13 15:01:14 +0200

    More flexible horzcat in ts

--- classes/time_series/@ts/horzcat.m
+++ classes/time_series/@ts/horzcat.m
@@ -1,17 +1,13 @@
 function this=horzcat(varargin)
 
 for ii=1:length(varargin)
-    if isempty(varargin{ii})
-        if ii==1
-            error('the first argument cannot be empty')
-        end
+    if ~isequal(class(varargin{ii}),'ts')
+        error([mfilename,':: input ',int2str(ii),' must be from class ts'])
+    end
+    if ii==1
+        this=varargin{ii};
     else
-        if ~isequal(class(varargin{ii}),'ts')
-            error([mfilename,':: input ',int2str(ii),' must be from class ts'])
-        end
-        if ii==1
-            this=varargin{ii};
-        else
+        if ~isempty(varargin{ii})
             this=this&varargin{ii};
         end
     end
