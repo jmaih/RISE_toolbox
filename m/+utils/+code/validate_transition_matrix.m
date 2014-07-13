c2fd3d3d3675f99fce8596d4770efce7082d16e5
jmaih <junior.maih@gmail.com>
2014-07-13 15:24:26 +0200

    validation of transition matrices as a separate function

---
+++ m/+utils/+code/validate_transition_matrix.m
@@ -0,0 +1,12 @@
+function [Qx,retcode]=validate_transition_matrix(Qx)
+total=sum(Qx,2);
+if any(isnan(Qx(:))) || ...
+        any(Qx(:)<0) || ...
+        any(Qx(:)>1)|| ...
+        ~max(abs(total-1))<1e-9
+    retcode=3;
+else
+    retcode=0;
+    Qx=bsxfun(@rdivide,Qx,total);
+end
+end
