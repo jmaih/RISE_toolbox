dcc3b5c2c9247b7808ee131b3aff78b29fcb57aa
jmaih <junior.maih@gmail.com>
2014-07-29 04:25:54 +0200

    update of the howto file for estim of stoch replan

--- examples/UNDER_REVISION/StochasticReplanning_switching_Estimation/howto.m
+++ examples/UNDER_REVISION/StochasticReplanning_switching_Estimation/howto.m
@@ -17,7 +17,7 @@ data=ts(data_start,data,vnames);
 sw=rise('usmodel_sr_switch','data',data,...
     'estim_start_date',obs2date(data_start,71),... % retrieve the date of the 71st observation
     'kf_presample',4,...
-    'lc_MaxIter',5000,... % jack up the number of iterations to increase the probability of solving
+    'fix_point_maxiter',5000,... % jack up the number of iterations to increase the probability of solving
     'check_stability',false); % save some time by avoiding the checking of stability all the time
     
 
