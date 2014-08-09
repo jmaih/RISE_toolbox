b37e6aefed8e2ff511d0a983f9c159808bfc2a3d
jmaih <junior.maih@gmail.com>
2014-08-09 21:09:30 +0200

    Construction of block exogenous restrictions
    and help menu

--- classes/utils/@coef/coef.m
+++ classes/utils/@coef/coef.m
@@ -1,14 +1,35 @@
 classdef coef
+    % utility class for coef class for setting linear restrictions on
+    % models
     properties
+        % place holder for the parameter name, which is either
+        %    - a string: this assumes that the name of the parameter is
+        %    fully known in advance
+        %    - a 3-element cell {eqtn,var_name,lag}: this information will
+        %    be used later on to construct the string name of the
+        %    corresponding parameter. The first element is the equation
+        %    number, the second is the name of the endogenous variable and
+        %    the third is the lag position.
+        %    -  a 5-element cell {eqtn,var_name,lag,chain_name,state}.  Two
+        %    elements are added to the previous case and they are needed in
+        %    case the parameter is switching. So, the fourth element
+        %    corresponds to the name of the markov chain controling the
+        %    parameter and the fifth element is the state of the chain.
         par_name
+    end
+    properties(Access=protected)%,Hidden=true
+        % place holder for function, which can only be one of the
+        % following: plus, minus, mtimes, mrdivide
         func
+        % place holder for the arguments of the function
         args
     end
     methods
         function obj=coef(varargin)
-            % string
-            % eqtn,var_pos,lag
-            % eqtn,var_pos,lag,chain_name,state
+            % there are 3 syntaxes for constructing a coef object
+            % 1-) obj=coef('name')
+            % 2-) obj=coef(eqtn,vname,lag)
+            % 3-) obj=coef(eqtn,vname,lag,chain_name,state)
             n=nargin;
             if n
                 if isa(varargin{1},mfilename)
@@ -26,22 +47,19 @@ classdef coef
             end
         end
         function obj=plus(a,b)
-            if ~(isa(a,'coef') && isa(b,'coef'))
-                error('both arguments should be coef')
-            end
+            % overloading of plus for coef objects
             obj=coef();
             obj.args={a,b};
             obj.func='plus';
         end
         function obj=minus(a,b)
-            if ~(isa(a,'coef') && isa(b,'coef'))
-                error('both arguments should be coef')
-            end
+            % overloading of minus for coef objects
             obj=coef();
             obj.args={a,b};
             obj.func='minus';
         end
         function obj=mtimes(a,b)
+            % overloading of mtimes for coef objects
             obj=coef();
             if isa(a,'coef') && isa(b,'coef')
                 error('multiplication of coef x coef objects not allowed')
@@ -50,6 +68,7 @@ classdef coef
             obj.func='mtimes';
         end
         function obj=mrdivide(a,b)
+            % overloading of mrdivide for coef objects
             if ~isa(b,'double')
                 error('the second argument must be a double')
             end
@@ -72,7 +91,9 @@ classdef coef
             end
             function idval=process_object(this,restr_id)
                 idval=[];
-                if isempty(this.args)
+                if isa(this,'double')
+                    idval=this;
+                elseif isempty(this.args)
                     [id_,parval]=column_location(this.par_name);
                     if isempty(id_)
                         % elements to go to the rhs
@@ -166,6 +187,47 @@ classdef coef
                 governing_chain=self.parameters.governing_chain;
                 endo_names=self.endogenous.name;
             end
+        end
+    end
+    methods(Static)
+        function obj=block_exogenous(endo_list,blk_exo_list,nlags)
+            % list of endogenous
+            % list of block exogenous
+            % number of lags
+            % this cannot be applied to markov switching. In that case, one
+            % has to explicitly list the coefficients that are zero. This
+            % so because different coefficients may be controled by
+            % different markov chains.
+            if ~isequal(sort(endo_list),endo_list)
+                error('The list of endogenous must be sorted')
+            end
+            nv=numel(endo_list);
+            siz_obj=100;
+            incmnt=100;
+            obj=cell(siz_obj,2);
+            iter=0;
+            for ieqtn=1:nv
+                home_equation=~any(strcmp(endo_list{ieqtn},blk_exo_list));
+                if home_equation
+                    continue
+                end
+                for ilag=1:nlags
+                    for ivar=1:nv
+                        endo_var=endo_list{ivar};
+                        home_variable=~any(strcmp(endo_var,blk_exo_list));
+                        if home_variable
+                            iter=iter+1;
+                            if iter==siz_obj
+                                obj=[obj
+                                    cell(incmnt,2)]; %#ok<AGROW>
+                            end
+                            obj(iter,:)={coef(ieqtn,endo_var,ilag),0};
+                        end
+                    end
+                end
+            end
+            % trim
+            obj=obj(1:iter,:);
         end
     end
 end
