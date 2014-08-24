f0565009cd37d30487c94b5a754da3421339317d
jmaih <junior.maih@gmail.com>
2014-08-24 21:25:41 +0200

    Decomposed the estimate method

--- classes/models/@rise_generic/estimate.m
+++ classes/models/@rise_generic/estimate.m
@@ -56,15 +56,7 @@ for jj=1:nobj
     end
 end
 
-optim_options=obj(1).options.optimset;
-optimizer=obj(1).options.optimizer;
-hessian_type=obj(1).options.hessian_type;
 estim_start_from_mode=obj(1).options.estim_start_from_mode;
-estim_parallel=obj(1).options.estim_parallel;
-estim_blocks=obj(1).options.estim_blocks;
-if ~isempty(estim_blocks)
-    estim_blocks=create_estimation_blocks(obj(1),estim_blocks);
-end
 
 % Load the function that computes the likelihood
 % preliminary checks:
@@ -152,129 +144,19 @@ x0=x0(:);
 lb=[obj(1).estimation.priors.lower_bound]; lb=lb(:);
 ub=[obj(1).estimation.priors.upper_bound]; ub=ub(:);
 
-% get the linear restrictions if any. a_func takes a2tilde as input and
-% returns "a", while a2tilde_func takes "a" as input and returns a2tilde.
-% restr_var_data_func takes obj as input and returns a structure with
-% ytilde, Xtilde, which are the transformed y and X given the restrictions.
-% na2tilde<=npar is the number of parameters to estimate
+estim_names=get_estimated_parameter_names(obj);
+% get the linear restrictions if any. basics.a_func takes a2tilde as input
+% and returns "a", while basics.a2tilde_func takes "a" as input and returns
+% a2tilde. 
+% npar_short<=npar is the number of parameters to estimate
 %-----------------------------------------------------------------------
-[a_func,a2tilde_func,restr_var_data_func,npar_short]=setup_linear_restrictions(obj);
+basics=setup_linear_restrictions(obj,estim_names);
 
 npar=size(x0,1);
-if isa(obj,'rfvar') && ...
-        obj.markov_chains.regimes_number==1 && ...
-        obj.options.vp_analytical_post_mode
-    vdata=restr_var_data_func(obj);
-    a_prior=[obj.estimation.priors.prior_mean];
-    a_prior=a_prior(vdata.estim_locs);
-    a_prior=a_prior(:);
-    va_prior=[obj.estimation.priors.prior_stdev];
-    va_prior=diag(va_prior(vdata.estim_locs).^2);
-    a2tilde_prior=a2tilde_func(a_prior);
-    va2tilde_prior=a2tilde_func(va_prior,true);
-    
-    [a2tilde_post,~,resid_post]=vartools.analytic_posterior_mode(...
-        vdata.Xtilde,vdata.ytilde,a2tilde_prior,va2tilde_prior);
-    resid_post=reshape(resid_post,obj.endogenous.number(end),[]);
-    [nvars,nobs]=size(resid_post);
-    a_post=a_func(a2tilde_post);
-    
-    x1=nan(npar,1);
-    x1(vdata.estim_locs)=a_post;
-    vcov=(resid_post*resid_post.')/(nobs-npar_short/nvars);
-    sdev=sqrt(diag(vcov));
-    for irow=1:nvars
-        sig_name=sprintf('sig_%0.0f_%0.0f',irow,irow);
-        sig_pos=strcmp(sig_name,vdata.orig_estim_names);
-        x1(sig_pos)=sdev(irow);
-        for icol=1:irow-1
-            corr_name=sprintf('omg_%0.0f_%0.0f',irow,icol);
-            corr_pos=strcmp(corr_name,vdata.orig_estim_names);
-            x1(corr_pos)=vcov(irow,icol)/(sdev(irow)*sdev(icol));
-        end
-    end
-%     keyboard
-    H=eye(npar);
-    
-    viol=[];
-    funevals=0;
-else
-    [nonlcon,nconst]=reprocess_nonlinear_restrictions(obj(1).parameter_restrictions);
-    % the functions in obj(1).parameter_random_draws_restrictions only check
-    % whether the restrictions are violated or not but do not give the strength
-    % of the violation, which we need in order to apply DEB. So we need to use
-    % the reprocessed form.
-    
-    % initialize the number of function calls
-    %----------------------------------------
-    funevals=0;
-    Nsim=max(1,estim_parallel);
-    % now shorten everything
-    %-----------------------
-    x0=a2tilde_func(x0);
-    x0=[x0,nan(npar_short,Nsim-1)];
-    f0=nan(1,Nsim);
-    lb=a2tilde_func(lb);
-    ub=a2tilde_func(ub);
-    bad=lb>ub;
-    if any(bad)
-        tmp=lb;
-        lb(bad)=ub(bad);
-        ub(bad)=tmp(bad);
-    end
-    % all objects are evaluated at the same point. Without a second argument,
-    % this is exactly what will happen.
-    [~,f0(1),~,retcode0,viol]=big_wrapper(x0(:,1));
-    if retcode0||any(viol>0)
-        % first check constraint violations
-        f0(1)=obj(1).options.estim_penalty;
-    end
-    % the objects are automatically updated and potentially contain crucial
-    % information going forward. In particular, they contain information
-    % about whether the models are stationary or not.
-    
-    if f0(1)<obj(1).options.estim_penalty
-        beg=2;
-    else
-        beg=1;
-    end
-    
-    %% disable those elements
-    warning('off','MATLAB:nearlySingularMatrix')
-    warning('off','MATLAB:illConditionedMatrix')
-    
-    fprintf(1,'%s\n','Looking for good enough start values. Please wait...');
-    for ii=beg:Nsim
-        NotDone=true;
-        iter=0;
-        while NotDone
-            [xtest,ftest,retcode]=utils.estim.generate_starting_point(@big_wrapper);
-            if ftest<obj(1).options.estim_penalty
-                NotDone=false;
-                f0(ii)=ftest;
-                x0(:,ii)=xtest;
-            end
-            iter=iter+1;
-            if iter>=obj(1).options.estim_max_trials
-                error([mfilename,':: No admissible starting value after ',...
-                    int2str(obj(1).options.estim_max_trials),' trials'])
-            else
-                fprintf(1,'%3.0d :: %s\n',iter,utils.error.decipher(retcode));
-            end
-        end
-        disp(['Starting value # ',int2str(ii),' found after ',int2str(iter),' iterations'])
-        ratio=ii/Nsim;
-        fprintf(1,'%s\n',['...', num2str(100*ratio),'% done']);
-    end
-    
-    [x1,f1,H,issue,viol,obj]=big_wrapper(x0,'estimate'); %#ok<ASGLU>
-    % both vector x1 and H here are short: extend them
-    %-------------------------------------------------
-    x1 = a_func(x1);
-    H = a_func(H,true); % the second arguments indicates that it is a covariance term
-    
-    viol=viol(viol>0);
-end
+
+[x1,f1,H,x0,f0,viol,funevals,issue,obj]=find_posterior_mode(obj,x0,lb,ub,basics,...
+    estim_general_restrictions,estim_gen_restr_args);
+
 numberOfActiveInequalities=numel(viol);
 
 % make the hessian positive definite if necessary
@@ -329,167 +211,6 @@ end
 % disp Estimation results
 print_estimation_results(obj);
 
-warning('on','MATLAB:nearlySingularMatrix')
-warning('on','MATLAB:illConditionedMatrix')
-
-    function [x1,f1,H,issue,viol,finalobj]=big_wrapper(x0,action)
-        if nargin<2
-            action='eval';
-            if nargin<1
-                x0=lb+(ub-lb).*rand(npar_short,1);
-            end
-        end
-        violLast=[];
-        xLast=[];
-        viol=[];
-        ngen_restr=[];
-        switch action
-            case 'estimate'
-                PROBLEM_=struct('objective',@fh_wrapper,...
-                    'x0',x0,...
-                    'lb',lb,...
-                    'ub',ub,...
-                    'nonlcon',@nonlcon_with_gradient,... % the nonlinear constraints restrictions take the same inputs as fh_wrapper
-                    'options',optim_options,...
-                    'solver',optimizer);
-                
-                [x1,f1,H,issue]=optimization.estimation_engine(PROBLEM_,hessian_type,estim_blocks);
-            case 'eval'
-                [f1,retcode_0]=fh_wrapper(x0);
-                x1=x0;
-                H=[];
-                issue=retcode_0;
-                viol=nonlcon_with_gradient(x0);
-            otherwise
-                error(['unknown type of action ',action])
-        end
-        finalobj=obj;
-        
-        function [minus_log_post,retcode]=fh_wrapper(xtilde)
-            % this function returns the minimax if there are many objects
-            % evaluated simultaneously
-            
-            % all objects are evaluated at the same point
-            % the reason you want to output the object here is because it potentially
-            % contains crucial information going forward. In particular, it contains
-            % information about whether the model is stationary or not.
-            
-            % expand x before doing anything
-            %-------------------------------
-            x=a_func(xtilde);
-            
-            fval=obj(1).options.estim_penalty*ones(1,nobj);
-            for mo=1:nobj
-                [fval(mo),~,~,~,retcode,obj(mo)]=log_posterior_kernel(obj(mo),x);
-                if retcode
-                    break
-                end
-            end
-            funevals=funevals+1;
-            % Now take the negative for minimization
-            minus_log_post=-min(fval);
-            % nonlinear constraints might be incompatible with blockwise
-            % optimization. In that case, it is better to compute the
-            % restriction violation while evaluating the objective and save
-            % the results to pass on to the optimizer when it calls the
-            % nonlinear constraints.
-            if retcode
-                if isempty(ngen_restr)
-                    % then we still have not found a good starting point
-                    % and therefore, there is no point in looking at the
-                    % constraints.
-                else
-                    violLast=ones(nconst+ngen_restr,1)*realmax/(nconst+ngen_restr);
-                end
-            else
-                violLast=mynonlinear_constraints(x,obj);
-            end
-            % update this element right here, so that it is ready when
-            % calling nonlcon_with_gradient
-            xLast=x;
-        end
-        
-        function viol=mynonlinear_constraints(x,obj) %#ok<INUSL>
-            % this function assumes that the first argument has already
-            % been pushed into the second one... I will need to make sure
-            % that this is actually the case.
-            viol_general=[];
-            viol_simple=[];
-            for iobj=1:nobj
-                vv=nonlcon(obj(iobj).parameter_values);
-                viol_simple=[viol_simple,vv(:)]; %#ok<AGROW>
-                if ~isempty(estim_general_restrictions{iobj})
-                    vv=estim_general_restrictions{iobj}(obj(iobj),estim_gen_restr_args{iobj}{:});
-                    viol_general=[viol_general,vv(:)]; %#ok<AGROW>
-                end
-            end
-            if isempty(ngen_restr)
-                ngen_restr=numel(viol_general);
-            end
-            viol=[viol_simple(:);viol_general(:)];
-        end
-        
-        function [viol,grad]=nonlcon_with_gradient(xtilde)
-            grad=[];
-            % expand x before doing anything
-            %-------------------------------
-            x=a_func(xtilde);
-            if isequal(x,xLast)
-                viol=violLast;
-            else
-                % % % % % % % % % % % %                 warning('evaluating constraints before computing log-posterior ')
-                thisobj=assign_estimates(obj,x);
-                % % % % % % % % % % % %                 thisobj=filter(thisobj);
-                viol=mynonlinear_constraints(x,thisobj);
-            end
-        end
-    end
 end
 
-function [nonlcon,nconst]=reprocess_nonlinear_restrictions(nonlcon)
-% transform the nonlinear constraints. I would like to keep the
-% flexibility of knowning what parameters enter the constraints and
-% so I do not do this in format parameters
-nconst=numel(nonlcon);
-for iconstr=1:nconst
-    % remove semicolon
-    nonlcon{iconstr}=strrep(nonlcon{iconstr},';','');
-    % now remove inequalities
-    cutoff_type={'>=','<=','>','<','='};
-    for itype=1:numel(cutoff_type)
-        cutoff_locs=strfind(nonlcon{iconstr},cutoff_type{itype});
-        if ~isempty(cutoff_locs)
-            cutoff_type=cutoff_type{itype};
-            break
-        end
-    end
-    if ~isempty(cutoff_locs)
-        span=length(cutoff_type);
-        left=nonlcon{iconstr}(1:cutoff_locs-1);
-        right=nonlcon{iconstr}(cutoff_locs+span:end);
-        switch cutoff_type
-            case '>='
-                nonlcon{iconstr}=[right,'-(',left,')-eps;'];
-            case '<='
-                nonlcon{iconstr}=[left,'-(',right,')-eps;'];
-            case '>'
-                nonlcon{iconstr}=[right,'-(',left,');'];
-            case '<'
-                nonlcon{iconstr}=[left,'-(',right,');'];
-            case '='
-                nonlcon{iconstr}=['abs(',left,'-(',right,'))-eps;'];
-        end
-    end
-end
-nonlcon=cell2mat(nonlcon(:)');
-nonlcon=nonlcon(1:end-1);
-if isempty(nonlcon)
-    nonlcon='0';
-    nconst=1;
-end
-nonlcon=str2func(['@(M)[',nonlcon,']']);
-end
-% function rst=push_time_series(C)
-% rst=ts(C{1},C{2},C{3});
-% end
 
