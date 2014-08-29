b10795c3b61e293e58940f32dd650052db9d4da6
jmaih <junior.maih@gmail.com>
2014-08-29 11:01:44 +0200

    separate function writing general restrictions

---
+++ classes/models/@rise_generic/setup_general_restrictions.m
@@ -0,0 +1,17 @@
+function obj=setup_general_restrictions(obj)
+% this can be useful also for checking restrictions such as explosive VARs,
+% etc. and not just restrictions on DSGEs
+nobj=numel(obj);
+for ii=1:nobj
+    if ~isempty(obj(ii).options.estim_general_restrictions)
+        if iscell(obj(ii).options.estim_general_restrictions)
+            obj(ii).general_restrictions_data=...
+                @(x)obj(ii).options.estim_general_restrictions{1}(x,...
+                obj(ii).options.estim_general_restrictions{2:end});
+        else
+            obj(ii).general_restrictions_data=obj(ii).options.estim_general_restrictions;
+        end
+    end
+end
+% estim_general_restrictions=cell(1,nobj);
+
