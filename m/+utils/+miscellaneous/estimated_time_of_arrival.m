eec1a5eb2799218f7fb521e4a9de12333e509c2f
jmaih <junior.maih@gmail.com>
2014-08-31 18:50:29 +0200

    Estimated time of arrival (ETA)

---
+++ m/+utils/+miscellaneous/estimated_time_of_arrival.m
@@ -0,0 +1,10 @@
+function [hrs,mins,secs]=estimated_time_of_arrival(start_clock,percentage_complete)
+if percentage_complete<=0||percentage_complete>1
+    error('second argument must be in (0,1]')
+end
+secs = etime(clock,start_clock)*(1-percentage_complete)/percentage_complete;
+mins = floor(secs/60);
+secs = ceil(secs - 60*mins);
+hrs  = floor(mins/60);
+mins = mins - hrs*60;
+end
