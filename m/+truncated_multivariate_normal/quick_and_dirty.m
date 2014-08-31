4c2c1a1f0a9ce44b7649b6b867a0034cd7573b0b
jmaih <junior.maih@gmail.com>
2014-08-31 18:50:55 +0200

    truncated multivariate normal sampling

---
+++ m/+truncated_multivariate_normal/quick_and_dirty.m
@@ -0,0 +1,15 @@
+function x=quick_and_dirty(mu,CS,lb,ub,Nsim)
+if nargin<5
+    Nsim=1;
+end
+npar=numel(mu);
+
+x=mu(:,ones(1,Nsim))+CS*randn(npar,Nsim);
+lb=lb(:,ones(1,Nsim));
+ub=ub(:,ones(1,Nsim));
+xlow=x<lb;
+x(xlow)=lb(xlow);
+xhigh=x>ub;
+x(xhigh)=ub(xhigh);
+
+end
