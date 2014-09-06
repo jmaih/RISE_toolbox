bf3aeb8bf6b575219275deb6bbef344246d079e5
jmaih <junior.maih@gmail.com>
2014-09-06 16:40:34 +0200

    new utility function

---
+++ m/+utils/+plot/one_figure_rows_columns.m
@@ -0,0 +1,15 @@
+function [r,c]=one_figure_rows_columns(nitems)
+
+r=ceil(sqrt(nitems));
+c=floor(sqrt(nitems));
+odd=true;
+while r*c<nitems
+    if odd
+        c=c+1;
+    else
+        r=r+1;
+    end
+    odd=~odd;
+end
+
+end
