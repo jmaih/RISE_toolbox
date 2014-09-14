279b6c036d33c60a51f07900c0ccdb95b69eca06
jmaih <junior.maih@gmail.com>
2014-09-14 11:24:48 +0200

    cosmetic change

--- m/+parser/translate_auxiliary_names.m
+++ m/+parser/translate_auxiliary_names.m
@@ -1,4 +1,9 @@
 function out=translate_auxiliary_names(names)
+
+% variables with lags
 out=regexprep(names,'(\w+)_AUX_L_(\d+)','$1{-$2}');
+
+% variables with leads
 out=regexprep(out,'(\w+)_AUX_F_(\d+)','$1{+$2}');
+
 end
