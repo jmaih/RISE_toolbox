e9af3e78f92b29bd373bdaaf68be020e46af6c2d
jmaih <junior.maih@gmail.com>
2014-09-16 11:08:53 +0200

    added debugging to a function

--- classes/models/+vartools/parameters_to_matrices.m
+++ classes/models/+vartools/parameters_to_matrices.m
@@ -1,10 +1,13 @@
 function [links,parameter_values]=parameters_to_matrices(param_template,...
-    param_names,numberOfRegimes)
-if nargin<3
-    numberOfRegimes=1;
+    param_names,numberOfRegimes,debug)
+if nargin<4
+    debug=false;
+    if nargin<3
+        numberOfRegimes=1;
+    end
 end
-% link the parameters to the structural matrices
-%-----------------------------------------------
+% link the parameters to the structural matrices : M(i,j)=p(k)
+%-------------------------------------------------------------
 ncell=size(param_template,2);
 links=cell(1,ncell);
 for ii=1:ncell
@@ -17,6 +20,16 @@ for ii=1:ncell
     plist=cellstr(strcat(header,'_',int2str(jj),'_',int2str(kk)));
     ploc=locate_variables(plist,param_names);
     links{ii}=sub2ind(size(themat),jj,kk)+ploc*1i;
+    if debug
+        image_themat=num2cell(themat);
+        for jj=1:numel(links{ii})
+            ll=links{ii}(jj);
+            image_themat{real(ll)}=param_names{imag(ll)};
+        end
+        disp(themat)
+        disp(image_themat)
+        keyboard
+    end
 end
 
 nparams=numel(param_names);
