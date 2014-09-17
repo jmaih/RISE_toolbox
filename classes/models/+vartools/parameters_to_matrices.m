a50b945c46e02c9132aa259b7985593576a2fb46
jmaih <junior.maih@gmail.com>
2014-09-17 19:11:03 +0200

    function updated

--- classes/models/+vartools/parameters_to_matrices.m
+++ classes/models/+vartools/parameters_to_matrices.m
@@ -1,4 +1,4 @@
-function [links,parameter_values]=parameters_to_matrices(param_template,...
+function [links,parameter_values,all_param_names_vec]=parameters_to_matrices(param_template,...
     param_names,numberOfRegimes,debug)
 if nargin<4
     debug=false;
@@ -16,6 +16,7 @@ assert(isequal(param_names,sort(param_names)))
 ncell=size(param_template,2);
 links=cell(1,ncell);
 
+bigimage=param_template(2,:);
 for ii=1:ncell
     themat=param_template{2,ii};
     header=param_template{1,ii};
@@ -26,17 +27,28 @@ for ii=1:ncell
     plist=cellstr(strcat(header,'_',int2str(jj),'_',int2str(kk)));
     ploc=locate_variables(plist,param_names);
     links{ii}=sub2ind(size(themat),jj,kk)+ploc*1i;
+    image_themat=num2cell(themat);
+    for jj=1:numel(links{ii})
+        ll=links{ii}(jj);
+        image_themat{real(ll)}=param_names{imag(ll)};
+    end
+    bigimage{ii}=image_themat;
     if debug
-        image_themat=num2cell(themat);
-        for jj=1:numel(links{ii})
-            ll=links{ii}(jj);
-            image_themat{real(ll)}=param_names{imag(ll)};
-        end
         disp(themat)
         disp(image_themat)
         keyboard
     end
 end
+bigimage=cellfun(@(x)replace_double(x),[bigimage{:}],'uniformOutput',false);
+all_param_names_vec=bigimage(:);
 
 nparams=numel(param_names);
 parameter_values=nan(nparams,numberOfRegimes);
+
+    function y=replace_double(x)
+        y=x;
+        if isa(y,'double')
+            y='louisPergaudBahoyaMaih';
+        end
+    end
+end
