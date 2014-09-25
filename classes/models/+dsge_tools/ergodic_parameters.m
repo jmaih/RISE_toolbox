e5997dff40bedde5179b67b6c7152e04159b81df
jmaih <junior.maih@gmail.com>
2014-09-25 17:40:14 +0200

    missing tilde added
    Raf Wouters

--- classes/models/+dsge_tools/ergodic_parameters.m
+++ classes/models/+dsge_tools/ergodic_parameters.m
@@ -3,12 +3,13 @@ ergodic=true;
 [PAI00,retcode]=initial_markov_distribution(Q,ergodic);
 pp_i=0;
 def_i=[];
-if isempty(def{1})
+do_defs= ~isempty(def{1});
+if do_defs
     def_i=0;
 end
 for ii=1:size(pp,2)
     pp_i=pp_i+PAI00(ii)*pp(:,ii);
-    if ~isempty(def{ii})
+    if do_defs
         def_i=def_i+PAI00(ii)*def{ii};
     end
 end
