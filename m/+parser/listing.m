317352d70cdcb76b45fd1eb274257285f5c73fb1
jmaih <junior.maih@gmail.com>
2014-09-27 12:45:25 +0200

    updated function to receive inputs

--- m/+parser/listing.m
+++ m/+parser/listing.m
@@ -1,12 +1,37 @@
-function xout=listing()
+function xout=listing(varargin)
+% LISTING parses the arguments of a parameter or a variable
 
-xout=struct('name',{},'tex_name',{},... all
-    'is_in_use',{},... exogenous and parameters
-    'governing_chain',{},'is_switching',{},'is_measurement_error',{},'is_trans_prob',{},... parameters
-    'is_log_var',{},... endogenous
-    'is_endogenous',{},... observables
-    'max_lead',{},... variables and parameters
-    'max_lag',{},... variables and parameters
-    'state_id',{},... observables
-	'is_auxiliary',{}... endogenous
-    );
+default_xout={
+    'name','',@(x)isvarname(x)% all
+    'tex_name','',@(x)ischar(x)% all
+    'is_in_use',false,@(x)islogical(x)% all exogenous and parameters
+    'governing_chain','',@(x)isvarname(x)% all parameters
+    'is_switching',false,@(x)islogical(x)% all parameters
+    'is_measurement_error',false,@(x)islogical(x)% all parameters
+    'is_trans_prob',false,@(x)islogical(x)% all parameters
+    'is_log_var',false,@(x)islogical(x)% all endogenous
+    'is_endogenous',false,@(x)islogical(x)% all observables
+    'max_lead',0,@(x)isdouble(x) && isscalar(x) && floor(x)==ceil(x) && x>=0% all variables and parameters
+    'max_lag',0,@(x)isdouble(x) && isscalar(x) && floor(x)==ceil(x) && x<=0% all variables and parameters
+    'state_id',nan,@(x)isdouble(x) && isscalar(x) && floor(x)==ceil(x) && x>=0% all observables
+	'is_auxiliary',false,@(x)islogical(x)% all endogenous
+    };
+    ff=default_xout(:,1).';
+    nff=numel(ff);
+    xout0=cell(1,nff);
+[xout0{1:nff}]=utils.miscellaneous.parse_arguments(default_xout,varargin{:});
+
+xout=struct();
+for ifield=1:nff
+    xout.(ff{ifield})=xout0{ifield};
+end
+
+if nargin
+    if isempty(xout.name)
+        error('must have a name')
+    end
+else
+    xout=xout(1:0);
+end
+
+end
