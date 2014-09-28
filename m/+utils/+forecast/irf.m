6780a6f1a92b3660963a8e0c1c7c6bd62792f03c
jmaih <junior.maih@gmail.com>
2014-09-28 15:11:49 +0200

    provision for historical shocks in irfs

--- m/+utils/+forecast/irf.m
+++ m/+utils/+forecast/irf.m
@@ -1,4 +1,4 @@
-function [irfs,retcode]=irf(y0,T,ss,state_vars_location,which_shocks,options)
+function [irfs,retcode]=irf(y0,T,ss,state_vars_location,which_shocks,det_vars,options)
 
 new_impulse=0;
 
@@ -8,22 +8,31 @@ exo_nbr=numel(which_shocks);
 
 nshocks=sum(which_shocks);
 
+if any(which_shocks & det_vars)
+    error('trying to compute irfs for deterministic shocks')
+end
+
 irfs=zeros(endo_nbr,nlags+options.nsteps,nshocks,options.nsimul);
 
 iter=0;
 
-det_vars=~which_shocks;
-
 retcode=0;
+orig_options=options;
 for ishock=1:exo_nbr
-    if det_vars(ishock)
+    if ~which_shocks(ishock)
         continue
     end
     iter=iter+1;
     shock_id=ishock;
     for isimul=1:options.nsimul
         if ~retcode
-            options.shocks=utils.forecast.create_shocks(exo_nbr,shock_id,det_vars,options);
+            options.shocks=utils.forecast.create_shocks(exo_nbr,shock_id,det_vars,orig_options);
+            % make sure that impulses that are inherited stay on in the
+            % reference simulation. This may imply over-riding the impulse
+            % itself if it happens to be on the path of an inherited shock
+            %--------------------------------------------------------------
+            inherited_shocks=orig_options.shocks~=0;
+            options.shocks(inherited_shocks)=orig_options.shocks(inherited_shocks);
             if ~retcode
                 [sim1,states1,retcode]=utils.forecast.multi_step(y0,ss,T,state_vars_location,options);
                 if ~retcode
@@ -33,7 +42,14 @@ for ishock=1:exo_nbr
                             path2=path1;
                         end
                         options2=options;
+                        % set to 0(new_impulse) the location
+                        % corresponding to the impulse
+                        %------------------------------------------
                         options2.shocks=utils.forecast.replace_impulse(options.shocks,shock_id,options.k_future+1,new_impulse);
+                        % set to 0 the location corresponding to the
+                        % inherited shocks
+                        %------------------------------------------
+                        options2.shocks(inherited_shocks)=0;
                         options2.states=states1;
                         [sim2,~,retcode]=utils.forecast.multi_step(y0,ss,T,state_vars_location,options2);
                         if ~retcode
