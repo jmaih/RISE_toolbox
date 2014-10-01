68e935b884dac10c56441c9a047cc0221e0f4059
jmaih <junior.maih@gmail.com>
2014-10-01 11:26:24 +0200

    safeguard against overflow

--- m/filtering/initial_markov_distribution.m
+++ m/filtering/initial_markov_distribution.m
@@ -7,6 +7,13 @@ retcode=0;
 if ergodic
     A=[eye(nreg)-transpose(q_now_lead);ones(1,nreg)];
     PAI00=A\[zeros(1,nreg),1]';
+    % safeguard against knife-edge cases e.g. PAI(1)=1 and PAI(2)=1e-17
+    %-----------------------------------
+    very_small=abs(PAI00)<1e-9;
+    if any(very_small)
+        PAI00(very_small)=0;
+        PAI00=PAI00/sum(PAI00);
+    end
     if any(isnan(PAI00))||any(PAI00<0)||abs(sum(PAI00)-1)>1e-12
         retcode=308;
     end
