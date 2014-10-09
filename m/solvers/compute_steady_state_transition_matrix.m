d7c0d82efc0ac555713e50857a0f00d3904549a3
jmaih <junior.maih@gmail.com>
2014-10-09 20:25:38 +0200

    Doc(WIP)

--- m/solvers/compute_steady_state_transition_matrix.m
+++ m/solvers/compute_steady_state_transition_matrix.m
@@ -1,5 +1,27 @@
 function [Q,retcode]=compute_steady_state_transition_matrix(trans_mat_func,...
     ss_0,pp0,def_0,exo_nbr)
+
+% compute_steady_state_transition_matrix computes the transition matrix a
+% the steady state
+%
+% Syntax
+% -------
+% ::
+%
+% Inputs
+% -------
+%
+% Outputs
+% --------
+%
+% Description
+% ------------
+%
+% Examples
+% ---------
+%
+% See also: 
+
 s0=1;s1=1;
 xss=zeros(exo_nbr,1);
 ys_=ss_0;
