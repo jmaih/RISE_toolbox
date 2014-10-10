a964a46560f81e2cacdeba6235291e5298f8ddad
jmaih <junior.maih@gmail.com>
2014-10-10 16:13:18 +0200

    Bug corrected in translate restrictions
    Andrew Binning

--- classes/models/@rfvar/translate_restrictions.m
+++ classes/models/@rfvar/translate_restrictions.m
@@ -160,6 +160,10 @@ obj.nonlinear_restrictions=nonlinear_restrictions;
             %--------------------------------------------------------------
             f(isnan(f))=0.5;
         end
+        % The following two lines were added correct by Andrew Binning on
+        % 2014/10/10
+        [~,ord]=sort([Q{2,:},2,'descend']); 
+        Q=Q(:,ord);
     end
     function fill_array()
         for irow=1:nrows
