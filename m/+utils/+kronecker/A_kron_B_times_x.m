d9fb41e91dc548e4db05100a597b40b4aefcd813
jmaih <junior.maih@gmail.com>
2014-10-28 19:46:17 +0100

    cosmetic changes

--- m/+utils/+kronecker/A_kron_B_times_x.m
+++ m/+utils/+kronecker/A_kron_B_times_x.m
@@ -1,4 +1,4 @@
-function z=A_kron_B_times_x(A,B,x,xr,xc)
+function z=A_kron_B_times_x(A,B,x,cols_b,cols_a)
 % H1 line
 %
 % Syntax
@@ -21,11 +21,11 @@ function z=A_kron_B_times_x(A,B,x,xr,xc)
 
 % computes the kron(A,B)*x
 if nargin<4
-    xc=size(A,2);
+    cols_a=size(A,2);
     if nargin<3
-        xr=size(B,2);
+        cols_b=size(B,2);
     end
 end
-z=B*reshape(x,xr,xc)*transpose(A);
+z=B*reshape(x,cols_b,cols_a)*transpose(A);
 z=z(:);
 end
