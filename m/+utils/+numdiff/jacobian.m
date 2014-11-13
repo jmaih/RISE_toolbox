30b72a108dd241aedfe2ac95827a7f98a4ffe699
jmaih <junior.maih@gmail.com>
2014-11-13 19:03:48 +0100

    expanded functionality of jacobian

--- m/+utils/+numdiff/jacobian.m
+++ m/+utils/+numdiff/jacobian.m
@@ -1,18 +1,31 @@
 function J=jacobian(func,x,varargin)
-% H1 line
+% jacobian - computes the jacobian of a function or a vector of functions
 %
 % Syntax
 % -------
 % ::
 %
+%   J=jacobian(func,x,varargin)
+%
 % Inputs
 % -------
+%
+% - **func** [fhandle|cell array]: function or functions to be
+%   differentiated
+%
+% - **x** [n x 1 vector]: Vector of arguments for differentiation
+%
+% - **varargin** : extra arguments of func beyond **x**
 %
 % Outputs
 % --------
 %
+% - **J** [matrix]: Numerical Jacobian of **func** at **x**
+%
 % More About
 % ------------
+%
+% The result of multiple functions is concatenated vertically.
 %
 % Examples
 % ---------
@@ -36,10 +49,27 @@ xxp(diag_terms)=xp;
 xxm=x;
 xxm(diag_terms)=xm;
 
-J=func(xxp,varargin{:})-func(xxm,varargin{:});
-nrows=size(J,1);
-hp=h';
-J=J./hp(ones(nrows,1),:);
+if ~iscell(func)
+    func={func};
+end
+[nrows,ncols]=size(func);
+if ncols>1
+    error('the objective cannot have many columns since outputs are to be concatenated vertically')
+end
+
+J=cell(nrows,1);
+for irow=1:nrows
+    J{irow}=func{irow}(xxp,varargin{:})-func{irow}(xxm,varargin{:});
+    nrows_i=size(J{irow},1);
+    hp=h';
+    J{irow}=J{irow}./hp(ones(nrows_i,1),:);
+end
+
+if nrows==1
+    J=J{1};
+else
+    J=cell2mat(J);
+end
 
 end
 
