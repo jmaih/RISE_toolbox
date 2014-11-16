d0fe38e64938273181b1fa028b8ffcef25f06b2c
jmaih <junior.maih@gmail.com>
2014-11-16 13:58:18 +0100

    routine for convergence to the steady state Kalman

---
+++ m/+utils/+filtering/check_steady_state_kalman.m
@@ -0,0 +1,58 @@
+function [is_steady,oldK]=check_steady_state_kalman(is_steady,K,oldK,options,t,no_more_missing)
+% check_steady_state_kalman - checks whether the covariance matrix has converged
+%
+% Syntax
+% -------
+% ::
+%
+%   [is_steady,oldK]=check_steady_state_kalman(is_steady,K,oldK,options,t,no_more_missing)
+%
+% Inputs
+% -------
+%
+% - **is_steady** [true|false]: flag indicating whether or not the steady
+%   state is reached
+%
+% - **K** [matrix]: Kalman gain
+%
+% - **oldK** [matrix]: old Kalman gain
+%
+% - **options** [struct]: options for the Kalman filter
+%
+% - **t** [integer]: iteration number
+%
+% - **no_more_missing** [integer]: iteration/date at which there are no
+%   more missing observations
+%
+% Outputs
+% --------
+%
+% - **is_steady** [true|false]: flag indicating whether or not the steady
+%   state is reached
+%
+% - **oldK** [matrix]: updated old Kalman gain
+%
+% More About
+% ------------
+%
+% Examples
+% ---------
+%
+% See also: 
+
+% the likelihood affects the values of the updated
+% probabilities. In turn, the updated probabilities affect the
+% values of the predicted probabilities. The predicted
+% probabilities enter the collapsing of the covariances and so,
+% we never reach the steady state in this case. So far I am
+% 100% sure about this. But in order to be 101% sure, I would
+% like to run an example.
+if t>no_more_missing && ~is_steady
+    discrep=max(abs(K(:)-oldK));
+    is_steady=discrep<options.kf_riccati_tol;
+    if options.debug
+        fprintf(1,'iteration %4.0f  discrepancy %4.4f\n',t,discrep);
+    end
+end
+oldK=K(:);
+end
