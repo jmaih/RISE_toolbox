750e8ed16f274f2b5c2402d17caf2b23881f916c
jmaih <junior.maih@gmail.com>
2014-11-17 18:08:24 +0100

    function updated and documented

--- m/+utils/+forecast/load_start_values.m
+++ m/+utils/+forecast/load_start_values.m
@@ -1,80 +1,122 @@
 function v=load_start_values(names,db,date,v)
-% H1 line
+% load_start_values - load the start values for forecasting
 %
 % Syntax
 % -------
 % ::
 %
+%   v=load_start_values(names,db,date)
+%   v=load_start_values(names,db,date,v)
+%
 % Inputs
 % -------
+%
+% - **names** [char|cellstr]: names of variables to load from the database
+%
+% - **db** [ts|struct]: ts time series data
+%
+% - **date** [char|numeric|serial date]: date for which the data are
+%   requested
+%
+% - **v** (optional)[vector,3-dimensional array|[]]: start values. If
+%   provided, it should have the same number of rows as the number of names
 %
 % Outputs
 % --------
 %
+% - **v** [vector,3-dimensional array|[]]: start values
+%
 % More About
 % ------------
+% 
+% - leads are ignored
+%
+% - if a variable is not found in the database, it is initialized at 0 in
+%   the first page and nan in the subsequent pages
 %
 % Examples
 % ---------
 %
-% See also: 
-
-% output arguments
-%   - v : vector of start values with the same number of elements as the
-%   number of names
-% input arguments
-%   - date: string, integer(if annual dates), serial date
-%   - db: ts object or struct with ts objects as fields
-%   - names: string or cellstr
-%   - v (optional): initial vector of starting values. It has to have the
-%   same number of elements as names. if absent, it is initialized with zeros. 
-% example
-% db=ts('1990q1',rand(100,5),{'v1','v2','v3','v4','v5'})
+% db=ts('1990q1',rand(100,5,3),{'v1','v2','v3','v4','v5'})
 % v=utils.forecast.load_start_values({'v4','v5','v5_AUX_L_8{+2}','v5_AUX_F_3'},db,'2011Q4')
+%
+% See also: data_request
 
 if ischar(names)
     names=cellstr(names);
 end
 
 nv=numel(names);
-
-if isa(db,'ts')
-    db=pages2struct(db);
-elseif ~isstruct(db)
+% missing variables will start at zero
+if nargin<4
+    v=[];
+end
+if isempty(v)
+    v=zeros(nv,1);
+end
+if ~isa(db,'ts')&&~isstruct(db)
     error('db must be a ts or a struct with ts fields')
 end
 
-% missing variables will start at zero
-if nargin<4
-    v=zeros(nv,1);
-end
+% first collect the data so as to make the pages consistent
+%-----------------------------------------------------------
+db=ts.collect(db);
+npages=get(db,'NumberOfPages');
+
 if numel(v)~=nv
     error('4th argument size does not match the number of names')
 end
 serial_date=date2serial(date);
 
-dbnames=fieldnames(db);
+dbnames=get(db,'varnames');
+
+% the database may have several pages : conditioning information
+% note that going from 
+%----------------------------------------------------------------
+v=full(v); % avoid problems with sparse
+if npages>1
+    if size(v,3)>1
+        error('it is not allowed to have initial conditions with more than one page when the database has more than one page')
+    end
+    % only pick the first page of v in case v has many pages
+    v=cat(3,v(:,1,1),nan(nv,1,npages-1));
+end
 
 for iv=1:nv
     vname1=names{iv};
-    in_database=isfield(db,vname1);
+    in_database=any(strcmp(vname1,dbnames));
     if in_database
         lead_lag=0;
         found_name=vname1;
     else
         [vname2,lead_lag,in_database]=get_name_and_lead_lag(vname1);
+        % ignore the leads
+        %------------------
         in_database=in_database && lead_lag<0;
         if in_database
             found_name=vname2;
         end
     end
     if in_database
-        data=db.(found_name);
+        data=db(found_name);
         if ~isa(data,'ts')
             error(['field ',found_name,' in db must be a ts'])
         end
         target_date=serial_date+lead_lag;
-        v(iv)=double(data(target_date));
+        tmp=double(data(target_date));
+        v(iv,1,:)=permute(tmp,[2,1,3]);
+    end
+end
+
+% remove the nan pages starting from the end until we hit a page that does
+% not contains nans
+%--------------------------------------------------------------------------
+for ipage=npages:-1:2
+    vlast=v(:,:,end);
+    if all(isnan(vlast))
+        v(:,:,end)=[];
+    else
+        break
     end
 end
 
