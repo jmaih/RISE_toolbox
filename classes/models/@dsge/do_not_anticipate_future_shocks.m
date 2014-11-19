e581edaaa84661aadeef3f79113c380df948ce06
jmaih <junior.maih@gmail.com>
2014-11-19 10:19:22 +0100

    function documented

--- classes/models/@dsge/do_not_anticipate_future_shocks.m
+++ classes/models/@dsge/do_not_anticipate_future_shocks.m
@@ -1,18 +1,29 @@
 function obj=do_not_anticipate_future_shocks(obj)
-% H1 line
+% do_not_anticipate_future_shocks - kills the effects of future shocks
 %
 % Syntax
 % -------
 % ::
 %
+%   obj=do_not_anticipate_future_shocks(obj)
+%
 % Inputs
 % -------
+%
+% - **obj** [dsge|rise]: model object
 %
 % Outputs
 % --------
 %
+% - **obj** [dsge|rise]: model object
+%
 % More About
 % ------------
+%
+% - this function sets to 0 all the impacts of future shocks irrespective
+%   of whether some have a longer horizon than others or not. All this is
+%   triggered in the presence of future shocks, if option
+%   **irf_anticipate** is set to false. 
 %
 % Examples
 % ---------
