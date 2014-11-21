b8e6f87f6762122bcd58a44e382fa7048f0b18c4
jmaih <junior.maih@gmail.com>
2014-11-21 19:33:20 +0100

    vectorized set for dsge models

--- classes/models/@dsge/set.m
+++ classes/models/@dsge/set.m
@@ -4,7 +4,7 @@ function obj=set(obj,varargin)
 % Syntax
 % -------
 % ::
-%   
+%
 %   obj=set(obj,varargin)
 %
 % Inputs
@@ -53,47 +53,54 @@ function obj=set(obj,varargin)
 if isempty(obj)
     obj=set@rise_generic(obj);
     % add other fields here if necessary
-else
-    shock_horizon_id=[];
-    use_disc_id=[];
-    nn=length(varargin);
-    for ii=1:2:nn
-        if strcmp(varargin{ii},'solve_shock_horizon')
-            if nn>ii
-                shock_horizon_id=[ii,ii+1];
-            else
-                shock_horizon_id=ii;
-            end
-        elseif strcmp(varargin{ii},'solve_function_mode')
-            use_disc_id=[ii,ii+1];
-        end
-    end
-    solve_shock_horizon=varargin(shock_horizon_id);
-    solve_function_mode=varargin(use_disc_id);
-    varargin(shock_horizon_id)=[];
-    % do not remove the disc property since that option has to be visible
-    % unlike the shocks horizon. varargin(use_disc_id)=[]; 
-    obj=set@rise_generic(obj,varargin{:});
-    if ~isempty(shock_horizon_id)
-        if numel(shock_horizon_id)==1
-            % display the options
-            error('somethings should be implemented in this case: contact junior.maih@gmail.com')
+    return
+end
+
+nobj=numel(obj);
+
+shock_horizon_id=[];
+use_disc_id=[];
+nn=length(varargin);
+for ii=1:2:nn
+    if strcmp(varargin{ii},'solve_shock_horizon')
+        if nn>ii
+            shock_horizon_id=[ii,ii+1];
         else
-            set_shock_horizon()
+            shock_horizon_id=ii;
         end
+    elseif strcmp(varargin{ii},'solve_function_mode')
+        use_disc_id=[ii,ii+1];
     end
-    if ~isempty(solve_function_mode)
-        solve_function_mode=solve_function_mode{2};
-        swap_routines();
+end
+solve_shock_horizon=varargin(shock_horizon_id);
+solve_function_mode=varargin(use_disc_id);
+varargin(shock_horizon_id)=[];
+% do not remove the disc property since that option has to be visible
+% unlike the shocks horizon. varargin(use_disc_id)=[];
+obj=set@rise_generic(obj,varargin{:});
+if ~isempty(shock_horizon_id)
+    if numel(shock_horizon_id)==1
+        % display the options
+        error('somethings should be implemented in this case: contact junior.maih@gmail.com')
+    else
+        set_shock_horizon()
+    end
+end
+if ~isempty(solve_function_mode)
+    solve_function_mode=solve_function_mode{2};
+    % do this one at a time
+    %-----------------------
+    for ii=1:nobj
+        obj(ii)=swap_routines(obj(ii));
     end
 end
 
-    function swap_routines()
-        routines_names=fieldnames(obj.online_routines);
+    function this=swap_routines(this)
+        routines_names=fieldnames(this.online_routines);
         routines_names=setdiff(routines_names,{'symbolic','likelihood'});
         switch solve_function_mode
             case {'explicit','amateur'}
-                obj.routines=obj.online_routines;
+                this.routines=this.online_routines;
             case {'vectorized','professional'}
                 vectorize_routines()
             case 'disc'
@@ -103,19 +110,19 @@ end
         function vectorize_routines()
             for irout=1:numel(routines_names)
                 r_name=routines_names{irout};
-                obj.routines.(r_name)=utils.code.code2vector(obj.online_routines.(r_name));
+                this.routines.(r_name)=utils.code.code2vector(this.online_routines.(r_name));
             end
         end
         function write_routines_to_disk()
-            MainFolder=obj.options.results_folder;
+            MainFolder=this.options.results_folder;
             routines_dir=[MainFolder,filesep,'routines'];
             for irout=1:numel(routines_names)
                 r_name=routines_names{irout};
                 fname=[r_name,'_',solve_function_mode,'__'];
-                rcode=utils.code.code2file(obj.online_routines.(r_name),fname);
+                rcode=utils.code.code2file(this.online_routines.(r_name),fname);
                 if ~rcode
                     movefile([fname,'.m'],routines_dir,'f')
-                    obj.routines.(r_name)=utils.code.func2fhandle([routines_dir,filesep,fname]);
+                    this.routines.(r_name)=utils.code.func2fhandle([routines_dir,filesep,fname]);
                 end
             end
         end
@@ -126,25 +133,31 @@ end
         % turn into struct if cell
         %---------------------------
         process_cell()
-        if isa(value,'double') && numel(value)==1
-            value=round(abs(value));
-            obj.exogenous.shock_horizon(~obj.exogenous.is_observed)=value;
-        elseif isstruct(value)
-            fields=fieldnames(value);
-            shock_locs=locate_variables(fields,obj.exogenous.name);
-            obs_status=obj.exogenous.is_observed(shock_locs);
-            if any(obs_status)
-                disp(fields(obs_status))
-                error('the shocks above are deterministic and there horizon cannot be set')
+        
+        % this has to be done one at a time
+        %-----------------------------------
+        for iobj=1:nobj
+            if isa(value,'double') && numel(value)==1
+                value=round(abs(value));
+                obj(iobj).exogenous.shock_horizon(~obj(iobj).exogenous.is_observed)=value;
+            elseif isstruct(value)
+                fields=fieldnames(value);
+                shock_locs=locate_variables(fields,obj(iobj).exogenous.name);
+                obs_status=obj(iobj).exogenous.is_observed(shock_locs);
+                if any(obs_status)
+                    disp(fields(obs_status))
+                    error('the shocks above are deterministic and there horizon cannot be set')
+                end
+                for ifield=1:numel(fields)
+                    shock=fields{ifield};
+                    v=round(abs(value.(shock)));
+                    obj(iobj).exogenous.shock_horizon(shock_locs(ifield))=v;
+                end
+            else
+                error('value must be a scalar or a structure')
             end
-            for ifield=1:numel(fields)
-                shock=fields{ifield};
-                v=round(abs(value.(shock)));
-                obj.exogenous.shock_horizon(shock_locs(ifield))=v;
-            end
-        else
-            error('value must be a scalar or a structure')
         end
+        
         function process_cell()
             if iscell(value)
                 if size(value,2)~=2
