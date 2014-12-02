ac727548106270d00666e83b018f8cdfbb2060a0
jmaih <junior.maih@gmail.com>
2014-12-02 12:39:04 +0100

    plot_real_time as a direct routine of ts

--- classes/time_series/@ts/ts.m
+++ classes/time_series/@ts/ts.m
@@ -1,150 +1,135 @@
 classdef ts
     % ts Time series
     %
-    % Constructor
-    % ------------
+    % ts Methods:
     %
-    % - [ts](ts/ts)
+    % acos -   H1 line
+    % acosh -   H1 line
+    % acot -   H1 line
+    % acoth -   H1 line
+    % aggregate -   H1 line
+    % allmean -   H1 line
+    % and -   H1 line
+    % apply -   H1 line
+    % asin -   H1 line
+    % asinh -   H1 line
+    % atan -   H1 line
+    % atanh -   H1 line
+    % automatic_model_selection -   H1 line
+    % bar -   H1 line
+    % barh -   H1 line
+    % boxplot -   H1 line
+    % bsxfun -   H1 line
+    % cat - concatenates time series along the specified dimension
+    % collect -   H1 line
+    % corr -   H1 line
+    % corrcoef -   H1 line
+    % cos -   H1 line
+    % cosh -   H1 line
+    % cot -   H1 line
+    % coth -   H1 line
+    % cov -   H1 line
+    % ctranspose -   H1 line
+    % cumprod -   H1 line
+    % cumsum -   H1 line
+    % decompose_series -   H1 line
+    % describe -   H1 line
+    % display -   H1 line
+    % double -   H1 line
+    % drop -   H1 line
+    % dummy -   H1 line
+    % eq -   H1 line
+    % exp -   H1 line
+    % expanding -   H1 line
+    % fanchart -   H1 line
+    % ge -   H1 line
+    % get -   H1 line
+    % gt -   H1 line
+    % head -   H1 line
+    % hist -   H1 line
+    % horzcat -   H1 line
+    % hpfilter -   H1 line
+    % index -   H1 line
+    % interpolate -   H1 line
+    % intersect -   H1 line
+    % isfinite -   H1 line
+    % isinf -   H1 line
+    % isnan -   H1 line
+    % jbtest -   H1 line
+    % kurtosis -   H1 line
+    % le -   H1 line
+    % log -   H1 line
+    % lt -   H1 line
+    % max -   H1 line
+    % mean -   H1 line
+    % median -   H1 line
+    % min -   H1 line
+    % minus -   H1 line
+    % mode -   H1 line
+    % mpower -   H1 line
+    % mrdivide -   H1 line
+    % mtimes -   H1 line
+    % nan -   H1 line
+    % ne -   H1 line
+    % numel -   H1 line
+    % ones - overloads ones for ts objects
+    % pages2struct -   H1 line
+    % plot -   H1 line
+    % plotyy -   H1 line
+    % plus -   H1 line
+    % power -   H1 line
+    % prctile - Percentiles of a time series (ts)
+    % quantile -   H1 line
+    % rand -   H1 line
+    % randn -   H1 line
+    % range -   H1 line
+    % rdivide -   H1 line
+    % regress -   H1 line
+    % reset_start_date -   H1 line
+    % rolling -   H1 line
+    % sin -   H1 line
+    % sinh -   H1 line
+    % skewness -   H1 line
+    % sort -   H1 line
+    % spectrum -   H1 line
+    % std -   H1 line
+    % step_dummy -   H1 line
+    % subsasgn -   H1 line
+    % subsref -   H1 line
+    % sum -   H1 line
+    % tail -   H1 line
+    % times -   H1 line
+    % transform -   H1 line
+    % transpose -   H1 line
+    % ts - Methods:
+    % uminus -   H1 line
+    % values -   H1 line
+    % var -   H1 line
+    % zeros -   H1 line
     %
-    % Visualization
-    % --------------
+    % ts  Properties:
     %
-    % - [head](ts/head)
-    % - [index](ts/index)
-    % - [describe](ts/describe)
-    % - [display](ts/display)
-    % - [jbtest](ts/jbtest)
-    % - [kurtosis](ts/kurtosis)
-    % - [isfinite](ts/isfinite)
-    % - [isinf](ts/isinf)
-    % - [isnan](ts/isnan)
-    % - [ge](ts/ge)
-    % - [get](ts/get)
-    % - [gt](ts/gt)
-    % - [le](ts/le)
-    % - [lt](ts/lt)
-    % - [max](ts/max)
-    % - [mean](ts/mean)
-    % - [median](ts/median)
-    % - [min](ts/min)
-    % - [mode](ts/mode)
-    % - [ne](ts/ne)
-    % - [quantile](ts/quantile)
-    % - [range](ts/range)
-    % - [skewness](ts/skewness)
-    % - [sum](ts/sum)
-    % - [tail](ts/tail)
-    % - [var](ts/var)
-    % - [std](ts/std)
-    % - [spectrum](ts/spectrum)
-    % - [sort](ts/sort)
-    %
-    % Graphing
-    % ---------
-    %
-    % - [bar](ts/bar)
-    % - [barh](ts/barh)
-    % - [boxplot](ts/boxplot)
-    % - [hist](ts/hist)
-    % - [plot](ts/plot)
-    % - [plotyy](ts/plotyy)
-    %
-    % Calculus
-    % ---------
-    %
-    % - [acos](ts/acos)
-    % - [acosh](ts/acosh)
-    % - [acot](ts/acot)
-    % - [acoth](ts/acoth)
-    % - [aggregate](ts/aggregate)
-    % - [allmean](ts/allmean)
-    % - [apply](ts/apply)
-    % - [asin](ts/asin)
-    % - [asinh](ts/asinh)
-    % - [atan](ts/atan)
-    % - [atanh](ts/atanh)
-    % - [bsxfun](ts/bsxfun)
-    % - [corr](ts/corr)
-    % - [corrcoef](ts/corrcoef)
-    % - [cos](ts/cos)
-    % - [cosh](ts/cosh)
-    % - [cot](ts/cot)
-    % - [coth](ts/coth)
-    % - [cov](ts/cov)
-    % - [cumprod](ts/cumprod)
-    % - [cumsum](ts/cumsum)
-    % - [decompose_series](ts/decompose_series)
-    % - [eq](ts/eq)
-    % - [exp](ts/exp)
-    % - [hpfilter](ts/hpfilter)
-    % - [interpolate](ts/interpolate)
-    % - [intersect](ts/intersect)
-    % - [log](ts/log)
-    % - [minus](ts/minus)
-    % - [mpower](ts/mpower)
-    % - [mrdivide](ts/mrdivide)
-    % - [mtimes](ts/mtimes)
-    % - [plus](ts/plus)
-    % - [power](ts/power)
-    % - [prctile](ts/prctile) Percentiles of a time series
-    % - [rdivide](ts/rdivide)
-    % - [sin](ts/sin)
-    % - [sinh](ts/sinh)
-    % - [transform](ts/transform)
-    % - [times](ts/times)
-    % - [uminus](ts/uminus)
-    %
-    % Lookarounds
-    % ------------
-    % - [pages2struct](ts/pages2struct)
-    % - [subsasgn](ts/subsasgn)
-    % - [subsref](ts/subsref)
-    %
-    % Utilities
-    % ------------
-    %
-    % - [and](ts/and)
-    % - [cat](ts/cat)
-    % - [collect](ts/collect)
-    % - [ctranspose](ts/ctranspose)
-    % - [double](ts/double)
-    % - [drop](ts/drop)
-    % - [dummy](ts/dummy)
-    % - [expanding](ts/expanding)
-    % - [fanchart](ts/fanchart)
-    % - [horzcat](ts/horzcat)
-    % - [nan](ts/nan)
-    % - [numel](ts/numel)
-    % - [ones](ts/ones)
-    % - [rand](ts/rand)
-    % - [randn](ts/randn)
-    % - [regress](ts/regress)
-    % - [reset_start_date](ts/reset_start_date)
-    % - [rolling](ts/rolling)
-    % - [automatic_model_selection](ts/automatic_model_selection)
-    % - [transpose](ts/transpose)
-    % - [zeros](ts/zeros)
-    % - [values](ts/values)
-    % - [step_dummy](ts/step_dummy)
-    %
-    %
-    % properties
-    % -----------
-    %
-    % - [varnames] -
-    % - [start] -
-    % - [finish] -
-    % - [frequency] -
-    % - [NumberOfObservations] -
-    % - [NumberOfPages] -
-    % - [NumberOfVariables] -
+    % varnames -   names of the variables in the database
+    % start - time of the time series
+    % finish -   end time of the time series
+    % frequency - of the time series
+    % NumberOfObservations -   number of observations in the time series
+    % NumberOfPages -   number of pages (third dimension) of the time series
+    % NumberOfVariables -   number of variables in the time series
     properties
+        % names of the variables in the database
         varnames={}
+        % start time of the time series
         start
+        % end time of the time series
         finish
+        % frequency of the time series
         frequency
+        % number of observations in the time series
         NumberOfObservations=0;
+        % number of pages (third dimension) of the time series
         NumberOfPages=0;
+        % number of variables in the time series
         NumberOfVariables=0;
     end
     properties(Hidden)
@@ -156,6 +141,52 @@ classdef ts
         % constructor
         %--------------
         function self=ts(varargin)
+            % ts - constructor for time series objects
+            %
+            % Syntax
+            % -------
+            % ::
+            %
+            %   self=ts() : construct a time series with no observations
+            %   self=ts(start_date,data)
+            %   self=ts(start_date,data,varnames)
+            %   self=ts(start_date,data,varnames,sorting)
+            %   self=ts(start_date,data,varnames,sorting,trailnans)
+            %
+            % Inputs
+            % -------
+            %
+            % - **start_date** [integer|char|serial date] : start date of
+            %   the time series. The following are admitted:
+            %   - annual data : e.g. 1990 or '1990'
+            %   - bi-annual data : e.g. '1990H1'
+            %   - Quarterly data : e.g. '1990Q3'
+            %   - monthly data : e.g. '1990M12'
+            % - **data** [numeric] : the format is nobs x nvars x npages,
+            %   where:
+            %   - ´nobs´ is the number of observations
+            %   - ´nvars´ is the number of variables
+            %   - ´npages´ is the number of pages (3rd dimension)
+            % - **varnames** [char|cellstr] : names of the variables in the
+            %   database
+            % - **sorting** [true|{false}]: sort the columns of data
+            %   according to the alphabetical order of the variable names
+            % - **trailnans** [true|{false}]: keep or remove nans (missing
+            %   observations)
+            %
+            % Outputs
+            % --------
+            %
+            % - **self** [ts] : time series
+            %
+            % More About
+            % ------------
+            %
+            % Examples
+            % ---------
+            %
+            % See also:
+
             vlen=length(varargin);
             if vlen
                 if isa(varargin{1},'ts')
@@ -274,6 +305,7 @@ classdef ts
         %         varargout=line(varargin)
         varargout=plot(varargin)
         varargout=plotyy(varargin)
+        varargout=plot_real_time(varargin)
         % calculus
         %---------
         %         varargout=ar(varargin)
