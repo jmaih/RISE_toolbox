62638da50558d69ebac7adf14b025b7634091f8e
jmaih <junior.maih@gmail.com>
2015-02-28 15:04:42 +0100

    Cosmetic changes

--- classes/models/@rise_generic/set_simulation_initial_conditions.m
+++ classes/models/@rise_generic/set_simulation_initial_conditions.m
@@ -70,6 +70,8 @@ states=[];
 % scale_shocks=1;
 shocks_found=false;
 if ~isempty(simul_historical_data)
+    % no burn-in with historical data
+    %----------------------------------
     obj.options.simul_burn=0;
     if isstruct(simul_historical_data)
         simul_historical_data=ts.collect(simul_historical_data);
