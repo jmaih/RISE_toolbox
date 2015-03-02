ffb6ecb33b6720da04281e3cca3d54c43349ed87
jmaih <junior.maih@gmail.com>
2015-03-02 21:11:00 +0100

    unfinished ireland model moved to development

--- examples/VariousModels/PeterIreland/technology_shocks/Contents.m
+++
@@ -1,3 +0,0 @@
-%-------------------   path: C:\Users\jma\Documents\GitHub\RISE_toolbox\examples\VariousModels\PeterIreland\technology_shocks   -------------------%
-%
-%   examples\VariousModels\PeterIreland\technology_shocks\howto                                           - (No help available)
