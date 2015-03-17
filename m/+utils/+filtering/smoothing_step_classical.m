0d9b01b743f46a2188a4f191036d6da974ef6e78
jmaih <junior.maih@gmail.com>
2015-03-17 19:26:11 +0100

    classical smoothing step for linear models

---
+++ m/+utils/+filtering/smoothing_step_classical.m
@@ -0,0 +1,44 @@
+function [alpha_t]=smoothing_step_classical(T,att,Ptt,Ptplus1,alpha_plus,a_plus)
+
+% smoothing_step - smoothing step
+%
+% Syntax
+% -------
+% ::
+%   
+%   [atT,etat,rlag]=smoothing_step(a,r,K,P,T,R,Z,iF,v)
+%
+% Inputs
+% -------
+%
+% - **att** [vector] : m x 1 updated state
+%
+% - **Ptt** [matrix] : m x m covariance matrix of updated state
+%
+% - **Ptplus1** [matrix] : m x m covariance matrix of filtered state next
+% period
+%
+% - **alpha_plus** [vector] : m x 1 smoothed state next period
+%
+% - **a_plus** [vector] : m x 1 filtered state next period
+%
+% Outputs
+% --------
+%
+% - **alpha_t** [vector] : m x 1 smoothed state
+%
+% More About
+% ------------
+%
+% Examples
+% ---------
+%
+% See also: 
+
+alpha_t=att;
+
+if ~isempty(Ptt)
+    alpha_t=alpha_t+Ptt*T.'*Ptplus1\(alpha_plus-a_plus);
+end
+
+end
