ee8b01c5d958e3145de6fa276b160a320a7991f8
Junior Maih <junior.maih@gmail.com>
2015-05-16 22:31:31 +0200

    different strategy for creating auxiliary variables

--- m/+parser/lead_lag_string.m
+++ m/+parser/lead_lag_string.m
@@ -20,9 +20,9 @@ function add_str=lead_lag_string(lead_or_lag)
 % See also: 
 
 if lead_or_lag>0
-    add_str='_AUX_F_';
+    add_str='LEAD_';
 elseif lead_or_lag<0
-    add_str='_AUX_L_';
+    add_str='LAG_';
 else
     error('input must be strictly positive or strictly negative')
 end
