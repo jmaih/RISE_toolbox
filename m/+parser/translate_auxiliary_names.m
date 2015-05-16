a171c621aba351f6e1876808cfe08ace4fb28e7a
Junior Maih <junior.maih@gmail.com>
2015-05-16 22:35:29 +0200

    Translation of auxiliary names updated

--- m/+parser/translate_auxiliary_names.m
+++ m/+parser/translate_auxiliary_names.m
@@ -22,10 +22,10 @@ function out=translate_auxiliary_names(names)
 
 % variables with lags
 negative_str=parser.lead_lag_string(-1);
-out=regexprep(names,['(\w+)',negative_str,'(\d+)'],'$1{-$2}');
+out=regexprep(names,[negative_str,'(\d+)','_(\w+)'],'$2{-$1}');
 
 % variables with leads
 positive_str=parser.lead_lag_string(+1);
-out=regexprep(out,['(\w+)',positive_str,'(\d+)'],'$1{+$2}');
+out=regexprep(out,[positive_str,'(\d+)','_(\w+)'],'$2{+$1}');
 
 end
