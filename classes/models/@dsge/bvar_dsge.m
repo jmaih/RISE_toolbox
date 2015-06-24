920b6edf0c8cb26b631c8410c727e7f2498d7be4
Junior Maih <junior.maih@gmail.com>
2015-06-24 15:30:29 +0200

    documentation of bvar_dsge

--- classes/models/@dsge/bvar_dsge.m
+++ classes/models/@dsge/bvar_dsge.m
@@ -1,4 +1,42 @@
 function [obj,retcode]=bvar_dsge(obj,varargin)
+% bvar_dsge -- intermediary file for computing key elements for dsge-var
+%
+% Syntax
+% -------
+% ::
+%
+%   [obj,retcode]=bvar_dsge(obj,varargin)
+%
+% Inputs
+% -------
+%
+% - **obj** [rise|dsge]: model object
+%
+% - **varargin** [?]: ususal dsge options. The most important of which are:
+%   - **dsgevar_lag** [integer|{4}]: number of lags in the VAR
+%   - **dsgevar_constant** [false|{true}]: flag for having a constant in
+%   the VAR 
+%   - **dsgevar_var_regime** [false|{true}]: use the VAR in simulations.
+%   Otherwise use the DSGE
+%   - **dsgevar_inner_param_uncertainty** [true|{false}]: make random draws
+%   for the parameters around the mode for each simulation
+%
+% Outputs
+% --------
+%
+% - **obj** [rise|dsge]: model object
+%
+% More About
+% ------------
+%
+% - Because the BVAR-DSGE will have fewer variables than the DSGE, in
+% simulations, the missing variables will be stored as 0+1i in time series.
+% This is true for forecast, irf and simulate
+%
+% Examples
+% ---------
+%
+% See also:
 
 if isempty(obj)
     obj=struct('dsgevar_lag',4,... # lags
