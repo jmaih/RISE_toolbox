f2ac4594fc2c67f858374c57614c19f140225e6f
Junior Maih <junior.maih@gmail.com>
2015-06-29 00:04:17 +0200

    example update

--- examples/MarkovSwitching/GavinKeenRichterThrockmorton/howto.m
+++ examples/MarkovSwitching/GavinKeenRichterThrockmorton/howto.m
@@ -7,10 +7,6 @@ clc
 gdexo=rise('globdynzlb','rise_flags',struct('exogenous_switching',true));
 gdexo.legend='exo_switch';
 
-% gdendo=rise('globdynzlb','rise_flags',struct('exogenous_switching',false));
-% gdendo.legend='endo_switch';
-% % 
-% gd=[gdexo,gdendo];
 %% solve the model
 
 gdexo=solve(gdexo);
@@ -21,8 +17,8 @@ print_solution(gdexo)
 
 %% compute irfs
 
-myirfs=irf(gdexo,'irf_risk',0,'irf_periods',20);
-mygirfs=irf(gdexo,'irf_type','girf','irf_periods',10,'irf_risk',0);
+myirfs=irf(gdexo,'irf_periods',20);
+mygirfs=irf(gdexo,'irf_type','girf','irf_periods',10,'irf_regime_specific',false);
 %% plot irfs
 endo_names=gdexo.endogenous.name;
 endo_texnames=gdexo.endogenous.tex_name;
@@ -36,19 +32,12 @@ for ishock=1:numel(shock_names)
     shock=shock_names{ishock};
     figure('name',['impulse responses to a ',shock_texnames{ishock}])
     for ivar=1:numel(varlist)
-        vname=varlist{ivar};  
+        vname=varlist{ivar};
         subplot(3,3,ivar)
         plot(myirfs.(shock).(vname),'linewidth',2)
         if ivar==1
-        legend('normal','zlb')
+            legend('normal','zlb')
         end
-        if ishock==1 && ivar==1
-            nobs=myirfs.(shock).(vname).NumberOfObservations;
-            ssbench=ts.ones(0,nobs,1);
-        end
-        hold on
-        plot(full(gdexo.solution.ss{1}(vlocs(ivar)))*ssbench,...
-            'linewidth',2,'color',[1 0 0])
         title([vtexNames{ivar},' (on ',shock_texnames{ishock},')'])
     end
 end
@@ -59,17 +48,11 @@ for ishock=1:numel(shock_names)
     figure('name',['Generalized impulse responses to a ',shock_texnames{ishock}])
     for ivar=1:numel(varlist)
         vname=varlist{ivar};
-        ssvar=full(gdexo.solution.ss{1}(vlocs(ivar)));  
         subplot(3,3,ivar)
-        plot(ssvar+mygirfs.(shock).(vname),'linewidth',2),
+        plot(mygirfs.(shock).(vname),'linewidth',2),
         if ishock==1 && ivar==1
             nobs=mygirfs.(shock).(vname).NumberOfObservations;
-            ssbench=ts.ones(0,nobs,1);
         end
-        hold on
-        plot(ssvar*ssbench,...
-            'linewidth',2,'color',[1 0 0])
-        title([vtexNames{ivar},' (on ',shock_texnames{ishock},')'])
     end
 end
 %% we can also solve the model using a 2nd-order perturbation
@@ -82,7 +65,3 @@ print_solution(ldzlb2)
 
 %% select the variables to print
 print_solution(ldzlb2,{'C','I','K','N','PAI','PSI','R','RK'})
-
-%% print the solution in a more compact form
-print_solution(ldzlb2,{'C','I','K','N','PAI','PSI','R','RK'},true)
-
