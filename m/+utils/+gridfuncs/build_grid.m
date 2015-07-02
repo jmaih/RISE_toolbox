9dca226f7d93fc85c7d500eb31ff348f7c4ac59e
Junior Maih <junior.maih@gmail.com>
2015-07-02 23:51:45 +0200

    added vectorization to build_grid

--- m/+utils/+gridfuncs/build_grid.m
+++ m/+utils/+gridfuncs/build_grid.m
@@ -1,4 +1,4 @@
-function R=build_grid(R,vp)
+function R=build_grid(R,vp,vectorized)
 % H1 line
 %
 % Syntax
@@ -18,17 +18,26 @@ function R=build_grid(R,vp)
 % ---------
 %
 % See also: 
-
+if nargin<3
+    vectorized=true;
+end
 
 % see also mygrid
 
 [rg,cg]=size(R);
 rg_star=max(rg,1);
 
-Ip=repmat(transpose(1:vp),1,rg_star);
-G0=nan(rg_star*vp,cg);
-for ii=1:rg
-    G0((ii-1)*vp+1:ii*vp,:)=R(ii*ones(vp,1),:);
+if vectorized
+    Ip=(1:vp).';
+    Ip=Ip(:,ones(rg_star,1));
+    ii_=ones(vp,1)*(1:rg);
+    R=[R(ii_(:),:),Ip(:)];
+else
+    Ip=repmat(transpose(1:vp),1,rg_star);
+    G0=zeros(rg_star*vp,cg);
+    for ii=1:rg
+        G0((ii-1)*vp+1:ii*vp,:)=R(ii*ones(vp,1),:);
+    end
+    R=[G0,Ip(:)];
 end
-R=[G0,Ip(:)];
 end
