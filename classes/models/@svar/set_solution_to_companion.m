e81873d19b9afc2e330224043f91e569fa764629
Junior Maih <junior.maih@gmail.com>
2015-07-07 16:01:53 +0200

    added fix for svar with deterministic variables

--- classes/models/@svar/set_solution_to_companion.m
+++ classes/models/@svar/set_solution_to_companion.m
@@ -41,9 +41,15 @@ nrows=obj.nlags*endo_nbr;
 steady_state=cell(1,reg_nbr);
 for ireg=1:reg_nbr
     steady_state{ireg}=repmat(obj.solution.ss{ireg},obj.nlags,1);
+    % constant is at the end according to vartools.set_y_and_x
+    %---------------------------------------------------------
+    det_terms=A{ireg}(:,nrows+1:end-obj.constant);
     A{ireg}=[A{ireg}(:,1:nrows);
         eye(cc),zeros(cc,endo_nbr)];
-    B{ireg}=[B{ireg};
+    tmp=zeros(obj.endogenous.number,exo_nbr);
+    tmp(:,obj.exogenous.is_observed)=det_terms;
+    tmp(:,~obj.exogenous.is_observed)=B{ireg};
+    B{ireg}=[tmp;
         zeros(cc,exo_nbr)];
 end
 
