340ce7cfec47983810c67405620d423fd221f3f6
jmaih <junior.maih@gmail.com>
2015-07-30 16:23:00 +0200

    deleted old entry for sparam

--- classes/models/@dsge/prepare_transition_routine.m
+++ classes/models/@dsge/prepare_transition_routine.m
@@ -24,11 +24,11 @@ if obj.is_endogenous_switching_model
     M=obj.parameter_values;
     % transition matrix should be invariant. And so, hopefully, the first
     % argument to Q{2} could be the updated data in any state.
-    % order of the input arguments is y,x,ss,param,sparam,def,s0,s1 
+    % order of the input arguments is y,x,ss,param,def,s0,s1 
     % remaining arguments of Q{2} after the first one
     %-------------------------------------------------
     defs=mean([obj.solution.definitions{:}],2);
-    Vargs={[],obj.solution.ss{1},mean(M,2),[],defs,[],[]};%Q{3}={[],obj.solution.ss{1},mean(M,2),[],[],[],[]};
+    Vargs={[],obj.solution.ss{1},mean(M,2),defs,[],[]};%Q{3}={[],obj.solution.ss{1},mean(M,2),[],[],[],[]};
     Qfunc=memoizer(obj.routines.transition_matrix,Vargs{:});
 else
     Qfunc=prepare_transition_routine@rise_generic(obj);
