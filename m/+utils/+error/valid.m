41bee054235ded838a7b1ac978dd623aa0fd94db
jmaih <junior.maih@gmail.com>
2015-08-01 15:52:07 +0200

    added check for tsparse class

--- m/+utils/+error/valid.m
+++ m/+utils/+error/valid.m
@@ -24,6 +24,8 @@ if iscell(x)
     flag=all(cellfun(@(x)utils.error.valid(x),x));
 elseif isa(x,'double')
     flag=isreal(x) && ~any(isnan(x(:))) && ~any(isinf(x(:)));
+elseif isa(x,'tsparse')
+    flag=utils.error.valid(x.v);
 else
     error(['class ',class(x),' not a valid type for checking validity'])
 end
