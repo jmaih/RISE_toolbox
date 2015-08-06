4e7935b26bb450f0889e47b95662d99a383564e2
jmaih <junior.maih@gmail.com>
2015-08-06 18:27:30 +0200

    kernel density estimation: efficiency update

--- m/+distributions/multivariate_kernel_density.m
+++ m/+distributions/multivariate_kernel_density.m
@@ -1,4 +1,4 @@
-function [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X,Hi_dHiOvern)
+function [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,F,X,Hi_dHiOvern)
 % multivariate_kernel_density -- multivariate kernel density estimation
 %
 % Syntax
@@ -15,13 +15,11 @@ function [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X,Hi_d
 %
 %   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex)
 %
-%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex)
+%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,F)
 %
-%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F)
+%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,F,X)
 %
-%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X)
-%
-%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X,Hi_dHi)
+%   [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,F,X,Hi_dHi)
 %
 % Inputs
 % -------
@@ -45,16 +43,13 @@ function [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X,Hi_d
 %
 % - **findex** [cell|empty]: coordinates to be filled in F below
 %
-% - **xindex** [empty|vector]: d-uple combination of grid points in each
-% direction
-%
 % - **F** [empty|matrix]: kernel density
 %
 % - **X** [empty|cell]: each cell contains a 1 x N vector of coordinates
 % for the observations in each dimension
 %
-% - **Hi_dHiOvern** [cell]: placeholder for the inverse of the bandwith matrix
-% and the determinant of the same inverse divided by n.
+% - **Hi_dHiOvern** [cell]: placeholder for the inverse of the bandwith
+% matrix and the determinant of the same inverse divided by n.
 %
 % Outputs
 % --------
@@ -71,25 +66,27 @@ function [F,X]=multivariate_kernel_density(X0,N,kernel,id,findex,xindex,F,X,Hi_d
 % either mesh, surf or any suitable 3-D plotting function as:
 %   - surf(X{1},X{2},F)
 %   - mesh(X{1},X{2},F)
-%   
+%
 % Examples
 % ---------
 %
 % See also: kernel_density
 
-% Sources: 
+% Sources:
 % - Wikipedia : 3.6 Multivariate Kernel Density Estimation
 % - David W. Scott, Stephan R. Sain (): "Multi-dimensional Density
 % Estimation", http://www.stat.rice.edu/~scottdw/ss.nh.pdf
+% - wikipedia: https://en.wikipedia.org/wiki/Kernel_(statistics)
 
-if ~iscell(X0)
-    lb=min(X0,[],2);
-    ub=max(X0,[],2);
-    X0={X0
-        [lb,ub]};
+lb=[];
+ub=[];
+if iscell(X0)
+    lb=X0{2}(:,1);
+    ub=X0{2}(:,2);
+    X0=X0{1};
 end
 
-[d,n]=size(X0{1});
+[d,n]=size(X0);
 if isscalar(N) && d>1
     N=N*ones(1,d);
 end
@@ -98,23 +95,21 @@ if numel(N)~=d
 end
 N=N(:).';
 matlab_plot_ready=true;
-kernel_types={'cosine','epanechnikov','normal','triangular','triweight',...
-    'uniform'};
-if nargin<9
+non_truncated_kernels={'normal','gaussian','logistic','silverman'};
+kernel_types=[non_truncated_kernels,{'cosine','epanechnikov','biweight',...
+    'quartic','triangular','triweight','tricube','uniform'}];
+if nargin<8
     Hi_dHiOvern=[];
-    if nargin<8
+    if nargin<7
         X=[];
-        if nargin<7
+        if nargin<6
             F=[];
-            if nargin<6
-                xindex=[];
-                if nargin<5
-                    findex=[];
-                    if nargin<4
-                        id=[];
-                        if nargin<3
-                            kernel=[];
-                        end
+            if nargin<5
+                findex=[];
+                if nargin<4
+                    id=[];
+                    if nargin<3
+                        kernel=[];
                     end
                 end
             end
@@ -122,93 +117,127 @@ if nargin<9
     end
 end
 
-populate()
+if isempty(id)
+    populate()
+end
 
-xlow=X0{2}(id,1);
-xhigh=X0{2}(id,2);
-
-X{id}=linspace(xlow,xhigh,N(id));
-
+X00=X0;
+x0_id=X0(id,:);
+x_id=X{id};
 for ii=1:N(id)
     findex{id}=ii;
-    xindex(id)=X{id}(ii);
+    X00(id,:)=x_id(ii)-x0_id;
     if id==d
         if matlab_plot_ready
             % somehow I need to do this so that things are not confused for
             % the surf and mesh functions later on
             flipped_findex=fliplr(findex);
-            F(flipped_findex{:})=mvkd();
+            F(flipped_findex{:})=mvkd____();
         else
-            F(findex{:})=mvkd();
+            F(findex{:})=mvkd____();
         end
     else
-        [F,X]=multivariate_kernel_density(X0,N,kernel,id+1,findex,xindex,...
-            F,X,Hi_dHiOvern);
+        [F,X]=multivariate_kernel_density(X00,N,kernel,id+1,findex,F,X,...
+            Hi_dHiOvern);
     end
 end
 
     function populate()
-        if isempty(Hi_dHiOvern)
-            %     h0=1;
-            %     H=h0*eye(d);
-            %     H=n^(-1/(d+4))*cov(X0.')^-.5;
-            H=n^(-1/(d+4))*cov(X0{1}.')^.5;
-            dHi_n=1/det(H)*1/n;
-            Hi=inv(H);
-            Hi_dHiOvern={Hi,dHi_n};
+        % bandwith
+        %------------
+        %     h0=1;
+        %     H=h0*eye(d);
+        %     H=n^(-1/(d+4))*cov(X0.')^-.5;
+        H=n^(-1/(d+4))*cov(X0.')^.5;
+        dHi_n=1/det(H)*1/n;
+        Hi=inv(H);
+        Hi_dHiOvern={Hi,dHi_n};
+        % grid points
+        %------------
+        if isempty(lb)
+            lb=min(X0,[],2);
+            ub=max(X0,[],2);
         end
-        if isempty(X),X=cell(1,d); end
-        if isempty(F)
-            if d==1
-                F=zeros(1,N);
+        X=cell(1,d);
+        for ivar=1:d
+            X{ivar}=linspace(lb(ivar),ub(ivar),N(ivar));
+        end
+        % density
+        %--------
+        if d==1
+            F=zeros(1,N);
+        else
+            if matlab_plot_ready
+                F=zeros(fliplr(N));
             else
-                if matlab_plot_ready
-                    F=zeros(fliplr(N));
-                else
-                    F=zeros(N);
-                end
+                F=zeros(N);
             end
         end
-        if isempty(xindex),xindex=zeros(d,1); end
-        if isempty(findex),findex=cell(1,d); end
-        if isempty(id),id=1; end
+        % indexing of density
+        %--------------------
+        findex=cell(1,d);
+        % variable index
+        %----------------
+        id=1;
+        % kernel density type
+        %---------------------
         if isempty(kernel),kernel='normal'; end
         if ~ismember(kernel,kernel_types)
             disp(kernel_types)
             error('kernel should be one of the above')
         end
     end
-    function f=mvkd()
-        xx_h=xindex(:,ones(1,n))-X0{1};
-%         xx_h=bsxfun(@minus,xindex(:),X0{1});
-        xx_h=Hi_dHiOvern{1}*xx_h;
+    function f=mvkd____()
+        xx_h=Hi_dHiOvern{1}*X00;
         %(1/n)*(1/det(H))*sum()
-        f=Hi_dHiOvern{2}*sum(kappa(xx_h));
+        f=kappa(xx_h);
+        f=Hi_dHiOvern{2}*sum(f);
         function k=kappa(u)
-            nstar=cleanup();
+            if ~any(strcmp(kernel,non_truncated_kernels))
+                nstar=[];
+                cleanup()
+            end
             switch kernel
-                case 'normal'
-                    k=1/((2*pi)^(d/2))*exp(-0.5*sum(u.*u,1));
-                case 'epanechnikov'
-                    k=(3/4)^d*prod((1-u).^2,1);
-                case 'triangular'
-                    k=prod(1-abs(u));
-                case 'triweight'
-                    k=(35/32)^d*prod((1-u.^2).^3,1);
                 case 'uniform'
                     k=(1/2)^d*ones(1,nstar);
+                case 'triangular'
+                    k=1-abs(u);
+                    k=prod(k,1);
+                case 'epanechnikov'
+                    k=(3/4)*(1-u.^2);
+                    k=prod(k,1);
+%                 case 'spherical_epanechnikov'
+%                     prop=1;
+%                     k=sum(u.*u,1);
+%                     k=k(k<=1);
+%                     k=prop*(1-k);
+                case {'quartic','biweight'}
+                    k=(15/16)*(1-u.^2).^2;
+                    k=prod(k,1);
+                case 'triweight'
+                    k=(35/32)*(1-u.^2).^3;
+                    k=prod(k,1);
+                case 'tricube'
+                    k=(70/81)*(1-u.^3).^3;
+                    k=prod(k,1);
+                case {'normal','gaussian'}
+                    k=1/((2*pi)^(0.5*d))*exp(-0.5*sum(u.*u,1));
                 case 'cosine'
-                    k=(pi/4)^d*prod(cos((pi/2)*u),1);
-                otherwise
+                    k=(0.25*pi)*cos(0.5*pi*u);
+                    k=prod(k,1);
+                case 'logistic'
+                    k=1./(exp(u)+2+exp(-u));
+                    k=prod(k,1);
+                case 'silverman'
+                    k=abs(u)/sqrt(2);
+                    k=0.5*exp(-k)*sin(k+0.25*pi);
+                    k=prod(k,1);
             end
-            function nstar=cleanup()
-                if ~strcmp(kernel,'normal')
-                    bad=any(abs(u)>1,1);
-                    u(:,bad)=[];
-                end
+            function cleanup()
+                bad=any(abs(u)>1,1);
+                u(:,bad)=[];
                 nstar=size(u,2);
             end
-            
         end
     end
 end
