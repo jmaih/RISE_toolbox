56c443662ae2954261e0d3c908438b906d78394f
jmaih <junior.maih@gmail.com>
2015-08-25 19:27:52 +0200

    cosmetic and efficiency

--- classes/models/@rise_generic/draw_parameter.m
+++ classes/models/@rise_generic/draw_parameter.m
@@ -1,5 +1,5 @@
 function [draw,obj]=draw_parameter(obj,simulation_folder)
-% draw_parameter - random parameter draws for RISE model objects. 
+% draw_parameter - random parameter draws for RISE model objects.
 %
 % Syntax
 % -------
@@ -36,7 +36,7 @@ function [draw,obj]=draw_parameter(obj,simulation_folder)
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 if isempty(obj)
     draw=struct();
@@ -44,54 +44,57 @@ if isempty(obj)
 end
 
 if nargin<2
+    simulation_folder=[];
+end
+if isempty(simulation_folder)
     simulation_folder=obj.folders_paths.simulations;
 end
 
 pnames={obj.estimation.priors.name};
 if ischar(simulation_folder) && ismember(simulation_folder,{'mode','prior'})
-        % this case might not work well if we have markov chains with more
-        % than 2 states. the recenter function is not enough in that case.
-        % One also needs to find a way to make sure that all the drawn
-        % elements in the dirichlet parameters, say, sum to 1. Also, the
-        % use of the recenter function in this case might be called into
-        % question since only the parameters that fall outside their
-        % boundaries are "recentered" 
-        n=numel(obj.estimation.priors);
-        lb=vertcat(obj.estimation.priors.lower_bound);
-        ub=vertcat(obj.estimation.priors.upper_bound);
-        switch simulation_folder
-            case 'mode'
-                [cc,pp]=chol(obj.estimation.posterior_maximization.vcov);
-                if pp
-                    error([mfilename,':: covariance matrix of estimated parameters not positive definite'])
-                end
-                xmode=obj.estimation.posterior_maximization.mode;
-                draw=xmode+transpose(cc)*randn(n,1);
-            case 'prior'
-                distribs={obj.estimation.priors.prior_distrib};
-                % replace the dirichlet with the beta
+    % this case might not work well if we have markov chains with more
+    % than 2 states. the recenter function is not enough in that case.
+    % One also needs to find a way to make sure that all the drawn
+    % elements in the dirichlet parameters, say, sum to 1. Also, the
+    % use of the recenter function in this case might be called into
+    % question since only the parameters that fall outside their
+    % boundaries are "recentered"
+    n=numel(obj.estimation.priors);
+    lb=vertcat(obj.estimation.priors.lower_bound);
+    ub=vertcat(obj.estimation.priors.upper_bound);
+    switch simulation_folder
+        case 'mode'
+            [cc,pp]=chol(obj.estimation.posterior_maximization.vcov);
+            if pp
+                error([mfilename,':: covariance matrix of estimated parameters not positive definite'])
+            end
+            xmode=obj.estimation.posterior_maximization.mode;
+            draw=xmode+transpose(cc)*randn(n,1);
+        case 'prior'
+            distribs={obj.estimation.priors.prior_distrib};
+            % replace the dirichlet with the beta
+            %-------------------------------------
+            distribs=strrep(distribs,'dirichlet','beta');
+            udistrib=unique(distribs);
+            draw=nan(numel(distribs),1);
+            for idist=1:numel(udistrib)
+                % the dirichlets are drawn separately
                 %-------------------------------------
-                distribs=strrep(distribs,'dirichlet','beta');
-                udistrib=unique(distribs);
-                draw=nan(numel(distribs),1);
-                for idist=1:numel(udistrib)
-                    % the dirichlets are drawn separately
-                    %-------------------------------------
-                    if strcmp(udistrib{idist},'dirichlet')
-                        continue
-                    end
-                    loc=strcmp(udistrib{idist},distribs);
-                    a=vertcat(obj.estimation.priors(loc).a);
-                    b=vertcat(obj.estimation.priors(loc).b);
-                    [~,~,~,rndfn]=distributions.(udistrib{idist});
-                    draw(loc)=rndfn(a,b);
+                if strcmp(udistrib{idist},'dirichlet')
+                    continue
                 end
-                for id=1:numel(obj.estim_dirichlet)
-                    loc=obj.estim_dirichlet(id).location;
-                    draw(loc)=obj.estim_dirichlet(id).rndfn(1);
-                end
-        end
-        draw=utils.optim.recenter(draw,lb,ub);
+                loc=strcmp(udistrib{idist},distribs);
+                a=vertcat(obj.estimation.priors(loc).a);
+                b=vertcat(obj.estimation.priors(loc).b);
+                [~,~,~,rndfn]=distributions.(udistrib{idist});
+                draw(loc)=rndfn(a,b);
+            end
+            for id=1:numel(obj.estim_dirichlet)
+                loc=obj.estim_dirichlet(id).location;
+                draw(loc)=obj.estim_dirichlet(id).rndfn(1);
+            end
+    end
+    draw=utils.optim.recenter(draw,lb,ub);
 else
     is_saved_to_disk=ischar(simulation_folder);
     if is_saved_to_disk
@@ -102,18 +105,18 @@ else
         end
         W=strrep(W,'.mat','');
         N=numel(W);
-        choice=pick_one_randomly(N);
+        choice=randi(N);
         this_matrix=W{choice};
         tmp=load([simulation_folder,filesep,this_matrix]);
         if ~isfield(tmp,'x')
             error('wrong format for the stored objects to draw from')
         end
         nn=numel(tmp);
-        id=pick_one_randomly(nn);
+        id=randi(nn);
         draw=tmp.(id).x;
     elseif isstruct(simulation_folder)
         N=numel(simulation_folder);
-        choice=pick_one_randomly(N);
+        choice=randi(N);
         draw=simulation_folder(choice).x;
     else
         error('wrong specification of input')
@@ -126,16 +129,8 @@ x=unstransform_estimates(obj,draw{2});
 draw={pnames,x};
 if nargout>1
     % assignin estimates will be faster than calling
-    % obj=set(obj,'parameters',draw); 
+    % obj=set(obj,'parameters',draw);
     obj=assign_estimates(obj,x);
 end
 
-function choice=pick_one_randomly(N)
-
-if N<=0||~isequal(N,ceil(N))
-	error([mfilename,':: input must be a positive integer'])
 end
-probs=1/N*ones(1,N);
-cprob=[0,cumsum(probs)];
-cprob(end)=1;
-choice=find(cprob>rand,1,'first')-1;
