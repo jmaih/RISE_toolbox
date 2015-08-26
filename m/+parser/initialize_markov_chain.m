1e081c6993060b30aaa6d28668bbfc426bd8bc31
jmaih <junior.maih@gmail.com>
2015-08-26 21:00:49 +0200

    clean up file

--- m/+parser/initialize_markov_chain.m
+++ m/+parser/initialize_markov_chain.m
@@ -63,20 +63,3 @@ nmc=struct('name',name,...
     'duration',duration);
 
 end
-
-% function nmc=initialize_markov_chain(name,nstates,endo_flag)
-% if nargin<3
-%     endo_flag=[];
-% end
-% if isempty(endo_flag)
-%     endo_flag=nan;
-% elseif ~islogical(endo_flag)
-%     error('endo_flag must be logical')
-% end
-% state_names=parser.create_state_list(name,nstates);
-% nmc=struct('name',name,...
-%     'number_of_states',nstates,...
-%     'is_endogenous',endo_flag,...
-%     'state_names',{state_names(:).'},...
-%     'state_tex_names',{state_names(:).'});
-% end
