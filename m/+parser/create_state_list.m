4689497ea3477a8bf4bf67de1e471f3a79febc4b
jmaih <junior.maih@gmail.com>
2015-08-27 21:15:54 +0200

    expanded functionality

--- m/+parser/create_state_list.m
+++ m/+parser/create_state_list.m
@@ -19,7 +19,13 @@ function x=create_state_list(name,number)
 %
 % See also: 
 
-x=cellstr(strcat(name,'_',num2str((1:number)')));
+if isscalar(number)
+    number=(1:number).';
+else
+    number=number(:);
+end
+
+x=cellstr(strcat(name,'_',num2str(number)));
 x=cellfun(@(z)z(~isspace(z)),x,'uniformOutput',false);
-x=x(:)';
+x=x(:).';
 end
