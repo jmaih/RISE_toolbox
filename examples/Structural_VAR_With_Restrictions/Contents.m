02dedc5ff0ca29a57c6095ce9b78e0fa8bf8d9b6
jmaih <junior.maih@gmail.com>
2015-08-31 14:44:12 +0200

    deleted an old example
    A better version can be found in Dr Zha's folder

--- examples/Structural_VAR_With_Restrictions/Contents.m
+++
@@ -1,3 +0,0 @@
-%-------------------   path: C:\Users\jma\Documents\GitHub\RISE_toolbox\examples\Structural_VAR_With_Restrictions   -------------------%
-%
-%   examples\Structural_VAR_With_Restrictions\howto                                           - (No help available)
