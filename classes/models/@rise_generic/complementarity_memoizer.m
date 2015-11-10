778f48712babf9013470b77df2f805b8ca4d7ab6
jmaih <junior.maih@gmail.com>
2015-11-10 11:50:07 +0100

    memoization for log vars

--- classes/models/@rise_generic/complementarity_memoizer.m
+++ classes/models/@rise_generic/complementarity_memoizer.m
@@ -33,68 +33,140 @@ function [sep_cf,cf]=complementarity_memoizer(obj)
 
 if ~isfield(obj.routines,'complementarity')||... I do not expect VARs to have this although in theory they could.
         isempty(obj.routines.complementarity)
+    
     routines=[];
+    
 else
+    
     routines=obj.routines.complementarity;
-    def=obj.solution.definitions{1}; %#ok<NASGU>
-    ss=obj.solution.ss{1}; %#ok<NASGU>
-    param=obj.parameter_values(:,1); %#ok<NASGU>
-    % missing inputs
-    %-----------------
-    x=[];sparam=[];s0=1;s1=1; %#ok<NASGU>
+    
     rise_inputs=parser.input_list;
+    
 end
+
 nrout=numel(routines);
+
 hc=nrout>0 && obj.options.simul_honor_constraints;
+
 simul_honor_constraints_through_switch=obj.markov_chains.regimes_number>1 &&...
     obj.options.simul_honor_constraints_through_switch;
+
 if hc && ~(simul_honor_constraints_through_switch||...
         any(obj.exogenous.shock_horizon(:)>0)||...
         ~isempty(obj.options.solve_occbin))
     error('restrictions detected but no anticipatory or switching behavior to satisfy them')
+    
 end
+
 clear obj
 
 sep_cf=[];
+
 cf=[];
+
 if ~hc
+    
     return
+    
 end
+
 dd=cell(1,nrout);
+
 for ii=1:nrout
-    if ii==1
-        % make sure the inputs [EXCEPT THE FIRST] are all here
-        %------------------------------------------------------
-        for inp=2:numel(rise_inputs)
-            eval([rise_inputs{inp},'=',rise_inputs{inp},';'])
-        end
-    end
+    
     % those functions are expected to return scalar values
     %-----------------------------------------------------
     rout_i=func2str(routines{ii});
+    
     right_par=find(rout_i==')',1,'first');
+    
     if ii==1
+        
         header=rout_i(1:right_par);
+        
     end
+    
     % check that all inputs are correct
     %-----------------------------------
     your_inputs=regexp(header(3:end-1),',','split');
+    
     if ~all(strcmp(your_inputs,rise_inputs))
+        
         error('wrong input names')
+        
     end
+    
     ge_=strfind(rout_i,'>=');
+    
     if isempty(ge_)||numel(ge_)>1
+        
         error('">=" missing or too many ">="')
+        
     end
+    
     dd{ii}=rout_i(right_par+1:ge_-1);
+    
 end
+
 dd=cell2mat(strcat(dd,';'));
 
-sep_cf=str2func(['@(',rise_inputs{1},')[',dd(1:end-1),']']);
-cf=str2func(['@(',rise_inputs{1},')all([',dd(1:end-1),']>=0)']);
+dd=dd(1:end-1);
 
-% no need for memoization at this point. all params, defs, ss, x, are
-% indexed as if they were matrices and str2func in that case treats them as
-% variables to be included in the workspace.
+is_log_var=obj.endogenous.is_log_var|obj.endogenous.is_log_expanded;
+
+% inputs and missing inputs
+%---------------------------
+def=obj.solution.definitions{1}; %#ok<NASGU>
+
+ss=obj.solution.ss{1}; %#ok<NASGU>
+
+param=obj.parameter_values(:,1); %#ok<NASGU>
+
+x=[];s0=1;s1=1; %#ok<NASGU>
+
+% load the additional inputs
+%----------------------------
+ninp=numel(rise_inputs);
+
+additional_inputs=cell(1,ninp-1);
+
+for inp=2:ninp
+    
+    additional_inputs{inp-1}=eval(rise_inputs{inp});
+    
+end
+
+[sep_cf, cf]=do_memo(dd, is_log_var,additional_inputs{:});
+
+end
+
+function [sep_cf, cf]=do_memo(dd, is_log_var, varargin)
+
+args=cell2mat(strcat(parser.input_list,','));
+
+sep_cf0=str2func(['@(',args(1:end-1),')[',dd,']']);
+
+cf0=str2func(['@(',args(1:end-1),')all([',dd,']>=0)']);
+
+sep_cf=@engine_sepcf;
+
+cf=@engine_cf;
+
+    function out=engine_sepcf(y)
+        
+        y(is_log_var)=exp(y(is_log_var));
+        
+        out=sep_cf0(y,varargin{:});
+        
+    end
+
+
+    function out=engine_cf(y)
+        
+        y(is_log_var)=exp(y(is_log_var));
+        
+        out=cf0(y,varargin{:});
+        
+    end
 
 end
