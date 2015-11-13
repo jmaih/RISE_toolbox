7d983e11bdb77d760b80ca52ed40f9d0ec248a3b
jmaih <junior.maih@gmail.com>
2015-11-13 17:11:11 +0100

    outsourced system differentiation

--- m/+parser/parse.m
+++ m/+parser/parse.m
@@ -390,7 +390,7 @@ if dictionary.parse_debug
 end
 routines.probs_times_dynamic=parser.burry_probabilities(dynamic.shadow_model,myifelseif);
 [routines.probs_times_dynamic_derivatives,numEqtns,numVars,jac_toc,...
-    original_funcs]=differentiate_system(...
+    original_funcs]=parser.differentiate_system(...
     routines.probs_times_dynamic,...
     dictionary.input_list,...
     wrt,...
@@ -435,7 +435,7 @@ if parameter_differentiation
         end
     end
     [routines.parameter_derivatives,numEqtns,numVars,jac_toc,original_funcs]=...
-        differentiate_system(...
+        parser.differentiate_system(...
         ppdd(dynamic.shadow_model),...
         dictionary.input_list,wrt,1);
     routines.symbolic.parameters={original_funcs,wrt};
@@ -600,48 +600,6 @@ dictionary=parser.dictionary_cleanup(dictionary,dynamic,static,old_endo_names,lo
     end
 end
 
-function [derivs,numEqtns,numVars,jac_toc,original_funcs]=differentiate_system(myfunc,input_list,wrt,order)
-
-numEqtns=numel(myfunc);
-
-myfunc=parser.remove_handles(myfunc);
-
-myfunc=parser.symbolic_model(myfunc,input_list);
-
-% list of symbols
-symb_list=parser.collect_symbolic_list(myfunc,strcat(input_list,'_'));
-% force 's0' and 's1' to enter the list
-state_inputs={'s0','s1'};
-input_list=input_list(:)';
-for ii=1:numel(state_inputs)
-    if ~any(strcmp(symb_list,state_inputs{ii}))
-        symb_list=[symb_list,state_inputs{ii}];
-    end
-end
-% sorting will be useful if we need to debug
-symb_list=sort(symb_list);
-
-args=splanar.initialize(symb_list,wrt);
-numVars=numel(wrt);
-original_funcs=myfunc;
-for ifunc=1:numEqtns
-    [occur,myfunc{ifunc}]=parser.find_occurrences(myfunc{ifunc},symb_list);
-    % re-create the function
-    var_occur=symb_list(occur);
-    argfun=cell2mat(strcat(var_occur,','));
-    myfunc{ifunc}=str2func(['@(',argfun(1:end-1),')',myfunc{ifunc}]);
-    original_funcs{ifunc}=myfunc{ifunc};
-    arg_occur=args(occur);
-    myfunc{ifunc}=myfunc{ifunc}(arg_occur{:});
-end
-
-verbose=false;
-tic
-derivs=splanar.differentiate(myfunc,numVars,order,verbose);
-derivs=splanar.print(derivs,input_list);
-jac_toc=toc;
-
-end
 
 function [wrt,v,locations,siz,order_var,inv_order_var,steady_state_index]=...
     dynamic_differentiation_list(LLI,exo_nbr,pindex)%% partition the endogenous
