cb0d7febff5b6a065908309463027aa59dbd51d9
jmaih <junior.maih@gmail.com>
2015-11-14 12:10:57 +0100

    update of growth database

--- classes/models/@rise_generic/growth_database.m
+++ classes/models/@rise_generic/growth_database.m
@@ -1,8 +1,11 @@
 function db=growth_database(obj,endhist_date,end_sample,growth_type)
 
 if isempty(obj)
+
     db=struct();
+
     return
+
 end
 
 if ~ismember(growth_type,{'zero','steady'})
@@ -24,8 +27,6 @@ last_date=date2serial(end_sample);
 nperiods=numel(starthist_date:last_date);
 
 db=struct();
-dlin=zeros(nperiods,1);
-dlog=ones(nperiods,1);
 nv=obj.endogenous.number;
 span=(1-maxlag:nperiods-maxlag)';
 for iv=1:nv
@@ -38,17 +39,20 @@ for iv=1:nv
     db.(name)=ts(starthist_date,d);
 end
 
-d=dlin;
+d=zeros(nperiods,1);
 for ix=1:sum(obj.exogenous.number)
     name=obj.exogenous.name{ix};
     db.(name)=ts(starthist_date,d);
 end
 
     function d=zero_growth()
+        ssv=ss.(name);
+        gv=g.(name);
+        span_zero=0*span;
         if is_log_var(iv)
-            d=dlog;
+            d=ssv*gv.^span_zero;
         else
-            d=dlin;
+            d=ssv+gv*span_zero;
         end
     end
 
