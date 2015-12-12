3a453d5c75187649b55ddf89542e7ef38ab4e8c9
Junior Maih <junior.maih@gmail.com>
2015-12-12 20:13:19 +0100

    added bsxfun to binary operation for ts

--- classes/time_series/@ts/binary_operation.m
+++ classes/time_series/@ts/binary_operation.m
@@ -19,14 +19,17 @@ function db=binary_operation(db1,db2,op_string)
 %
 % See also: 
 
-operation=str2func(['@(x,y)',op_string,'(x,y)']);
+operation=str2func(['@(x,y)bsxfun(@',op_string,',x,y)']);
 if isa(db1,'ts')
     if isa(db2,'ts')
         [db1,db2]=intersect(db1,db2);
-        if ~isequal(db1.NumberOfVariables,db2.NumberOfVariables)
-            error([mfilename,':: datasets must have same number of columns'])
+        %         if ~isequal(db1.NumberOfVariables,db2.NumberOfVariables)
+        %         end
+        try
+            newdata=operation(db1.data,db2.data);
+        catch
+            error([mfilename,':: mismatch in sizes of datasets'])
         end
-        newdata=operation(db1.data,db2.data);
     elseif isa(db2,'double')
         if isscalar(db2)||isequal(size(db1.data),size(db2))
             newdata=operation(db1.data,db2);
