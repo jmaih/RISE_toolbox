5f56bec3120e8df7a5b8204fb9112b980d3ad824
Junior Maih <junior.maih@gmail.com>
2015-12-24 08:51:28 +0100

    Take care of trends in conditional forecast

---
+++ m/+utils/+forecast/+rscond/separate_terms.m
@@ -0,0 +1,93 @@
+function [Ty,Te,Tsig,C]=separate_terms(T,sstate,state_cols,k,nshocks)
+% SEPARATE_TERMS -- separate terms for conditional forecasting
+%
+% Syntax
+% -------
+% ::
+%
+%   [Ty,Te,Tsig,C]=SEPARATE_TERMS(T,sstate,ny,nx,nshocks,k,h)
+%
+% Inputs
+% -------
+%
+% - **T** [cell array]: solution impact
+%
+% - **sstate** [cell array]: steady state
+%
+% - **state_cols** [vector]: location of state variables excluding shocks
+%
+% - **k** [numeric]: number of forward steps (anticipation)
+%
+% - **nshocks** [numeric]: number of shocks
+%
+% Outputs
+% --------
+%
+% - **Ty** [cell array]: includes square matrices for the impact of
+% endogenous variables
+%
+% - **Te** [cell array]: includes matrices for the impact of shocks
+%
+% - **Tsig** [cell array]: includes vectors summing the impact of
+% uncertainty and the trend
+%
+% - **C** [cell array]: includes vectors summing the impact of
+% uncertainty, the trend and the steady state
+%
+% More About
+% ------------
+%
+% Examples
+% ---------
+%
+% See also:
+
+h=numel(T);
+
+ny=size(T{1},1);
+
+nx=numel(state_cols);
+
+C=cell(1,h);
+
+Ty=cell(1,h);
+
+Te=cell(1,h);
+
+Tsig=cell(1,h);
+
+Iy=eye(ny);
+
+dim1Dist=ny;
+
+dim2Dist=nshocks*ones(1,k+1);
+
+for ireg=1:h
+    
+    Ti=T{ireg};
+    
+    Tx=Ti(:,1:nx);
+    
+    Ty{ireg}=add_zeros(Tx);
+    
+    % merge trend and the uncertainty impact
+    %---------------------------------------
+    Tsig{ireg}=Ti(:,nx+1);
+    
+    Tsig{ireg}=real(Tsig{ireg})+imag(Tsig{ireg});
+    
+    Te{ireg}=mat2cell(Ti(:,nx+2:end),dim1Dist,dim2Dist);
+    
+    C{ireg}=(Iy-Ty{ireg})*sstate{ireg}+Tsig{ireg};
+    
+end
+
+    function Ty=add_zeros(Tx)
+        
+        Ty=zeros(ny);
+        
+        Ty(:,state_cols)=Tx;
+        
+    end
+
+end
--- m/+utils/+forecast/+rscond/stochastic_impact_cumulator.m
+++ m/+utils/+forecast/+rscond/stochastic_impact_cumulator.m
@@ -97,89 +97,150 @@ function [M,ufkst,states,PAI,TT]=stochastic_impact_cumulator(model,y0,nsteps,...
 % altmany at gmail.com
 
 if nargin<6
+    
     states=[];
+    
     if nargin<5
+        
         e_pos=[];
+        
         if nargin<4
+            
             y_pos=[];
+            
         end
+        
     end
+    
 end
 
 if isempty(states)
+    
     states=nan(nsteps,1);
+    
 end
+
 if numel(states)~=nsteps
+    
     error('number of states does not match number of steps')
+    
 end
 
 T=model.T;
+
 [ny,nz]=size(T{1});
+
 sstate=model.sstate;
+
 state_cols=model.state_cols;
+
 if isempty(state_cols)
+    
     state_cols=1:ny;
+    
 end
+
 k=model.k;
+
 Qfunc=model.Qfunc;
 
 h=size(T,2);
+
 nx=numel(state_cols);
+
 nshocks=(nz-(nx+1))/(k+1);
-[C,Ty,Te]=separate_terms();
+
+[Ty,Te,~,C]=utils.forecast.rscond.separate_terms(T,sstate,state_cols,k,nshocks);
+
 % const/y{-1}/shk(0)/shk(1)/.../shk(k)/shk(k+1)/.../shk(k+nsteps-1)
 ProtoR=cell(1,1+1+k+nsteps);
+
 ProtoR{1}=zeros(ny,1);
+
 ProtoR{2}=y0;
+
 ncols=1+1+(k+nsteps)*nshocks;
+
 nconds=numel(y_pos);
+
 R=zeros(nconds*nsteps,ncols);
+
 is_convolute=nargout>4;
+
 if is_convolute
+    
     TT=zeros(nconds*nsteps,ny);
+    
     Tconv=eye(ny);
+    
 end
+
 ufkst=y0(:,ones(1,nsteps+1));
+
 PAI=zeros(h,nsteps);
+
 for jstep=1:nsteps
+    
     % pick the state
     %----------------
     st=pick_a_regime();
+    
     % deal with the constant
     %------------------------
     ProtoR{1}=C{st}+Ty{st}*ProtoR{1};
+    
     % origin
     %--------
     ProtoR{2}=Ty{st}*ProtoR{2};
+    
     % convolution of autoregressive terms
     %-------------------------------------
     if is_convolute
+        
         convolute()
+        
     end
+    
     % shocks
     %--------
     iter=2;
+    
     for icol=1:k+jstep
+        
         iter=iter+1;
+        
         if isempty(ProtoR{iter});
+            
             ProtoR{iter}=zeros(ny,nshocks);
+            
         else
+            
             ProtoR{iter}=Ty{st}*ProtoR{iter};
+            
         end
+        
         which_shock=icol-jstep+1;
+        
         if which_shock>0
+            
             ProtoR{iter}=ProtoR{iter}+Te{st}{which_shock};
+            
         end
+        
     end
+    
     % process all cells up to iter
     %------------------------------
     tmp=cell2mat(ProtoR(:,1:iter));
+    
     R((jstep-1)*nconds+1:jstep*nconds,1:size(tmp,2))=tmp(y_pos,:);
+    
     ufkst(:,jstep+1)=tmp(:,2); % unconditional forecasts
     
     % next period's origin
     %----------------------
     y0=ProtoR{1}+ProtoR{2};
+    
 end
 
 % create rows for the shocks
@@ -189,66 +250,79 @@ S=do_shocks_conditions();
 % format output
 %---------------
 R=sparse(R);
+
 M=struct('R',R(:,3:end),'ufkst',R(:,2),'const',R(:,1),'S',S,...
     'nshocks',nshocks,'ny',ny);
 
     function convolute()
+        
         Tconv=Ty{st}*Tconv;
+        
         TT((jstep-1)*nconds+1:jstep*nconds,:)=Tconv(y_pos,:);
+        
     end
 
     function reg=pick_a_regime()
+        
         if isnan(states(jstep))
+            
             if h==1
+                
                 states(jstep)=1;
+                
                 PAI(:,jstep)=1;
+                
             else
+                
                 Q=Qfunc(y0);
+                
                 if jstep==1
+                    
                     [PAI0,retcode]=initial_markov_distribution(Q,true);
+                    
                     if retcode
+                        
                         warning('ergodic distribution failed...')
+                        
                         PAI0=initial_markov_distribution(Q,false);
+                        
                     end
+                    
                     PAI(:,jstep)=PAI0(:);
+                    
                 else
+                    
                     % draw conditional on yesterday's state
                     PAI(:,jstep)=Q(states(jstep-1),:).';
+                    
                 end
+                
                 cp=cumsum(PAI(:,jstep).');
+                
                 cp=[0,cp];
+                
                 states(jstep)=find(cp>rand,1,'first')-1;
+                
             end
+            
         end
+        
         reg=states(jstep);
-    end
-
-    function [C,Ty,Te]=separate_terms()
-        C=cell(1,h);
-        Ty=cell(1,h);
-        Te=cell(1,h);
-        Iy=eye(ny);
-        dim1Dist=ny;
-        dim2Dist=nshocks*ones(1,k+1);
-        for ireg=1:h
-            Ti=T{ireg};
-            Tx=Ti(:,1:nx);
-            Ty{ireg}=add_zeros(Tx);
-            Tsig=Ti(:,nx+1);
-            Te{ireg}=mat2cell(Ti(:,nx+2:end),dim1Dist,dim2Dist);
-            C{ireg}=(Iy-Ty{ireg})*sstate{ireg}+Tsig;
-        end
-        function Ty=add_zeros(Tx)
-            Ty=zeros(ny);
-            Ty(:,state_cols)=Tx;
-        end
+        
     end
 
     function S=do_shocks_conditions()
+        
         S=speye(nshocks*(k+nsteps));
+        
         e_good=false(nshocks,1);
+        
         e_good(e_pos)=true;
+        
         e_good=e_good(:,ones(1,k+nsteps));
+        
         S=S(e_good(:),:);
+        
     end
+
 end
