c14b9848dd2d9cdf1a52b433e97550e9d45fd11a
jmaih <junior.maih@gmail.com>
2016-03-19 11:05:37 +0100

    robustification of serial conversions

--- classes/time_series/time_frequency_stamp.m
+++ classes/time_series/time_frequency_stamp.m
@@ -1,4 +1,4 @@
-function [stamp,unstamp]=time_frequency_stamp()
+function [stamp,unstamp]=time_frequency_stamp(option)
 % H1 line
 %
 % Syntax
@@ -17,23 +17,55 @@ function [stamp,unstamp]=time_frequency_stamp()
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 % stamps helps put a stamp on the serial numbers such that the frequency is
 % recoverable
 
 % unstamp inverts the stamp above
 
-option=2;
-switch option
-    case 1
-        stamp=@(x)0.01*x;
-        unstamp=@(x)round(100*x);
-    case 2
-        stamp=@(x)1./(1+x);
-        unstamp=@(x)round(1./x-1);
-    otherwise
-        error('case not implemented')
+if nargin==0
+    
+    option=2;
+    
+elseif ~ismember(option,[1,2])
+    
+    error('case not implemented')
+    
 end
 
+stamp=@mystamp;
 
+unstamp=@myunstamp;
+
+    function us=myunstamp(x)
+        
+        if option==1
+            
+            us=round(100*x);
+            
+        elseif option==2
+            
+            us=round(1./x-1);
+            
+            us(isinf(us))=1;
+            
+        end
+        
+    end
+
+    function s=mystamp(x)
+        
+        if option==1
+            
+            s=0.01*x;
+            
+        elseif option==2
+            
+            s=1./(1+x);
+            
+        end
+        
+    end
+
+end
