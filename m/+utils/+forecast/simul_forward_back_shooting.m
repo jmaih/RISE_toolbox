a4f7b98cbc8a67eb93f7b66fa92aa9614a159ea0
jmaih <junior.maih@gmail.com>
2016-03-23 22:00:23 +0100

    disaggregation of multi-step

---
+++ m/+utils/+forecast/simul_forward_back_shooting.m
@@ -0,0 +1,241 @@
+function [sims,myshocks,PAI,retcode]=simul_forward_back_shooting(T,ss,y0,state_vars_location,h,nx,Qfunc,regimes,options)
+% H1 line
+%
+% Syntax
+% -------
+% ::
+%
+% Inputs
+% -------
+%
+% Outputs
+% --------
+%
+% More About
+% ------------
+%
+% Examples
+% ---------
+%
+% See also:
+
+cutoff=-1e-10;
+
+PAI=1;
+
+if h>1
+    
+    error('Forward-back shooting only available for constant-parameter models')
+    
+end
+
+st=1;
+
+xlocs=state_vars_location;
+
+if options.simul_anticipate_zero
+    
+    error('simul_anticipate_zero inconsistent with forward-back shooting')
+    
+end
+
+retcode=0;
+
+model=struct('T',{T},'sstate',{ss},'state_cols',state_vars_location,...
+    'Qfunc',Qfunc,'k',options.k_future,'nshocks',nx);
+
+horizon=options.k_future;
+
+if horizon==0
+    
+    error('Must anticipate the future for forward-back shooting')
+    
+end
+
+sims=y0.y(:,ones(options.nsteps+1,1));
+
+violation=@(x)any(options.sep_compl(x)<cutoff);
+
+% straigthen the shocks (they may be in a wrong order)...
+%--------------------------------------------------------
+myshocks=nan(nx,size(y0.econd.data,2));
+
+myshocks(y0.econd.pos,:)=y0.econd.data(:,:,1);
+
+is_endogenous=options.is_endogenous_exo_vars;
+
+if ~any(is_endogenous)
+    
+    is_endogenous=all(isnan(myshocks),2);
+    
+    if ~any(is_endogenous)
+        
+        error(['Forward-back shooting requires the presence of ',...
+            'exogenous variables to endogenize'])
+        
+    end
+
+end
+
+% add a number of pages for the anticipation
+%--------------------------------------------
+myshocks=myshocks(:,:,ones(1,1+horizon));
+
+myshocks(:,:,2:end)=0;
+
+for istep=2:options.nsteps+1
+    
+    j0=istep-1;
+    
+    shocks=get_shocks(j0);
+    
+    atmp_ss=sims(:,j0)-ss{st};
+    
+    sims(:,j0+1)=ss{st}+T{st}*[atmp_ss(xlocs);0;shocks(:)];
+    
+    start_iter=1;
+    
+    if violation(sims(:,istep))
+        % do a formal forward back shoot
+        [tmp,shocks,start_iter]=forward_back_shoot();
+        
+        if retcode
+            
+            error(decipher(retcode))
+            
+        end
+        
+        sims(:,j0+1)=tmp(:,1);
+        
+    end
+    
+    % replace the updated shocks
+    myshocks(is_endogenous,j0,1:start_iter)=shocks(is_endogenous,1:start_iter);
+    
+end
+
+myshocks=myshocks(:,1:options.nsteps,:);
+
+% Keep history in, it will be removed by the caller
+%--------------------------------------------------
+sims=sims(:,1:end);
+    
+
+    function shocks=get_shocks(j0,keep_nan)
+        
+        if nargin<2
+            
+            keep_nan=false;
+            
+        end
+        
+        limit=min(j0+options.k_future,size(myshocks,2));
+        
+        shocks=myshocks(:,j0:limit);
+        
+        if ~keep_nan
+            
+            shocks(isnan(shocks))=0;
+            
+        end
+        
+        missing=options.k_future+1-size(shocks,2);
+        
+        if missing>0
+            
+            shocks=[shocks,zeros(nx,missing)]; 
+            
+        elseif missing<0
+            
+            error('something quite not right')
+            
+        end
+        
+    end
+
+    function [y1,updated_shocks,start_iter]=forward_back_shoot()
+        
+        is_violation=true;
+        
+        start=j0;
+        
+        shocks_=get_shocks(start,true);
+        
+        start_iter=0;
+        
+        while is_violation && start_iter<horizon+1
+            
+            start_iter=start_iter+1;
+            
+            % set all the shocks beyond start_iter to 0
+            %-------------------------------------------  
+            these_shocks=shocks_; 
+            
+            these_shocks(:,2:end)=0;
+            
+            % set the endogenous shocks to nan over the conditioning period
+            %--------------------------------------------------------------
+            
+            these_shocks(is_endogenous,1:start_iter)=nan;
+            
+            y0_=initial_conditions_frwrd_back_shoot(these_shocks,sims(:,j0),start_iter);
+            
+            % forecast only for the required number of periods
+            opt=utils.miscellaneous.reselect_options(options,@utils.forecast.rscond.forecast);
+            opt.nsteps=start_iter;
+            
+            [updated_shocks,~,PAI,retcode,cfkst]=utils.forecast.rscond.forecast(model,y0_.y,...
+                y0_.ycond,y0_.econd,opt,regimes(1:opt.nsteps));
+            
+            % remove one period of history
+            y1=cfkst(:,2:start_iter+1,:);
+            
+            if retcode
+                
+                return
+                
+            end
+            
+            % The steps up until start_iter should check and so simply
+            % check the next step
+            
+            start=j0+start_iter;
+            
+            % these shocks are
+            these_other_shocks=get_shocks(start);
+            
+            f__=y1(:,end)-ss{st};
+            
+            a_expect=ss{st}+T{st}*[f__(xlocs);0;these_other_shocks(:)];
+            
+            is_violation=violation(a_expect);
+            
+        end
+        
+        if is_violation
+            
+            retcode=701;
+            
+        end
+        
+    end
+
+
+    function y0_=initial_conditions_frwrd_back_shoot(the_shocks,a_start,start_iter)
+        % compute a conditional forecast
+        
+        ycond=y0.ycond.data(:,1:start_iter,1);
+        
+        ycond=struct('data',ycond(:,:,ones(3,1)),'pos',y0.ycond.pos);
+        
+        econd=[the_shocks,zeros(nx,start_iter-1)];
+        
+        econd=struct('data',econd(:,:,ones(3,1)),'pos',1:nx);
+        
+        rcond=struct('data',ones(start_iter,1),'pos',nan);
+        
+        y0_=struct('y',a_start,'ycond',ycond,'econd',econd,'rcond',rcond);
+        
+    end
+
+end
---
+++ m/+utils/+forecast/simul_occbin.m
@@ -0,0 +1,202 @@
+function [sim1,regimes,retcode]=simul_occbin(y0,T,ss,state_vars_location,...
+    options,shocks)
+
+% H1 line
+%
+% Syntax
+% -------
+% ::
+%
+% Inputs
+% -------
+%
+% Outputs
+% --------
+%
+% More About
+% ------------
+%
+% Examples
+% ---------
+%
+% See also:
+
+use_pinv=false;
+is_accelerated=true;
+
+% find the first-order approximation of the system
+%--------------------------------------------------
+
+A0=options.occbin.A0;
+Aplus=A0;
+h=size(A0,3);
+for ireg=1:h
+    Aplus(:,:,ireg)=options.occbin.Gplus01(:,:,ireg,ireg);
+end
+Aminus=options.occbin.Aminus;
+B=options.occbin.B;
+c=options.occbin.c;
+
+ref_state=options.solve_occbin;
+other_state=setdiff(1:h,ref_state);
+if numel(other_state)>1
+    error('occbin can only handle one constraint')
+end
+nstate_vars=numel(state_vars_location);
+
+endo_nbr=size(y0.y,1);
+exo_nbr=size(B,2);
+
+span=options.nsteps+options.burn;
+
+compl=@(x)isempty(options.complementarity)||options.complementarity(x);
+
+% find the solution for the reference regime
+%--------------------------------------------
+[H,G,k]=reference_solution();
+
+retcode=0;
+y0.econd.data=shocks(:,:,ones(3,1));
+
+last_step=0;
+bigt=0;
+Ht0=[];
+Gt0=[];
+kt0=[];
+
+% initialize with nans in order to see if/when simulation breaks down
+sim1=nan(endo_nbr,span);
+regimes=ref_state*ones(span,1);
+do_one_occbin_path()
+
+    function [H,G,k]=reference_solution()
+        H=zeros(endo_nbr);
+        H(:,state_vars_location)=T{ref_state}(:,1:nstate_vars);
+        k=full(T{ref_state}(:,nstate_vars+1));
+        G=full(T{ref_state}(:,nstate_vars+2:end));
+    end
+
+    function do_one_occbin_path()
+        % try and forecast from the ref
+        %------------------------------
+        Tfunc=@(t)T{ref_state};
+        is_viol=forecaster(y0.y,Tfunc);
+        if ~is_viol
+            return
+        end
+        bigt=1;
+        last_step=1;
+        % accelerate things by preparing the longest possible stretch
+        %-------------------------------------------------------------
+        if is_accelerated
+            [Ht0,Gt0,kt0]=resolve_occbin(span-1);
+        end
+        
+        % make assumption about the duration of the violation and retry
+        %---------------------------------------------------------------
+        if last_step<=1
+            y00=y0.y;
+        else
+            y00=sim1(:,last_step-1);
+        end
+        while is_viol && last_step-1+bigt<span
+            [Ht,Gt,kt]=load_solutions(bigt);
+            if retcode
+                % we exit as quickly as possible, it is not possible to
+                % tame the system.
+                bigt=inf;
+            else
+                Tfunc=@(t)[Ht(:,state_vars_location,t),kt(:,t),Gt(:,:,t)];
+                is_viol=forecaster(y00,Tfunc,last_step);
+                if is_viol
+                    bigt=bigt+1;
+                    if last_step<=1
+                        y00=y0.y;
+                    else
+                        y00=sim1(:,last_step-1);
+                    end
+                end
+            end
+        end
+        if is_viol
+            retcode=701;
+        end
+    end
+
+    function [Ht,Gt,kt]=load_solutions(bigt)
+        if is_accelerated
+            last_step_=max(1,last_step);
+            wing=last_step_:span;
+            nwing=numel(wing);
+            stretch=span-bigt:span-1;
+            Ht=H(:,:,ones(nwing,1));Ht(:,:,1:bigt)=Ht0(:,:,stretch);
+            Gt=G(:,:,ones(nwing,1));Gt(:,:,1:bigt)=Gt0(:,:,stretch);
+            kt=k(:,ones(nwing,1));kt(:,1:bigt)=kt0(:,stretch);
+            regimes(last_step_:last_step_+bigt-1)=other_state;
+            regimes(last_step_+bigt:end)=ref_state;
+        else
+            [Ht,Gt,kt]=resolve_occbin(bigt);
+        end
+    end
+
+    function is_viol=forecaster(y00,Tfunc,istart)
+        if nargin<3
+            istart=1;
+        end
+        iter=0;
+        for istep=istart:span
+            iter=iter+1;
+            y00_s=y00-ss{1}; % steady state is the same???
+            state=[y00_s(state_vars_location);1;shocks(:,istep)];
+            sim1(:,istep)=ss{1}+Tfunc(istep-istart+1)*state;
+            y00=sim1(:,istep);
+            if any(~isfinite(y00))||~isreal(y00)
+                retcode=701;
+            end
+            is_viol=~compl(y00);
+            if is_viol||retcode
+                break
+            end
+        end
+        % if the period after the guess does not violate, then the guess
+        % was correct. Update the last step
+        %----------------------------------------------------------------
+        if ~is_viol||(iter>bigt && compl(sim1(:,istart+bigt)))
+            last_step=istep;
+            bigt=0;
+        end
+    end
+
+    function [Ht,Gt,kt]=resolve_occbin(bigt)
+        new_span=span-last_step+1;
+        Ht=zeros(endo_nbr,endo_nbr,new_span);
+        Gt=zeros(endo_nbr,exo_nbr,new_span);
+        kt=zeros(endo_nbr,new_span);
+        
+        t=bigt+1:new_span;
+        Ht(:,:,t)=H(:,:,ones(1,new_span-bigt));
+        Gt(:,:,t)=G(:,:,ones(1,new_span-bigt));
+        kt(:,t)=k(:,ones(1,new_span-bigt));
+        regimes(t+last_step-1)=ref_state;
+        
+        maxed_out=bigt>=new_span;
+        if maxed_out
+            retcode=701;
+        else
+            for t=bigt:-1:1
+                if use_pinv
+                    AHAi=-pinv(Aplus(:,:,other_state)*Ht(:,:,t+1)+...
+                        A0(:,:,other_state));
+                else
+                    AHAi=-(Aplus(:,:,other_state)*Ht(:,:,t+1)+...
+                        A0(:,:,other_state))\eye(endo_nbr);
+                end
+                Ht(:,:,t)=AHAi*Aminus(:,:,other_state);
+                Gt(:,:,t)=AHAi*B(:,:,other_state);
+                kt(:,t)=AHAi*(c(:,other_state)+Aplus(:,:,other_state)*kt(:,t+1));
+                regimes(t+last_step-1)=other_state;
+            end
+        end
+    end
+end
+
