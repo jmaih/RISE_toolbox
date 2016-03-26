626ca4cd5253dc21a9026b44cd721a6a7f894f22
jmaih <junior.maih@gmail.com>
2016-03-26 09:50:19 +0100

    improved efficiency of pages2struct

--- classes/time_series/@ts/pages2struct.m
+++ classes/time_series/@ts/pages2struct.m
@@ -20,12 +20,32 @@ function this=pages2struct(this0)
 % See also: 
 
 vnames=this0.varnames;
+
 if numel(unique(vnames))~=this0.NumberOfVariables
-    error([mfilename,':: number of unique variable names different from number of columns of data matrix'])
+    
+    error([mfilename,':: number of unique variable names different ',...
+        'from number of columns of data matrix'])
+    
 end
+
 this=struct();
+
 datta=this0.data;
+
 for ii=1:this0.NumberOfVariables
-    this.(this0.varnames{ii})=ts(this0.date_numbers,permute(datta(:,ii,:),[1,3,2]));
+    
+    newdata=permute(datta(:,ii,:),[1,3,2]);
+    
+    if ii==1
+        
+        this.(this0.varnames{ii})=ts(this0.date_numbers,newdata);
+        
+    else
+        
+        this.(this0.varnames{ii})=reset_data(this.(this0.varnames{1}),newdata);
+        
+    end
+    
 end
+
 end
