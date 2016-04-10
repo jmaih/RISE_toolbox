19bd71183a2a2a3a590e8b83fd26b011e262e81b
jmaih <junior.maih@gmail.com>
2016-04-10 17:01:17 +0200

    Additional output for set y and x

--- classes/models/+vartools/set_y_and_x.m
+++ classes/models/+vartools/set_y_and_x.m
@@ -1,4 +1,4 @@
-function [y,x,n,T]=set_y_and_x(datay,datax,nlags,constant)
+function [y,x,n,T,num_regress]=set_y_and_x(datay,datax,nlags,constant)
 % H1 line
 %
 % Syntax
@@ -20,17 +20,38 @@ function [y,x,n,T]=set_y_and_x(datay,datax,nlags,constant)
 % See also: 
 
 [n,smpl]=size(datay);
-if ~all(isfinite(datay(:)))
+
+if (~all(isfinite(datay(:)))) ||...
+        (~isempty(datax) && ~all(isfinite(datax(:))))
+    
     error('estimation data for VARs must be free of NaNs and Inf')
+    
 end
+
 if constant
+    
     datax=[datax;ones(1,smpl)];
+    
 end
+
 nx=size(datax,1);
+
 T=smpl-nlags;
+
 y=datay(:,nlags+1:end);
-x=nan(n*nlags+nx,T);
+
+num_regress=n*nlags+nx;
+
+x=nan(num_regress,T);
+
 for ilag=1:nlags
-    x((ilag-1)*n+1:n*ilag,:)=datay(:,(nlags+1:end)-ilag);
+    
+    stretch=(ilag-1)*n+1:n*ilag;
+    
+    x(stretch,:)=datay(:,(nlags+1:end)-ilag);
+    
 end
+
 x(n*nlags+(1:nx),:)=datax(:,nlags+1:end);
+
+end
