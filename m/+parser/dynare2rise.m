fe0cd14c0b7c9a4b27ba72a2de8857b3389cf23b
jmaih <junior.maih@gmail.com>
2016-04-22 21:46:50 +0200

    WIP: conversion from dynare to RISE

---
+++ m/+parser/dynare2rise.m
@@ -0,0 +1,259 @@
+function dynare2rise(dynFileName,riseFileName,stderr_name,fast)
+
+if nargin<4
+    
+    fast=[];
+    
+    if nargin<3
+        
+        stderr_name=[];
+        
+    end
+    
+end
+
+if isempty(fast)
+    
+    fast=false;
+    
+end
+
+if isempty(stderr_name)
+    
+    stderr_name='std';
+    
+end
+
+dynFileName = strtrim(dynFileName);
+
+riseFileName = strtrim(riseFileName);
+
+% read input file
+raw_code = read_file();
+
+header = sprintf('Conversion of Dynare file [%s] into RISE file [%s]\n',...
+    dynFileName,riseFileName);
+
+% Remove line comments.
+rise_code = regexprep(raw_code,'//(.*?\n)','');
+
+% Convert char(10) to white space.
+eol=char(10);
+rise_code = converteols(rise_code);
+
+% remove multiple white characters
+rise_code = regexprep(rise_code,'\s+',' ');
+
+rise_code = regexprep(rise_code,'model\(linear\)','model');
+
+% endogenous: var section
+%-------------------------
+endo_list=get_list('var',';');
+
+% exogenous: varexo section
+%----------------------------
+exo_list=get_list('varexo',';');
+
+% parameters: parameters section
+%--------------------------------
+par_list=get_list('parameters',';');
+
+% add new parameters
+par_list=[par_list,strcat([stderr_name,'_'],exo_list)];
+
+% model equations
+%-----------------
+model_eqtns = get_list('model;','end;',false);
+
+% replace time indices
+model_eqtns = regexprep(model_eqtns,'(?<=\w)\(([\+-]?\d*)\)','{$1}');
+
+% steady state model equations
+%------------------------------
+ssmodel_eqtns = get_list('steady_state_model;','end;',false);
+
+% push standard deviations into equations directly
+%-------------------------------------------------
+add_stdev_to_model(fast)
+
+% Looking for covariances: shocks section
+%----------------------------------------
+shocks_block = get_list('shocks;','end;',false);
+
+if ~isempty(shocks_block)
+    
+    match = regexpi(shocks_block,'var\s*\w+\s*,\s*\w+\s*=.*?;','match');
+    
+    for ii = 1 : length(match)
+        
+        header = [header,sprintf('Cross-covariance ignored:\n  %s\n',...
+            match{ii})]; %#ok<AGROW>
+        
+    end
+    
+end
+
+% Create and save RISE code.
+%---------------------------
+timestamp = datestr(now);
+
+recreate_code();
+
+    function add_stdev_to_model(fast)
+        
+        if fast
+            
+            for ishock=1:numel(exo_list)
+                
+                expre=exo_list{ishock};
+                
+                repl=[stderr_name,'_',expre,'*',expre];
+                
+                model_eqtns=regexprep(model_eqtns,expre,repl);
+                
+            end
+            
+        else
+            
+            expre=cell2mat(strcat(exo_list,'|'));
+            
+            expre=['(?<!\w+)(',expre(1:end-1),')(?!\w+)'];
+            
+            repl=[stderr_name,'_$1*$1'];
+            
+            model_eqtns=regexprep(model_eqtns,expre,repl);
+        end
+    end
+
+    function rise_code = recreate_code()
+        
+        header = [header,sprintf('\n Done %s.',timestamp)];
+        
+        rise_code = [
+            'endogenous ',endo_list,eol,...
+            'exogenous ',exo_list,eol,...
+            'parameters ',par_list,eol,...
+            'model ',model_eqtns,eol
+            ];
+        
+        if ~isempty(ssmodel_eqtns)
+            rise_code = [rise_code,...
+                'steady_state_model ',ssmodel_eqtns,eol
+                ];
+        end
+        
+        rise_code = regexprep(rise_code,'(\n)\s*','$1  ');
+        
+        rise_code = regexprep(rise_code,';',sprintf(';\n\n'));
+        
+        rise_code = [
+            regexprep(['%',header],'(\n)','$1%'),...
+            rise_code
+            ];
+        
+        write2file(rise_code,riseFileName);
+        
+    end
+
+    function list=get_list(typeof,closing,do_match)
+        
+        if nargin<3
+            
+            do_match=true;
+            
+        end
+        
+        tokens = regexpi(rise_code,...
+            [typeof,'\s*(.*?)',closing],'tokens','once');
+        
+        if isempty(tokens)
+            
+            list=tokens;
+            
+            return
+            
+        end
+        
+        if do_match
+            
+            list = regexp(tokens{1},'\w*','match');
+            
+        else
+            
+            list = tokens{1};
+            
+        end
+        
+    end
+
+    function raw_code = read_file()
+        
+        fid = fopen(dynFileName,'r');
+        
+        if fid == -1
+            
+            if exist(dynFileName,'file') == false
+                
+                error('Unable to find ''%s''.',dynFileName);
+                
+            else
+                
+                error('Unable to open ''%s'' for reading.',dynFileName);
+                
+            end
+            
+        end
+        
+        raw_code = transpose(fread(fid,'*char'));
+        
+        fclose(fid);
+        
+    end
+
+end
+
+function x = converteols(x)
+%x = regexprep(x,'\r\n?','\n');
+% This is much faster:
+% Windows:
+x = strrep(x,sprintf('\r\n'),sprintf('\n'));
+% Apple:
+x = strrep(x,sprintf('\r'),sprintf('\n'));
+
+end
+
+function write2file(C,filename)
+
+fid = fopen(filename,'w+');
+
+if fid == -1
+    
+    error(['Cannot open file ''%s'' for writing.',filename]);
+    
+end
+
+if iscellstr(C)
+    
+    C = sprintf('%s\n',C{:});
+    
+    if ~isempty(C)
+        
+        C(end) = '';
+        
+    end
+    
+end
+
+count = fwrite(fid,C,'char');
+
+if count ~= length(C)
+    
+    fclose(fid);
+    
+    error(['Cannot write character string to file ''%s''.',filename]);
+    
+end
+
+fclose(fid);
+
+end
---
+++ m/shortcuts/dynare2rise.m
@@ -0,0 +1,13 @@
+function dynare2rise(varargin)
+% converts a basic dynare file into a RISE one
+%
+% dynare2rise(dynFileName,riseFileName)
+% dynare2rise(dynFileName,riseFileName,stderr_name)
+% dynare2rise(dynFileName,riseFileName,stderr_name,fast)
+% dynare2rise(dynFileName,riseFileName,[],fast)
+% dynare2rise(dynFileName,riseFileName,stderr_name,[])
+% dynare2rise(dynFileName,riseFileName,[],[])
+
+parser.dynare2rise(varargin{:});
+
+end
