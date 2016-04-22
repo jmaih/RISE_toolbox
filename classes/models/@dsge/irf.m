8a0cf6b199e63dd3dee49dfc9e0a09b4e0271c24
jmaih <junior.maih@gmail.com>
2016-04-22 15:23:01 +0200

    FIX: IRF: pass options before doing anything

--- classes/models/@dsge/irf.m
+++ classes/models/@dsge/irf.m
@@ -20,15 +20,24 @@ function out=irf(obj,varargin)
 % See also: 
 
 if isempty(obj)
+    
     out=irf@rise_generic(obj);
+    
     out=utils.miscellaneous.mergestructures(out,...
         struct('irf_anticipate',true));%,'irf_risk',true
+    
     return
+    
 end
+
+obj=set(obj,varargin{:});
 
 for iobj=1:numel(obj)
+    
     obj(iobj).options.simul_anticipate_zero=~obj(iobj).options.irf_anticipate;
+    
 end
 
-out=irf@rise_generic(obj,varargin{:});
+out=irf@rise_generic(obj);
+
 end
