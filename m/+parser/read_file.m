854baaebe40defac8b0a0dba0fd9f71e0ecf1b6c
jmaih <junior.maih@gmail.com>
2016-05-09 00:21:34 +0200

    cosmetics and implementation of block comments

--- m/+parser/read_file.m
+++ m/+parser/read_file.m
@@ -21,22 +21,69 @@ function RawFile=read_file(FileName)
 
 
 RawFile=cell(0,1);
+
 if isempty(FileName)
+    
     return
+    
 end
+
 fid = fopen(FileName);
+
 iter=0;
+
+is_block_comments_open=false;
+
 while 1
+    
     rawline = fgetl(fid);
+    
     if ~ischar(rawline), break, end
-    rawline=parser.remove_comments(rawline);
-    iter=iter+1;
-    if all(isspace(rawline))
-        continue
+    
+    tokk=strtok(rawline);
+    
+    ibco=strcmp(tokk,'%{')||strcmp(tokk,'/*');
+    
+    if ibco 
+        
+        if is_block_comments_open
+            
+            error('nested block comments not allowed ')
+            
+        end
+        
+        is_block_comments_open=true;
+        
     end
+    
+    if is_block_comments_open
+        
+        if strcmp(tokk,'%}')||strcmp(tokk,'*/')
+            
+            is_block_comments_open=false;
+            
+        end
+        
+        continue
+        
+    end
+    
+    rawline=parser.remove_comments(rawline);
+    
+    iter=iter+1;
+    
+    if all(isspace(rawline))
+        
+        continue
+        
+    end
+    
     rawline={rawline,FileName,iter}; %#ok<*AGROW>
+    
     RawFile=[RawFile;rawline];
+    
 end
+
 fclose(fid);
 
 end
