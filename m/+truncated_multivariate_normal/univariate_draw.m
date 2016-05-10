cfdd365c7b066441051c1ea9a625636c9dc8ac61
jmaih <junior.maih@gmail.com>
2016-05-10 16:26:21 +0200

    mostly cosmetics

--- m/+truncated_multivariate_normal/univariate_draw.m
+++ m/+truncated_multivariate_normal/univariate_draw.m
@@ -17,33 +17,60 @@ function y=univariate_draw(lb,ub)
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 [nr,nc]=size(lb);
+
 lb=lb(:);
+
 ub=ub(:);
+
 y=nan(nr*nc,1);
+
 PHIl = normcdf(lb);
+
 PHIr = normcdf(ub);
+
 same=abs(lb-ub)<10*eps(lb);
+
 tails=~same & abs(PHIr-PHIl)<10*eps(PHIr);
+
 good=~same & ~tails;
 
-y(same)=ub(same);
-y(tails)=lb(tails)+(ub(tails)-ub(tails)).*rand(sum(tails),1);
-y(good)=InverseNormalCumulativeDistribution(PHIl(good)+(PHIr(good)-PHIl(good)).*rand(sum(good),1));
+if any(same)
+    
+    y(same)=ub(same);
+    
+end
+
+if any(tails)
+    
+    y(tails,1)=lb(tails,1)+(ub(tails,1)-ub(tails,1)).*rand(sum(tails),1);
+    
+end
+
+if any(good)
+    
+    y(good)=InverseNormalCumulativeDistribution(PHIl(good)+(PHIr(good)-PHIl(good)).*rand(sum(good),1));
+    
+end
 
 y=reshape(y,nr,nc);
+
 end
 
 %====================================BEGINNING OF NEW SUB=ROUTINE ===============================================%
 
 function cdf=normcdf(x)
+
 cdf=.5*(1+erf(x/sqrt(2)));
+
 end
 
 %====================================BEGINNING OF NEW SUB=ROUTINE ===============================================%
 
 function x=InverseNormalCumulativeDistribution(cdf)
+
 x=sqrt(2)*erfinv(2*cdf-1);
+
 end
