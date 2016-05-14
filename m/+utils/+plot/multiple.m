7b52d8493e8601e292f4ba2ce26f3e36cae2bec4
jmaih <junior.maih@gmail.com>
2016-05-14 14:54:58 +0200

    mostly cosmetics

--- m/+utils/+plot/multiple.m
+++ m/+utils/+plot/multiple.m
@@ -14,7 +14,7 @@ function hfig=multiple(plotfunc,vnames,fig_title,r0,c0,varargin)
 %
 % - **plotfunc** [function handle]: which takes as input a valid variable
 %   name and returns (1) the name/description of the variable/parameter
-%   plotted, (2) the legend 
+%   plotted, (2) the legend
 %
 % - **vnames** [cellstr]: names of variables to be plotted
 %
@@ -38,37 +38,70 @@ function hfig=multiple(plotfunc,vnames,fig_title,r0,c0,varargin)
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 if ischar(vnames)
+    
     vnames=cellstr(vnames);
+    
 end
+
 npar=numel(vnames);
+
 rmdoll=@(x)strrep(x,'$','');
+
 nstar=r0*c0;
+
 nfig=ceil(npar/nstar);
+
 hfig=nan(nfig,1);
+
 for fig=1:nfig
+    
     [Remains,r,c]=number_of_rows_and_columns_in_figure(fig,npar,r0,c0);
+    
     if nfig>1
+        
         titelfig=[fig_title,' ',int2str(fig)];
+        
     else
+        
         titelfig=fig_title;
+        
     end
+    
     hfig(fig)=figure('name',titelfig);
+    
     for plt=1:min(nstar,Remains)
+        
         par_id=(fig-1)*nstar+plt;
+        
         figure(hfig(fig))
+        
         subplot(r,c,plt)
+        
         [tex_name,legend_]=plotfunc(vnames{par_id});
+        
         if ~isempty(tex_name)
+            
             title(rmdoll(tex_name),varargin{:})
+            
         end
+        
         if plt==1 && ~isempty(legend_)
+            
             hleg=legend(legend_);
-            set(hleg,varargin{:});
+            
+            if ~isempty(varargin)
+                
+                set(hleg,varargin{:});
+                
+            end
+            
         end
+        
     end
+    
 end
 
 end
