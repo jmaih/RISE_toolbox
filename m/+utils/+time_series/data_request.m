f277187753f54bdf771d040b5280959ec393efa0
Junior Maih <junior.maih@gmail.com>
2016-06-09 07:46:20 +0200

    cosmetics

--- m/+utils/+time_series/data_request.m
+++ m/+utils/+time_series/data_request.m
@@ -40,54 +40,102 @@ function [data_values,start_date,end_date,missing]=data_request(...
 % See also: 
 
 if nargin<5
+    
     pages=[];
+    
     if nargin<4
+        
         end_date='';
+        
         if nargin<3
+            
             start_date='';
+        
         end
+        
     end
+    
 end
 
+
 The_data=ts.collect(data);
+
 if isempty(start_date)
+    
     start_date=The_data.start;
+
 end
+
 if isempty(end_date)
+    
     end_date=The_data.finish;
+
 end
+
 start=date2obs(The_data.start,start_date);
+
 finish=date2obs(The_data.start,end_date);
+
 if start<=0
+    
     error('estimation start date inferior to data start')
+
 end
+
 nobs__=The_data.NumberOfObservations;
+
 missing=max(0,finish-nobs__);
+
 if missing>0
+    
     warning('estimation end date greater than data end date: nan observations will be added')
+
 end
+
 nvarobs=numel(varlist);
+
 ids=locate_variables(varlist,The_data.varnames,true);
+
 DataValues=double(The_data);
+
 npages=The_data.NumberOfPages;
+
 if isempty(pages)
+    
     pages=1:npages;
+    
 end
+
 if any(pages>npages)
+    
     error('the anticipated horizon of shocks exceeds the number of "extra" pages in the dataset')
+
 end
+
 data_values=nan(nobs__,nvarobs,numel(pages));
+
 for ivar=1:numel(ids)
+    
     if isnan(ids(ivar))
+        
         warning(['variable ',varlist{ivar},' not found in the database'])
+        
         continue
+        
     end
+    
     data_values(:,ivar,pages)=DataValues(:,ids(ivar),pages);
+    
 end
 % extend as necessary
 %--------------------
 sizDV=size(data_values);
+
 data_values=[data_values;nan(missing,sizDV(2),numel(pages))];
+
 data_values=permute(data_values,[2,1,3]);
+
 % send forward only the data that are needed.
 data_values=data_values(:,start:finish,:);
+
+end
