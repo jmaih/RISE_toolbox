2ea210627cae7a4508e1883068b597d995faf918
Junior Maih <junior.maih@gmail.com>
2016-06-09 10:49:27 +0200

    robustness: use strcmpi instead of strcmp

--- classes/time_series/date2obs.m
+++ classes/time_series/date2obs.m
@@ -20,26 +20,47 @@ function [obs,flag_]=date2obs(start_date,new_date)
 % See also: 
 
 [start_serial,start_frequency]=date2serial(start_date);
+
 [new_serial,new_frequency]=date2serial(new_date);
-if ~strcmp(start_frequency,new_frequency)
+
+if ~strcmpi(start_frequency,new_frequency)
+    
     error('dates are not in the same frequency')
+    
 end
+
 retcode=1-(new_serial>=start_serial);
+
 if nargout>1
+    
     flag_=retcode;
+    
 end
+
 if retcode
+    
     if nargout<2
+        
         error('new date occurs before the start date')
+        
     end
+    
     obs=[];
+    
 else
+    
     obs=nan(size(new_serial));
+    
     for iserial=1:numel(obs)
+        
         span=start_serial:new_serial(iserial);
+        
         obs(iserial)=numel(span);
+        
     end
+    
 end
+
 end
 
 %     start=The_data.TimeInfo(1).date_2_observation(obj.options.estim_start_date);
