dfbfe0668a5f85bff4cf118b0d516024afbaf1f9
Junior Maih <junior.maih@gmail.com>
2016-06-15 10:09:27 +0200

    MH sampler: save to disk + cosmetics

--- m/mcmc/mh_sampler.m
+++ m/mcmc/mh_sampler.m
@@ -48,6 +48,8 @@ function [Results]=mh_sampler(logf,lb,ub,options,mu,SIG)
 %   - **MaxTime** [numeric|{inf}]: maximum simulation time.
 %   - **adapt_covariance** [true|{false}]: If true, the covariance matrix
 %   is updated with the sampled draws.
+%   - **save** [struct|{'every='inf,'location=',pwd,'filename=',''}]:
+%   structure with fields "every", "location", "filename"
 %
 % - **mu** [d x 1 vector]: initial condition for the sampler
 %
@@ -80,7 +82,11 @@ function [Results]=mh_sampler(logf,lb,ub,options,mu,SIG)
 % See also:	CONSTANT_BVAR_SAMPLER, RRF_SAMPLER
 
 num_fin=@(x)isnumeric(x) && isscalar(x) && isfinite(x) && isreal(x);
+
 num_fin_int=@(x)num_fin(x) && floor(x)==ceil(x) && x>=0;
+
+savestruct=struct('every',inf,'location',pwd,'filename','');
+
 defaults={ % arg_names -- defaults -- checks -- error_msg
     'alpha',[.25,.45],@(x)((numel(x)==2 && x(2)>=x(1))||numel(x)==1) && all(x>0) && all(x<1),'target_range should be a 2-element vector s.t. x(2)>=x(1) and x(i) in (0,1)'
     'burnin',0,@(x)num_fin_int(x),'burnin should be an integer in [0,inf)'
@@ -98,47 +104,94 @@ defaults={ % arg_names -- defaults -- checks -- error_msg
     'logproppdf',[],@(x)isa(x,'function_handle'),'logproppdf must be a function handle'
     'MaxTime',inf,@(x)num_fin(x) && x>0,'MaxTime must be a positive scalar'
     'adapt_covariance',false,@(x)isscalar(x) && islogical(x),'adapt_covariance should be a logical scalar'
+    'save',savestruct,@(x)isstruct(x) && all(ismember(fieldnames(x),fieldnames(savestruct))),'fields for save must be "every", "location" and "filename"'
 %     'delay_rejection',true,@(x)isscalar(x) && islogical(x),'delay_rejection should be a logical scalar'
     };
 
 
 if nargin==0
+    
     Results=cell2struct(defaults(:,2),defaults(:,1),1);
+    
     return
+    
 end
 
 if nargin<6
+    
     SIG=[];
+    
     if nargin<5
+        
         mu=[];
+        
         if nargin<4
+            
             options=struct();
+            
         end
+        
     end
+    
 end
 
 % number of parameters
 [d,ncols]=size(lb);
+
 if ncols~=1
+    
     error('number of columns of lb should be 1')
+    
 end
 
 if ~(all(isfinite(lb)) && all(isfinite(ub)))
+    
     error('lb and ub shoud be finite')
+    
 end
 
 if d==0
+    
     error('lb cannot have less than 1 row')
+    
 end
 
 if ~isequal(size(ub),[d,ncols])
+    
     error('size ub does not match size lb')
+    
 end
 
 options=parse_arguments(defaults,options);
 
+savefile=options.save.filename;
+
+savelocation=options.save.location;
+
+saveevery=options.save.every;
+
+is_save=isfinite(saveevery);
+
+if is_save
+    
+    if isempty(savelocation)
+        
+        savelocation=pwd;
+        
+    end
+    
+    if isempty(savefile)
+        
+        savefile='mhDraws';
+        
+    end
+    
+end
+
 if ischar(logf)
+    
     logf=str2func(logf);
+    
 end
 
 logproppdf=options.logproppdf;
@@ -146,7 +199,9 @@ logproppdf=options.logproppdf;
 symmetric=isempty(logproppdf);
 
 if ~isa(logf,'function_handle')
+    
     error('logf should be a function handle or a string')
+    
 end
 
 % target acceptance range
@@ -156,8 +211,11 @@ alpha=options.alpha;
 % more options
 %-------------
 fixed_scaling=options.fixed_scaling;
+
 rwm_exp=options.rwm_exp;
+
 c_range=options.c_range;
+
 use_true_moments=options.use_true_moments;
 
 % worst value that the function can assume
@@ -185,7 +243,9 @@ log_c=log(options.c)*ones(1,nchain);
 N=options.N;
 
 if isempty(mu)
+    
     mu=.5*(ub+lb);
+    
 end
 
 % covariance adaptations
@@ -193,12 +253,27 @@ end
 adapt_covariance=options.adapt_covariance;
 
 if isempty(SIG)
+    
     SIG=1e-4*eye(d);%SIG=covar;
+    
     adapt_covariance=true;
+    
 end
-mu=mu(:,ones(1,nchain));
-SIG=SIG(:,:,ones(1,nchain));
+
+if size(mu,2)<nchain
+    
+    mu=mu(:,ones(1,nchain));
+    
+end
+
+if size(SIG,3)<nchain
+
+    SIG=SIG(:,:,ones(1,nchain));
+    
+end
+
 mu_algo=mu;
+
 SIG_algo=SIG;
 
 % draw initial distribution:
@@ -209,36 +284,51 @@ stud=stud(:);
 
 % pre-allocate
 %--------------
-pop=stud(:,ones(1,N));
+if is_save
+    
+    saveevery=min(saveevery,N);
+    
+    pop=stud(:,ones(1,saveevery));
+    
+    the_iter=0;
+    
+    isave_batch=0;
+    
+else
+    
+    pop=stud(:,ones(1,N));
+    
+end
 
 retune_cov_every=options.retune_cov_every;
 
 d=numel(lb);
 
-obj=struct();
-obj.funcCount=sum(funevals);
-obj.iterations=0;
-obj.start_time=clock;
-obj.MaxTime=options.MaxTime;
-obj.verbose=options.verbose;
-obj.MaxFunEvals=inf;
-obj.best_fval=[];
-obj.MaxNodes=1;
-obj.optimizer=mfilename;
-obj.number_of_parameters=size(mu,1);
-obj.accept_ratio=[];
+obj=struct('funcCount',sum(funevals),'iterations',0,'start_time',clock,...
+    'MaxTime',options.MaxTime,'verbose',options.verbose,'MaxFunEvals',inf,...
+    'best_fval',[],'MaxNodes',1,'optimizer',mfilename,...
+    'number_of_parameters',size(mu,1),'accept_ratio',[]);
 
 sqrt_cSIG=[];
+
 nbatch=1;
+
 accept=zeros(1,nchain);
+
 big_accept=0;
+
 scale_times_sqrt_covariance_updating()
 
 idraw=-burnin;
+
 total_draws=N*thin+burnin;
+
 obj.MaxIter=total_draws;
+
 U = log(rand(total_draws,nchain));
+
 q_x0_given_y = 0;
+
 q_y_given_x0 = q_x0_given_y;
 
 utils.optim.manual_stopping;
@@ -254,17 +344,26 @@ accept_ratio=[];
 % wtbh=waitbar(0,'please wait...','Name','MH sampling');
 
 while isempty(stopflag)
+    
     idraw=idraw+1;
+    
     obj.iterations=idraw+burnin;
+    
     % sample from the proposal distribution
     %---------------------------------------
     y=proprnd();
+    
     if ~symmetric
+        
         q_x0_given_y = logproppdf(stud,y);
+        
         q_y_given_x0 = logproppdf(y,stud);
+        
     end
+    
     % this is a generic formula.
     rho = ([y.f]+q_x0_given_y)-([stud.f]+q_y_given_x0);
+    
     % minimization: change sign
     %--------------------------
     rho=-rho;
@@ -281,9 +380,28 @@ while isempty(stopflag)
     %----------
     if idraw>0 && mod(idraw,thin)==0 % burnin and thin
         
-        the_iter=idraw/thin;
+        if is_save
+            
+            the_iter=the_iter+1;
+            
+        else
+            
+            the_iter=idraw/thin;
+            
+        end
+        
         
         pop(:,the_iter) = stud;
+        
+        if is_save && the_iter==saveevery
+            
+            isave_batch=isave_batch+1;
+            
+            save(sprintf('%s%s%s_%0.0f',savelocation,filesep,savefile,isave_batch),'pop')
+            
+            the_iter=0;
+            
+        end
         
     end
     
@@ -304,13 +422,17 @@ while isempty(stopflag)
     % update the cCs
     %---------------
     if mod(obj.iterations,retune_cov_every)==0
+        
         nbatch=nbatch+1;
+        
         scale_times_sqrt_covariance_updating()
+        
     end
     
     % display progress
     %------------------
     obj.accept_ratio=accept_ratio;
+    
     utils.optim.display_progress(obj)
     
     % check convergence
@@ -325,9 +447,21 @@ end
 do_results()
 
     function do_results()
-        pop=pop(:,1:the_iter);
+        
+        if is_save
+            % in case we end right at the re-initialization...
+            pop=pop(:,1:max(1,the_iter));
+            
+        else
+            
+            pop=pop(:,1:the_iter);
+            
+        end
+        
         bestOfTheBest=utils.mcmc.sort_population(best);
+        
         obj.end_time=clock;
+        
         Results=struct('pop',pop,...
             'bestf',bestOfTheBest(1).f,...
             'bestx',bestOfTheBest(1).x,...
@@ -342,52 +476,86 @@ do_results()
     end
 
     function scale_times_sqrt_covariance_updating()
+        
         accept_ratio=accept/retune_cov_every;
+        
         sqrt_cSIG=SIG;
+        
         iterations=obj.iterations;
+        
         parfor (ichain=1:nchain,NumWorkers)
+            
             if iterations>0
+                
                 log_c(ichain)=utils.mcmc.update_scaling(log_c(ichain),...
                     accept_ratio(ichain),alpha,fixed_scaling,nbatch,...
                     rwm_exp,[],c_range);
+                
             end
+            
             if use_true_moments
+                
                 sqrt_cSIG(:,:,ichain)=chol(...
                     exp(log_c(ichain))*SIG(:,:,ichain),...
                     'lower');
+                
             else
+                
                 sqrt_cSIG(:,:,ichain)=chol(...
                     exp(log_c(ichain))*SIG_algo(:,:,ichain),...
                     'lower');
+                
             end
+            
         end
+        
         big_accept=big_accept+accept;
+        
         accept=0*accept;
+        
     end
 
     function moments_updating()
+        
         if adapt_covariance
+            
             for ichain=1:nchain
+                
                 [mu(:,ichain),SIG(:,:,ichain)]=utils.moments.recursive(...
                     mu(:,ichain),SIG(:,:,ichain),stud(ichain).x,obj.iterations);
+                
                 [mu_algo(:,ichain),SIG_algo(:,:,ichain)]=utils.mcmc.update_moments(...
                     mu_algo(:,ichain),SIG_algo(:,:,ichain),stud(ichain).x,obj.iterations,...
                     options.rwm_exp);
+                
             end
+            
         end
+        
     end
 
     function v1=proprnd()
+        
         v1=stud;
+        
         parfor (ichain=1:nchain,NumWorkers)
+            
             xd=stud(ichain).x+sqrt_cSIG(:,:,ichain)*randn(d,1);
+            
             bad=xd<lb; xd(bad)=lb(bad);
+            
             bad=xd>ub; xd(bad)=ub(bad);
+            
             v1(ichain).x=xd;
+            
             v1(ichain).f=logf(xd); %#ok<PFBNS>
+            
         end
+        
         funevals=funevals+nchain;
+        
         obj.funcCount=sum(funevals);
+        
     end
 
 %     function waitbar_updating()
