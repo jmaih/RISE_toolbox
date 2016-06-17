99cbb4a6b19109e415af9503de761b171e73c3d6
Junior Maih <junior.maih@gmail.com>
2016-06-17 15:23:12 +0200

    cosmetics

--- m/+utils/+miscellaneous/apply_property.m
+++ m/+utils/+miscellaneous/apply_property.m
@@ -28,19 +28,33 @@ function newv=apply_property(type,refv,newv)
 % See also:
 
 switch lower(type)
+    
     case 'range'
+        
         if max(newv)==min(newv)
+            
             ratio=.5;
+            
         else
+            
             ratio=(newv-min(newv))/(max(newv)-min(newv));
+            
         end
+        
         newv=min(refv)+ratio*(max(refv)-min(refv));
+        
     case 'max'
+        
         newv=newv-max(newv)+max(refv);
+        
     case 'min'
+        
         newv=newv-min(newv)+min(refv);
+        
     otherwise
+        
         error(['unknown property:: ',type])
+        
 end
 
 end
