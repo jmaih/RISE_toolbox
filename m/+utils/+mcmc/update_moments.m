1d1f36720f1fd35bdcbf8266965c07c79d652e0c
Junior Maih <junior.maih@gmail.com>
2016-06-21 20:42:48 +0200

    cosmetics

--- m/+utils/+mcmc/update_moments.m
+++ m/+utils/+mcmc/update_moments.m
@@ -46,28 +46,49 @@ function [mu,SIG]=update_moments(mu,SIG,x,n,xi2,c2,e)
 % Applies formulae 23 and 24 in Blazej Miasojedow, Eric Moulines and Matti
 % Vihola (2012): "Adaptive Parallel Tempering Algorithm"
 if nargin<7
+    
     e=sqrt(eps);
+    
     if nargin<6
+        
         c2=1;
+        
     end
+    
 end
+
 if c2<=0||c2>1
+    
     error('c2 must be in (0,1]')
+    
 end
+
 if xi2<=0.5||xi2>=1
+    
     error('xi2 must be in (0.5,1)')
+    
 end
 
 L=size(x,2);
+
 L_xbar=sum(x,2);
+
 L_C=0;
+
 for icol=1:L
+    
     xm=x(:,icol)-mu;
+    
    L_C=L_C+(xm*xm.'); 
+   
 end
 
 gam2=c2*(n+1)^(-xi2);
+
 SIG=(1-gam2)*SIG+gam2/L*L_C;
+
 SIG = utils.cov.project(SIG,e);
+
 mu=(1-gam2)*mu+gam2/L*L_xbar;
+
 end
