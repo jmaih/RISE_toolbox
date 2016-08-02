928349f7499d59276d91183495ab58a3fbf13818
Junior Maih <junior.maih@gmail.com>
2016-08-02 13:35:59 +0200

    change in regular expression

--- m/+parser/collect_symbolic_list.m
+++ m/+parser/collect_symbolic_list.m
@@ -32,7 +32,8 @@ batch=parser.remove_handles(batch);
 items=strcat(items,'|');
 items=cell2mat(items);
 items=items(1:end-1);
-list_ss=regexp(batch,['(?<![\w])(',items,')[\d]+(?![\w])'],'match');
+% list_ss=regexp(batch,['(?<![\w])(',items,')[\d]+(?![\w])'],'match');
+list_ss=regexp(batch,['\<(',items,')[\d]+\>'],'match');
 if ~isempty(list_ss) && iscell(list_ss{1})
     list_ss_={};
     for ilist=1:numel(list_ss)
