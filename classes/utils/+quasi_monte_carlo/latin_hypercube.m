12811349dc98f155f34a292eb1618e6433294976
Junior Maih <junior.maih@gmail.com>
2016-08-02 13:35:08 +0200

    typo fixed in LHS

--- classes/utils/+quasi_monte_carlo/latin_hypercube.m
+++ classes/utils/+quasi_monte_carlo/latin_hypercube.m
@@ -20,28 +20,48 @@ function theta_=latin_hypercube(lb,ub,K,center)
 % See also: 
 
 
-if nargin<3
+if nargin<4
+    
     center=true;
+    
 end
+
 n=size(lb,1);
 
 batch=linspace(0,1,K+1);
+
 control=true(n,K);
+
 seq=nan(n,K);
+
 for k=1:K
+    
     for ii=1:n
+        
         avail=find(control(ii,:));
+        
         interv=avail(randi(numel(avail),1));
+        
         control(ii,interv)=false;
+        
         low=batch(interv);
+        
         high=batch(interv+1);
+        
         if center
+            
             seq(ii,k)=.5*(low+high);
+            
         else
+            
             seq(ii,k)=low+rand*(high-low);
+            
         end
+        
     end
+    
 end
-theta_=bsxfun(@plus,lb,bsxfun(@times,ub-lb,Seq));
+
+theta_=bsxfun(@plus,lb,bsxfun(@times,ub-lb,seq));
 
 end
