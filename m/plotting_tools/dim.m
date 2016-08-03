ff92555370f9355a1412739a93e435f11948355a
Junior Maih <junior.maih@gmail.com>
2016-08-03 10:39:01 +0200

    updates in shading

--- m/plotting_tools/dim.m
+++ m/plotting_tools/dim.m
@@ -1,4 +1,4 @@
-function []=dim(start,finish,colorstr)
+function []=dim(start,finish,colorstr,fig)
 % H1 line
 %
 % Syntax
@@ -17,31 +17,68 @@ function []=dim(start,finish,colorstr)
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 % function []=shade(start,finish,colorstr);
 %
-%  start and finish are Nx1 vectors of starting and ending years.
+%  start and finish are N x 1 vectors of starting and ending years.
 %  The function shades between the start and finish pairs using colorstr
 
-if nargin<3 || isempty(colorstr)
-    colorstr=[159 182 205]/256; 
-end;  % default is yellow
+if nargin<4
+    
+    fig=[];
+    
+    if nargin<3 || isempty(colorstr)
+        
+        colorstr=[159 182 205]/256;
+        
+    end  % default is yellow
+    
+end
 
-fig=gcf;
+if isempty(fig)
+    
+    fig=gcf;
+    
+end
+
+nfig=numel(fig);
+
+if nfig > 1
+    
+    for ifig=1:nfig
+        
+        dim(start,finish,colorstr,fig(ifig))
+        
+    end
+    
+    return
+    
+end
+
 kids=get(fig,'children');
+
 for ii=1:numel(kids)
+    
     ylim=get(kids(ii),'ylim');
+    
     if isnumeric(ylim)
+        
         y=[ylim(1) ylim(2) ylim(2) ylim(1)];
+        
         axes(kids(ii))
-        hold on;
-        for i=1:length(start);
+        
+        hold on
+        
+        for i=1:length(start)
+            
             x=[start(i) start(i) finish(i) finish(i)];
+            
             fill(x,y,colorstr);
-        end;
+            
+        end
         % Now, prevent the shading from covering up the lines in the plot.
-        h = findobj(kids(ii),'Type','line');
+%         h = findobj(kids(ii),'Type','line');
         % set(h,'EraseMode','xor');
         
         h = findobj(kids(ii),'Type','patch');
@@ -49,5 +86,7 @@ for ii=1:numel(kids)
         
         % This last one makes the tick marks visible
         set(kids(ii), 'Layer', 'top')
+        
     end
+    
 end
--- m/plotting_tools/dim_relevant.m
+++ m/plotting_tools/dim_relevant.m
@@ -1,4 +1,4 @@
-function []=dim_relevant(freq,nber_datesfunc)
+function []=dim_relevant(freq,nber_datesfunc,figs)
 % H1 line
 %
 % Syntax
@@ -17,45 +17,114 @@ function []=dim_relevant(freq,nber_datesfunc)
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
-if nargin<1
-    freq=[];
+if nargin < 3
+    
+    figs=[];
+    
+    if nargin < 2
+        
+        nber_datesfunc = [];
+                
+        if nargin<1
+            
+            freq=[];
+            
+        end
+        
+    end
+    
 end
-if nargin<2
+
+if isempty(figs)
+    
+    figs=gcf;
+    
+end
+
+nfigs=numel(figs);
+
+if nfigs>1
+    
+    for ifig=1:nfigs
+        
+        dim_relevant(freq,nber_datesfunc,figs(ifig))
+        
+    end
+    
+    return
+    
+end
+
+if isempty(nber_datesfunc)
+    
     [start,finish]= nber_dates(freq);
+    
 else
+        
     nber_datesfunc=fcnchk(nber_datesfunc);
-    [start,finish]= nber_datesfunc(freq);
+    
+    [start,finish]= nber_datesfunc(freq); %#ok<RHSFN>
+    
 end
+
 if ischar(start)
+    
     start= cellstr(start);
+    
 end
+
 if ischar(finish)
-    finish=cellstr(finish);
+    
+    finish = cellstr(finish);
+    
 end
 % shadenber.m
 %
 %  Routine to shade the nberdates in a figure
 
-curax=axis;
-start_dn=date2serial(start);
-finish_dn=date2serial(finish);
-if start_dn(1)>curax(1)
-    begin=find(start_dn>=curax(1),1,'first');  % First recession to include;
-else
-    begin=1;
-end
-if finish_dn(end)>curax(2)
-    last=find(finish_dn<=curax(2),1,'first');  % First recession to include;
-else
-    last=numel(finish_dn);
-end
-
-start=start_dn(begin:last);
-finish=finish_dn(begin:last);
+set_bounds()
 
 colorstr=[159 182 205]/256;
 
-dim(start,finish,colorstr);
+dim(start,finish,colorstr,figs);
+
+    function set_bounds()
+        
+        figure(figs)
+        
+        curax = axis;
+        
+        start_dn = date2serial(start);
+        
+        finish_dn = date2serial(finish);
+        
+        if start_dn(1)>curax(1)
+            
+            begin=find(start_dn>=curax(1),1,'first');  % First recession to include;
+            
+        else
+            
+            begin=1;
+            
+        end
+        
+        if finish_dn(end)>curax(2)
+            
+            last=find(finish_dn<=curax(2),1,'first');  % First recession to include;
+            
+        else
+            
+            last=numel(finish_dn);
+            
+        end
+        
+        start=start_dn(begin:last);
+        
+        finish=finish_dn(begin:last);
+        
+    end
+
+end
 
