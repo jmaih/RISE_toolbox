091b7be75e79125c441af2c8dbee12c1fa372412
Junior Maih <junior.maih@gmail.com>
2017-02-10 10:10:39 +0100

    checking size before resetting data

--- classes/time_series/@ts/reset_data.m
+++ classes/time_series/@ts/reset_data.m
@@ -13,13 +13,16 @@ function this=reset_data(this,newdata)
 %
 % More About
 % ------------
+% - Keeps the start date and only changes the data. New data no longer
+% needs to be of the same size as the old one.
 %
 % Examples
 % ---------
 %
-% See also: 
-if ~isequal(size(this.data),size(newdata))
-    error('data size mismatch')
-end
+% See also:
+
+ts.check_size(newdata);
+
 this.data=newdata;
+
 end
