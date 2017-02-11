51a1c2e81ae29c1f58738a94e8c3f69b9ce4601a
Junior Maih <junior.maih@gmail.com>
2017-02-11 19:27:12 +0100

    mostly cosmetics

--- classes/time_series/@ts/horzcat.m
+++ classes/time_series/@ts/horzcat.m
@@ -21,15 +21,33 @@ function this=horzcat(varargin)
 
 
 for ii=1:length(varargin)
-    if ~isequal(class(varargin{ii}),'ts')
-        error([mfilename,':: input ',int2str(ii),' must be from class ts'])
-    end
+    
     if ii==1
+        
         this=varargin{ii};
-    else
-        if ~isempty(varargin{ii})
-            this=this&varargin{ii};
+        
+        if ~((isa(this,'double') && isempty(this))||isa(this,'ts'))
+            
+            error('first argument must be an empty double or a ts')
+            
         end
+        
+    else
+        
+        if ~isequal(class(varargin{ii}),'ts')
+            
+            error([mfilename,':: input ',int2str(ii),' must be from class ts'])
+            
+        end
+        
+        if ~isempty(varargin{ii})
+            
+            this=this&varargin{ii};
+            
+        end
+        
     end
+    
 end
+
 end
