f63677a32e12f1e84c28f710f368a8e9c22d8979
Junior Maih <junior.maih@gmail.com>
2017-02-25 14:00:30 +0100

    ts: outputable index

--- classes/time_series/@ts/index.m
+++ classes/time_series/@ts/index.m
@@ -1,4 +1,4 @@
-function index(self)
+function out=index(self)
 % H1 line
 %
 % Syntax
@@ -19,9 +19,32 @@ function index(self)
 %
 % See also: 
 
+sp=cell(0,1);
+
 if ~isempty(self.date_numbers)
-    fprintf(1,'start_date: %s end_date: %s frequency: %s\n',self.start,self.finish,self.frequency);
+    
+    sp=[sp,...
+        sprintf('start_date: %s end_date: %s frequency: %s\n',...
+        self.start,self.finish,self.frequency)];
+    
 end
-fprintf(1,'# variables: %0.0f # observations: %0.0f # pages: %0.0f \n',self.NumberOfVariables,self.NumberOfObservations,self.NumberOfPages);
+
+sp=[sp,...
+    sprintf('# variables: %0.0f # observations: %0.0f # pages: %0.0f \n',...
+    self.NumberOfVariables,self.NumberOfObservations,self.NumberOfPages)];
+
+if nargout
+    
+    out=sp;
+    
+else
+    
+    for ii=1:numel(sp)
+        
+        fprintf(1,sp{ii});
+        
+    end
+    
+end
 
 end
