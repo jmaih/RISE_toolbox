0d0d4804aeffe33c32129dfe2e2343d4ba705c6d
Junior Maih <junior.maih@gmail.com>
2017-03-09 17:45:01 +0100

    cosmetics

--- m/+parser/create_state_list.m
+++ m/+parser/create_state_list.m
@@ -20,12 +20,19 @@ function x=create_state_list(name,number)
 % See also: 
 
 if isscalar(number)
+    
     number=(1:number).';
+    
 else
+    
     number=number(:);
+    
 end
 
 x=cellstr(strcat(name,'_',num2str(number)));
+
 x=cellfun(@(z)z(~isspace(z)),x,'uniformOutput',false);
+
 x=x(:).';
+
 end
