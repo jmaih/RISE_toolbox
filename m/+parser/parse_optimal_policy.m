1f94799907d01bc0ee1a102c1788d96ae66514b9
Junior Maih <junior.maih@gmail.com>
2017-03-19 04:37:39 +0100

    cosmetics

--- m/+parser/parse_optimal_policy.m
+++ m/+parser/parse_optimal_policy.m
@@ -3,8 +3,12 @@ function [dictionary,blocks,Model_block,PlannerObjective_block,jac_toc_]=...
 
 current_block_id=find(strcmp('planner_objective',{blocks.name}));
 
-[dictionary,PlannerObjective_block]=...
-    parser.planner_objective(dictionary,blocks(current_block_id).listing);
+% auxiliary variables and equations may also come from the planner
+% objective. In that case we need to add auxiliary equations to the model
+% block
+[dictionary,PlannerObjective_block,Model_block]=...
+    parser.planner_objective(dictionary,blocks(current_block_id).listing,...
+    Model_block);
 
 % remove item from block
 blocks(current_block_id)=[];
@@ -18,8 +22,10 @@ dictionary.is_optimal_policy_model=...
     dictionary.is_deficient_eqtns;
 
 if dictionary.is_optimal_policy_model|| dictionary.is_optimal_simple_rule_model
+    
     [Model_block,dictionary,jac_toc_]=parser.optimal_policy_system(...
         PlannerObjective_block,Model_block,dictionary);
+    
 end
 
 end
