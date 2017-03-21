615a4b0d565d7d42a8b32ea86cb4be5dde227605
Junior Maih <junior.maih@gmail.com>
2017-03-21 07:53:26 +0100

    updated removal of comments

--- m/+parser/remove_comments.m
+++ m/+parser/remove_comments.m
@@ -14,23 +14,15 @@ function rawline_=remove_comments(rawline_)
 % More About
 % ------------
 %
-% - "\%" is not considered a comment. This is the way to mask "%"
-%
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
-% locate comments of type %: those are the ones not preceeded with a \
-%----------------------------------------------------------------------
-loc_=regexp(rawline_,'(?<!\\)%'); % strfind(rawline_,'%');
+treat_double_quotes()
 
-if ~isempty(loc_)
-    
-    rawline_=rawline_(1:loc_(1)-1);
-    
-end
-
+% double slash
+%--------------
 loc_=strfind(rawline_,'//');
 
 if ~isempty(loc_)
@@ -38,5 +30,53 @@ if ~isempty(loc_)
     rawline_=rawline_(1:loc_(1)-1);
     
 end
+
+% percent
+%--------
+loc_=strfind(rawline_,'%');
+
+if ~isempty(loc_)
+    
+    rawline_=rawline_(1:loc_(1)-1);
+    
+end
+
+undo_double_quotes()
+
+    function undo_double_quotes()
+        
+        rawline_=strrep(rawline_,'¤','%');
+        
+        rawline_=strrep(rawline_,'§','/');
+        
+    end
+
+    function treat_double_quotes()
+        
+        dbl_quotes=strfind(rawline_,'"');
+        
+        while ~isempty(dbl_quotes)
+            
+            if numel(dbl_quotes)==1
+                
+                break
+                
+            end
+            
+            quotes=dbl_quotes(1:2);
+            
+            stretch=quotes(1):quotes(2);
+            
+            % should replace with exactly one character
+            rawline_(stretch)=strrep(rawline_(stretch),'%','¤');
+            
+            % should replace with exactly two character
+            rawline_(stretch)=strrep(rawline_(stretch),'//','§§');
+            
+            dbl_quotes=dbl_quotes(3:end);
+            
+        end
+        
+    end
 
 end
