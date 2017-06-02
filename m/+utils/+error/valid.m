bc2b28b73859e9725eb57ebf97ce5c46753f248f
Junior Maih <junior.maih@gmail.com>
2017-06-02 21:04:19 +0200

    cosmetics

--- m/+utils/+error/valid.m
+++ m/+utils/+error/valid.m
@@ -21,13 +21,21 @@ function flag=valid(x)
 
 
 if iscell(x)
+    
     flag=all(cellfun(@(x)utils.error.valid(x),x));
+    
 elseif isa(x,'double')
+    
     flag=isreal(x) && ~any(isnan(x(:))) && ~any(isinf(x(:)));
+    
 elseif isa(x,'tsparse')
+    
     flag=utils.error.valid(x.v);
+    
 else
+    
     error(['class ',class(x),' not a valid type for checking validity'])
+    
 end
 
 % valid=@(x)~any(isnan(x(:))) && ~any(isinf(x(:))); % nans in jacobian
