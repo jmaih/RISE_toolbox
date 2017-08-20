167d4a321cdcba2c3c5bb28bad1ace1a849e0a01
Junior Maih <junior.maih@gmail.com>
2017-08-20 16:56:31 +0200

    Journal for transition matrix recreated

--- m/+utils/+gridfuncs/chain_grid.m
+++ m/+utils/+gridfuncs/chain_grid.m
@@ -1,15 +1,36 @@
 function [Regimes,journal]=chain_grid(v)
-% H1 line
+% CHAIN_GRID -- computes the regimes from one or multiple markov chains
 %
 % Syntax
 % -------
 % ::
 %
+%   Regimes=CHAIN_GRID(a); : one chain with "a" states
+%
+%   Regimes=CHAIN_GRID([a,b]); : two chains with "a" states for the first
+%   and "b" states for the second
+%
+%   Regimes=CHAIN_GRID([a1,a2,...,an]); : n chains with "a1" states for the
+%   first, "a2" states for the second,...,"an" states for the nth 
+%
+%   [Regimes,Journal]=CHAIN_GRID(...);  
+%
 % Inputs
 % -------
 %
+% - **a** [scalar|vector]: number of states in each one of the independent
+%           Markov chains
+%
 % Outputs
 % --------
+%
+% - **Regimes** [matrix]: Combinations of all states. The matrix is of size
+%   prod([a1,a2,...,an]) x n
+%
+% - **Journal** [cell array]: Description of the transition matrix. Each
+%   cell contains a matrix in which the first row is the regime today and
+%   the second one is the regime tomorrow. The cell array is of size
+%   prod([a1,a2,...,an]) x prod([a1,a2,...,an])
 %
 % More About
 % ------------
@@ -17,37 +38,46 @@ function [Regimes,journal]=chain_grid(v)
 % Examples
 % ---------
 %
+% Regimes=chain_grid(10); : one chain with 10 regimes
+%
+% Regimes=chain_grid([10,5]) : 2 chains with 5 regimes for the second one
+%
+% Regimes=chain_grid([10,5,3]): 3 chains with 3 regimes on the third one
+%
 % See also: 
 
 % v is a vector of the number of states in each markov chain
 % Regimes is an prod(v) x NumberOfParameters matrix describing the different
 % composite regimes
 % example
-% Regimes=chain_grid(10); : one chain with 10 regimes
-% Regimes=chain_grid([10,5]) : 2 chains with 5 regimes for the second one
-% Regimes=chain_grid([10,5,3]): 3 chains with 3 regimes on the third one
 % journal gives information about the transition matrix, which is the
 % tensor product of individual chains' transition matrices.
 
 if ~isequal(v,floor(v))
+    
     error([mfilename,':: elements in v must be integers'])
+    
 end
 
 Regimes=utils.gridfuncs.mygrid(v);
 
-test=num2str(Regimes(:,1));
-for kk=2:size(Regimes,2)
-    test=strcat(test,',',num2str(Regimes(:,kk)));
-end
-
 if nargout>1
+    
     pv=prod(v);
+    
     journal=cell(pv);
+    
     for ii=1:pv
+        
         for jj=1:pv
-            journal{ii,jj}=strcat(test(ii,:),';',test(jj,:));
+            
+            journal{ii,jj}=[Regimes(ii,:) % <-- today's regime
+                Regimes(jj,:)]; % <-- tomorrow's regime
+            
         end
+        
     end
+    
 end
 
 end
