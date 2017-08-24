2d7ccc04f0723f9d2dc9becbc78b1e1aaa285411
Junior Maih <junior.maih@gmail.com>
2017-08-24 17:42:59 +0200

    cosmetics

--- m/+utils/+estim/penalize_violations.m
+++ m/+utils/+estim/penalize_violations.m
@@ -32,23 +32,37 @@ function p=penalize_violations(viol,c)
 % See also:
 
 if nargin<2
+    
     c=[];
+    
 end
+
 if isempty(c)
+    
     c=10;
+    
 end
 
 if c<=0
+    
     error('penalty factor must be strictly positive')
+    
 end
 
 p=0;
+
 if ~isempty(viol)
+    
     viol=max(0,viol(:));
+    
     viol=viol(viol>0);
+    
     if ~isempty(viol)
+        
         p=c*sum(viol.^2);% <--- p=c*sum((viol/min(viol)).^2);
+        
     end
+    
 end
 
 end
