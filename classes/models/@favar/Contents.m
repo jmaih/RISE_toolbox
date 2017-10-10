08c9de181fbe04f17395e4a847fcc523e9e16089
Junior Maih <junior.maih@gmail.com>
2017-10-10 09:39:17 +0200

    removed stochvol and favar

--- classes/models/@favar/Contents.m
+++
@@ -1,3 +0,0 @@
-%-------------------   class: C:\Users\jma\Documents\GitHub\RISE_toolbox\classes\models\favar   -------------------%
-%
-%   <a href="matlab:help classes\models\favar\conclude_estimation">classes\models\favar\conclude_estimation</a>                                            - H1 line
