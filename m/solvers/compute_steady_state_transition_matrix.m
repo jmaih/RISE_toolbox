f4b1d3f22fed42b04322047af15f4b60757e9ea2
Junior Maih <junior.maih@gmail.com>
2017-10-23 13:44:17 +0200

    cosmetics

--- m/solvers/compute_steady_state_transition_matrix.m
+++ m/solvers/compute_steady_state_transition_matrix.m
@@ -22,9 +22,15 @@ function [Q,retcode]=compute_steady_state_transition_matrix(trans_mat_func,...
 %
 % See also: 
 
-s0=1;s1=1;
+s0=1;
+
+s1=1;
+
 xss=zeros(exo_nbr,1);
+
 ys_=ss_0;
+
 [Q,retcode]= utils.code.evaluate_transition_matrices(trans_mat_func,...
     ys_,xss,ss_0,pp0,def_0,s0,s1);
+
 end
