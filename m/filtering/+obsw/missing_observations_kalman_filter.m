9366104812acc7fd54af53707592512dcfed5025
Junior Maih <junior.maih@gmail.com>
2017-10-25 14:49:01 +0200

    cosmetics

--- m/filtering/+obsw/missing_observations_kalman_filter.m
+++ m/filtering/+obsw/missing_observations_kalman_filter.m
@@ -29,28 +29,28 @@ function f=missing_observations_kalman_filter(y,T,R,Z,H,Q,init,dy,ca,level,nstep
 %
 %   - **ca** [m x nca]:
 %
-%   - **level** [0|1|2|{3}]: controls the level output produced. 
-%       if level==0, only the likelihood is returned 
-%       if level==1, filters/forecasts are returned in addition to level 0 
-%       if level==2, updates are returned in addition to level 1 
-%       if level==3, smoothed values are returned in addition to level 2 
+%   - **level** [0|1|2|{3}]: controls the level output produced.
+%       if level==0, only the likelihood is returned
+%       if level==1, filters/forecasts are returned in addition to level 0
+%       if level==2, updates are returned in addition to level 1
+%       if level==3, smoothed values are returned in addition to level 2
 %
-%   - **nstep** [integer|{1}]: Number of forecast steps 
+%   - **nstep** [integer|{1}]: Number of forecast steps
 %
 % Outputs
 % --------
 %
-%   - **f** [struct]: output structure 
+%   - **f** [struct]: output structure
 %       - **retcode** [0|305]: 305 if computation of the likelihood breaks
-%       downs  
+%       downs
 %       - **incr** [vector]: likelihood in each period
-%       - **log_lik** [scalar]: log likelihood  
-%       - **a** [m x n+1 x nstep]: filtered stated (available if level >0)  
+%       - **log_lik** [scalar]: log likelihood
+%       - **a** [m x n+1 x nstep]: filtered stated (available if level >0)
 %       - **P** [m x m x n+1 x nstep]: Covariance matrix of filtered state
-%       (available if level >0) 
+%       (available if level >0)
 %       - **att** [m x n]: Updated state vector  (available if level >1)
 %       - **Ptt** [m x m x n]: Covariance matrix for updates (available if
-%       level >1) 
+%       level >1)
 %       - **iF** [p x p x n]: Inverses of covariance matrix of forecast
 %       errors
 %       - **v** [p x n]: Forecast errors
@@ -60,7 +60,7 @@ function f=missing_observations_kalman_filter(y,T,R,Z,H,Q,init,dy,ca,level,nstep
 %       - **r** [m x n+1]: quantity useful for the efficient computation of
 %       the state vector
 %       - **eta** [r x n]: smoothed shocks
-%       - **epsilon** [p x n]: smoothed measurement errors  
+%       - **epsilon** [p x n]: smoothed measurement errors
 %
 % More About
 % ------------
@@ -68,31 +68,33 @@ function f=missing_observations_kalman_filter(y,T,R,Z,H,Q,init,dy,ca,level,nstep
 % The system is of the form
 %   alpha(t+1) = T*alpha(t) + ca(t) + R(t)*eta(t),  eta ~ N(0, Q)
 %   y(t) =   Z*alpha(t) + dy(t) + epsilon(t),  epsilon ~ N(0,H)
-%                    
+%
 % Examples
 % ---------
 %
-% See also: 
+% See also:
 
 if nargin < 11
     
     nstep=[];
-
-if nargin<10
     
-    level=[];
-    
-    if nargin<9
+    if nargin<10
         
-        ca=[];
+        level=[];
         
-        if nargin<8
+        if nargin<9
             
-            dy=[];
+            ca=[];
             
-            if nargin<7
+            if nargin<8
                 
-                init=[];
+                dy=[];
+                
+                if nargin<7
+                    
+                    init=[];
+                    
+                end
                 
             end
             
@@ -100,8 +102,6 @@ if nargin<10
         
     end
     
-end
-
 end
 
 if isempty(nstep), nstep=1; end
@@ -248,7 +248,7 @@ end
                 return
                 
             end
-                        
+            
             f.incr(t)=-0.5*(pstar*l2pi+log(detF)+v.'*iF*v);
             %             f.incr(t)=-pstar/2*log(2*pi)-1/2*(log(det(F))+v.'*iF*v);
             
