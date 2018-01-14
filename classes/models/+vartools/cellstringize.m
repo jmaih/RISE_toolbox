d74a97fde3aca7a1601f593d2455c49b7a22b372
Junior Maih <junior.maih@gmail.com>
2018-01-14 17:39:40 +0100

    added check for duplicates

--- classes/models/+vartools/cellstringize.m
+++ classes/models/+vartools/cellstringize.m
@@ -24,4 +24,38 @@ if ~isempty(badx)
     
 end
 
+n=numel(x);
+
+ux=unique(x);
+
+if n>numel(ux)
+    
+    badx=cell(1,n);
+    
+    ii=0;
+    
+    while ~isempty(ux)
+        
+        ping=strcmp(ux{1},x);
+        
+        if sum(ping)>1
+            
+            ii=ii+1;
+            
+            badx{ii}=ux{1};
+            
+        end
+        
+        ux=ux(2:end);
+        
+    end
+    
+    badx=badx(1:ii);
+    
+    disp(badx)
+    
+    error('the above variables are duplicated')
+    
+end
+
 end
