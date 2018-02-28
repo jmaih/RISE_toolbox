5728762ec5a372ebccc2bcd23c767a35c90eda2e
Junior Maih <junior.maih@gmail.com>
2018-02-28 19:53:29 +0100

    ts: correlation with nans

--- classes/time_series/@ts/corr.m
+++ classes/time_series/@ts/corr.m
@@ -20,14 +20,33 @@ function varargout=corr(this,varargin)
 % See also: 
 
 if ~isempty(varargin) && isa(varargin{1},'ts')
+    
     this=this & varargin{1};
+    
     varargin=varargin(2:end);
+
 end
-this=this.data;
-if size(this,3)>1
+
+data=this.data;
+
+if size(data,3)>1
+
     error([mfilename,':: this operation is only defined for databases with one page'])
+
 end
+
 nout=nargout;
-[varargout{1:nout}]=utils.stat.corr(this.data,varargin{:});
+
+bad=any(isnan(data),2);
+
+if any(bad)
+    
+    data(bad,:)=[];
+        
+    disp([int2str(sum(bad)),' nan rows above discarded'])
+    
+end
+
+[varargout{1:nout}]=utils.stat.corr(data,varargin{:});
 
 end
