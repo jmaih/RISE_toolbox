45308dd9e7c3f3f7592bb5648420c5401687724a
Junior Maih <junior.maih@gmail.com>
2018-03-01 15:56:42 +0100

    basic help

--- m/+utils/+functional_programming/multiloop.m
+++ m/+utils/+functional_programming/multiloop.m
@@ -1,4 +1,13 @@
 function [b,out]=multiloop(b,start,pos,start_init,dofunc,out,varargin)
+% this function is designed to replace multi for loops
+% b : vector gathering the indices of the for the different for loops
+% start: index where the current for loop will start
+% pos: position of the current for loop
+% start_init: true or false, start the next for loop from 1 or from the
+% same index as the previous one
+% dofunc : at the end of one pass, i.e. b is completed, it is evaluated.
+% This is a function handle
+% out : input and output of the dofunc function
 
 n=size(out,2);
 
