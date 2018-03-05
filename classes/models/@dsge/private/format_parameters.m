c733ba75b00362bbb235e07d31aa66a3bf2345cb
Junior Maih <junior.maih@gmail.com>
2018-03-05 16:10:40 +0100

    cosmetics

--- classes/models/@dsge/private/format_parameters.m
+++ classes/models/@dsge/private/format_parameters.m
@@ -23,17 +23,25 @@ function obj=format_parameters(obj,ParameterizationArray)
 param_names=obj.parameters.name;
 
 if obj.is_dsge_var_model
+    
     obj.dsge_prior_weight_id=find(strcmp('dsge_prior_weight',param_names),1);
+    
 end
 
 if obj.is_hybrid_expectations_model
     % set all hybrid coefficients to 1
     pnames=obj.parameters.name;
+    
     good=strncmp(pnames,'hbe_param_',10);
+    
     pnames=pnames(good);
+    
     np=numel(pnames);
+    
     hbe_calibration=[pnames(:),num2cell(ones(np,1))];
+    
     obj=setup_calibration(obj,hbe_calibration);
+    
 end
 
 % baseline calibration
@@ -51,10 +59,14 @@ obj=setup_priors(obj,ParameterizationArray.Priors,ParameterizationArray.error_co
 % check that all the parameters in the model are in use
 %------------------------------------------------------
 not_in_use=param_names(~obj.parameters.is_in_use);
+
 if ~isempty(not_in_use)
+    
     warning([' the following parameters do not seem to affect model ',obj.filename,...
         '. You may want to discard them from model for tidiness'])
+    
     disp(not_in_use)
+    
 end
 
 end
