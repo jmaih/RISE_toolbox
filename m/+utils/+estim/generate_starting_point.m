29f9f9e8e0001c8c3ff80623e28bd710b295cb65
Junior Maih <junior.maih@gmail.com>
2018-03-05 16:08:32 +0100

    cosmetics

--- m/+utils/+estim/generate_starting_point.m
+++ m/+utils/+estim/generate_starting_point.m
@@ -25,23 +25,39 @@ function [x,f,retcode]=generate_starting_point(objective,max_trials)
 % satisfies them all can be challenging. The strategy is to select the
 % vector with the least possible constraints.
 if nargin<2
+    
     max_trials=[];
+    
 end
+
 if isempty(max_trials)
+    
     max_trials=50;
+    
 end
 
 [x,f,~,retcode,viol]=objective();
+
 iter=0;
+
 while iter<max_trials && sum(viol)>0
+    
     iter=iter+1;
+    
     [newc,fc,~,retcodec,violc]=objective();
+    
     if sum(violc)<sum(viol)
+        
         x=newc;
+        
         viol=violc;
+        
         retcode=retcodec;
+        
         f=fc;
+        
     end
+    
 end
 
 end
