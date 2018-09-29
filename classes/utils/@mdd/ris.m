43a186fc2549008e6dddf660b336b354c7c899f0
Junior Maih <junior.maih@gmail.com>
2018-09-29 20:53:25 +0200

    default inputs

--- classes/utils/@mdd/ris.m
+++ classes/utils/@mdd/ris.m
@@ -4,6 +4,10 @@ function log_mdd=ris(obj,~,opts)
 % likelihoods for mixture and Markov switching models using bridge
 % sampling techniques". Econometrics Journal 7(1) pp 143--167
 
+n=nargin;
+
+set_inputs()
+
 Logq_M=old_draws_in_weighting_function(obj,opts,'RIS');
 
 ratio = Logq_M-obj.LogPost_M;
@@ -11,5 +15,17 @@ ratio = Logq_M-obj.LogPost_M;
 ratiomax=max(ratio);
 
 log_mdd = -1*(ratiomax+log(mean(exp(ratio-ratiomax))));
+
+    function set_inputs()
+        
+        if n<3
+            
+            opts=[];
+            
+        end
+        
+        opts=mdd.global_options(opts);
+        
+    end
 
 end
