8c10db1a8f3983d500f956e7866532e436d8e52a
Junior Maih <junior.maih@gmail.com>
2018-10-02 19:06:20 +0200

    option for untransforming params

--- classes/utils/@mcmc/mcmc.m
+++ classes/utils/@mcmc/mcmc.m
@@ -1,7 +1,7 @@
 classdef mcmc < handle
-% mcmc object makes diagnostic plots from mcmc draws
-%
-
+    % mcmc object makes diagnostic plots from mcmc draws
+    %
+    
     properties
         pnames
         nchains
@@ -13,10 +13,10 @@ classdef mcmc < handle
         psrf
         best
     end
-
+    
     methods
-
-        function obj=mcmc(draws,pnames,drop,start_from,trimming)
+        
+        function obj=mcmc(draws,pnames,drop,start_from,trimming,ilinres)
             % Constructor for mcmc object
             %
             % ::
@@ -34,6 +34,10 @@ classdef mcmc < handle
             %    start_from (integer): discard first (start_from-1) samples (default: 1)
             %
             %    trimming (integer): only use every trimming value (default: 1)
+            %
+            %    ilinres (function handle|{empty}): function handle that
+            %       untransforms the parameters in the presence of linear
+            %       restrictions.
             %
             % Returns:
             %    :
@@ -58,103 +62,121 @@ classdef mcmc < handle
             %      values, so make sure that **start_from** parameters is the
             %      intended value.
             %
-
-            if nargin<5
-
-                trimming=[];
-
-                if nargin<4
-
-                    start_from=[];
-
-                    if nargin<3
-
-                        drop=[];
-
-                        if nargin<2
-
-                            pnames=[];
-
-                            if nargin<1
-
-                                obj=mcmc.empty();
-
-                                return
-
+            
+            n=nargin;
+            
+            if n<6
+                
+                ilinres=[];
+                
+                if n<5
+                    
+                    trimming=[];
+                    
+                    if n<4
+                        
+                        start_from=[];
+                        
+                        if n<3
+                            
+                            drop=[];
+                            
+                            if n<2
+                                
+                                pnames=[];
+                                
+                                if n<1
+                                    
+                                    obj=mcmc.empty();
+                                    
+                                    return
+                                    
+                                end
+                                
                             end
-
+                            
                         end
-
+                        
                     end
-
+                    
                 end
-
+                
             end
-
+            
             [obj.draws,obj.i_dropped,obj.start,results_summary]=mcmc.process_draws(draws,drop,start_from,trimming);
-
+            
             obj.best=results_summary.best_of_the_best;
-
+            
+            if ~isempty(ilinres)
+                % untransform the parameters
+                for ii=1:numel(obj.draws)
+                    
+                    obj.draws(ii).x=ilinres(obj.draws(ii).x);
+                    
+                end
+                
+            end
+            
             [obj.nchains,obj.npop]=size(obj.draws);
-
+            
             if obj.npop
-
+                
                 obj.nparams=numel(obj.draws(1).x);
-
+                
             else
-
+                
                 obj=mcmc.empty();
-
+                
                 return
-
+                
             end
-
+            
             if isempty(pnames)
-
+                
                 pnames=parser.create_state_list('p',obj.nparams);
-
+                
             end
-
+            
             obj.pnames=pnames;
-
+            
             if obj.npop
-
+                
                 obj.psrf=gelman_rubin(obj,true);
-
+                
             end
-
+            
         end
-
+        
         varargout=traceplot(varargin)
-
+        
         varargout=densplot(varargin)
-
+        
         varargout=meanplot(varargin)
-
+        
         varargout=autocorrplot(varargin)
-
+        
         varargout=psrf_plot(varargin)
-
+        
         varargout=scatterplot(varargin)
-
+        
         varargout=summary(varargin)
-
+        
         % varargout=heidelberg_welch(varargin)
-
+        
     end
-
+    
     methods(Static)
-
+        
         varargout=process_draws(varargin)
-
+        
     end
-
+    
     methods(Access=private)
-
+        
         varargout=load_draws(varargin)
-
+        
         varargout=gelman_rubin(varargin)
-
+        
     end
-
+    
 end
