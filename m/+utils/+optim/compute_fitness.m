393e817534de09735e01d0570f17417a4611c8d6
Junior Maih <junior.maih@gmail.com>
2019-03-13 04:44:16 +0100

    cosmetics

--- m/+utils/+optim/compute_fitness.m
+++ m/+utils/+optim/compute_fitness.m
@@ -3,17 +3,33 @@ function fit=compute_fitness(ff,flag)
 %
 
 if nargin<2
+    
     flag=1;
+    
 end
+
 switch flag
+    
     case 1
+        
         fit=nan(size(ff));
+        
         pos=ff>=0;
+        
         neg=~pos;
+        
         fit(pos)=1./(1+ff(pos));
+        
         fit(neg)=1+abs(ff(neg));
+        
     case 2
+        
         fit = exp(-ff/(1+max(abs(ff))));
+        
     otherwise
+        
         error([mfilename,':: unknown flag'])
+        
+end
+
 end
