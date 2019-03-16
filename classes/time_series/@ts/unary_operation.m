d683f76bdcc340c5af9c8c3a3bdcb0ae44411639
Junior Maih <junior.maih@gmail.com>
2019-03-16 22:56:08 +0100

    Added functionality to ts

--- classes/time_series/@ts/unary_operation.m
+++ classes/time_series/@ts/unary_operation.m
@@ -16,9 +16,15 @@ function db=unary_operation(db,op_string)
 %    See also:
 
 if ischar(op_string)
+    
     op_string=str2func(['@(x)',op_string,'(x)']);
+
 elseif ~isa(op_string,'function_handle')
+
     error('function must be a string or a function handle')
+
 end
+
 db=ts(db.date_numbers,op_string(db.data));
+
 end
